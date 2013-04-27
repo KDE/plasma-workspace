0fc3deb057d727f4cf917886393eee214b26024b
Sebastian Kügler <sebas@kde.org>
2013-04-27 03:13:01 +0200

    Correct Messages.sh 	../lookandfeel/

--- qmlpackages/desktop/Messages.sh
+++ qmlpackages/desktop/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_package_org.kde.active.contour-tablet-homescreen.pot
+$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_shell_org.kde.desktop.pot
 rm -f rc.cpp
