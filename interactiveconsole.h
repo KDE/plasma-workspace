199668fb8bd9dc2776f60d86d50c704489e962fd
Marco Martin <notmart@gmail.com>
2014-02-26 16:24:28 +0100

    add copyright

--- interactiveconsole.h
+++ interactiveconsole.h
@@ -1,5 +1,6 @@
 /*
  *   Copyright 2009 Aaron Seigo <aseigo@kde.org>
+ *   Copyright 2014 Marco Martin <mart@kde.org>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as
