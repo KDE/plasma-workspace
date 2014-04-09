cdc5a414e8d90eb267dd3ec3d0ac42438110c413
Burkhard Lück <lueck@hube-lueck.de>
2014-04-09 13:02:48 +0200

    fix msg extraction to follow code move into subdir bugzillaintegration

--- drkonqi/Messages.sh
+++ drkonqi/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC ui/*.ui >> rc.cpp
-$XGETTEXT *.cpp -o $podir/drkonqi.pot
+$EXTRACTRC ui/*.ui bugzillaintegration/ui/*.ui >> rc.cpp
+$XGETTEXT *.cpp bugzillaintegration/*.cpp -o $podir/drkonqi.pot
 rm -f rc.cpp
