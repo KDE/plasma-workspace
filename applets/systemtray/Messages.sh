62af4ef66b688075a00cbbaa8a0e7b68938320a9
Albert Astals Cid <aacid@kde.org>
2014-05-19 23:11:16 +0200

    Do not extract i18n from the tests folder

--- applets/systemtray/Messages.sh
+++ applets/systemtray/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.js -o -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_org.kde.plasma.systemtray.pot
+$EXTRACTRC `find . \( -name \*.rc -o -name \*.ui -o -name \*.kcfg \) -not -path "./tests/*"` >> rc.cpp
+$XGETTEXT `find . \( -name \*.js -o -name \*.qml -o -name \*.cpp \) -not -path "./tests/*"` -o $podir/plasma_applet_org.kde.plasma.systemtray.pot
 rm rc.cpp
