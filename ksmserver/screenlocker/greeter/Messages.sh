efb17fe58cc1a18fcc0d9dc31efd0bb0a5420d95
Yuri Chornoivan <yurchor@ukr.net>
2014-06-05 17:59:40 +0300

    Fix POT generation

--- ksmserver/screenlocker/greeter/Messages.sh
+++ ksmserver/screenlocker/greeter/Messages.sh
@@ -1,3 +1,2 @@
 #!bin/sh
 $XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kscreenlocker_greet.pot
-$XGETTEXT `find . -name '*.qml'` -j -L Java -o $podir/kscreenlocker_greet.pot
--- ksmserver/screenlocker/kcm/Messages.sh
+++ ksmserver/screenlocker/kcm/Messages.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 $EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.h *.cpp -o $podir/screenlocker_kcm.pot
+$XGETTEXT *.cpp -o $podir/screenlocker_kcm.pot
 rm -f rc.cpp
