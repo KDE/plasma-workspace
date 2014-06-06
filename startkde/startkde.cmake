778c4b5febbaf546e0c48f472432d41625759c6e
Marco Martin <notmart@gmail.com>
2014-06-06 19:17:15 +0200

    mouse cursor as breeze_cursors

--- startkde/startkde.cmake
+++ startkde/startkde.cmake
@@ -66,7 +66,7 @@ mkdir -p $configDir
 
 #This is basically setting defaults so we can use them with kstartupconfig5
 cat >$configDir/startupconfigkeys <<EOF
-kcminputrc Mouse cursorTheme 'breeze'
+kcminputrc Mouse cursorTheme 'breeze_cursors'
 kcminputrc Mouse cursorSize ''
 ksplashrc KSplash Theme Breeze
 ksplashrc KSplash Engine KSplashQML
