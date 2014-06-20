cd4dbbca57e1483954ff53eec3e8d3983d95aedd
Jonathan Riddell <jr@jriddell.org>
2014-06-20 10:32:16 +0100

    Add xsession file for Plasma REVIEW:118732

---
+++ plasma.desktop.cmake
@@ -0,0 +1,8 @@
+[Desktop Entry]
+Encoding=UTF-8
+Type=XSession
+Exec=${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/startkde
+TryExec=${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/startkde
+DesktopNames=KDE
+Name=Plasma
+Comment=Plasma by KDE
