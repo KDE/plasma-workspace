KDE/plasma-workspace

commit fa52f0f7379ef99dd2fc6d94fa47983f8610855b

Battery plasmoid, code simplification
If we have a ListView that is not interactive and resizes to show everything,
we can have a Row { Repeater {} } just as well, which will also work faster
and has less reported bugs.

applets/batterymonitor/contents/ui/CompactRepresentation.qml
@@ -26,21 +26,25 @@ import org.kde.plasma.components 2.0 as Components
 import org.kde.plasma.workspace.components 2.0
 import "plasmapackage:/code/logic.js" as Logic
 
-Item {
+//Should we consider turning this into a Flow item?
+Row {
     id: root
     Layout.minimumWidth: isConstrained() ? units.iconSizes.medium : 24 // NOTE: Keep in sync with systray
     Layout.minimumHeight: isConstrained() ? units.iconSizes.medium * view.count : 24
+    property real itemSize: Math.min(root.height, root.width/view.count)
 
     function isConstrained() {
         return (plasmoid.formFactor == PlasmaCore.Types.Vertical || plasmoid.formFactor == PlasmaCore.Types.Horizontal)
     }
 
-    ListView {
-        id: view
+    MouseArea {
+        anchors.fill: parent
+        onClicked: plasmoid.expanded = !plasmoid.expanded
+    }
 
-        anchors.centerIn: parent
-        width: Math.min(parent.width, contentWidth)
-        height: Math.min(parent.height, width)
+    Repeater {
+        id: view
+        anchors.fill: parent
 
         property bool hasBattery: pmSource.data["Battery"]["Has Battery"]
 
@@ -51,11 +55,6 @@ Item {
 
         model: singleBattery ? 1 : batteries
 
-        orientation: ListView.Horizontal
-        interactive: false
-
-        cacheBuffer: 1000
-
         delegate: Item {
             id: batteryContainer
 
@@ -63,13 +62,13 @@ Item {
             property int percent: view.singleBattery ? batteries.cumulativePercent : model["Percent"]
             property bool pluggedIn: view.singleBattery ? (pmSource.data["AC Adapter"] != undefined && pmSource.data["AC Adapter"]["Plugged in"]) : (pmSource.data["AC Adapter"] != undefined && pmSource.data["AC Adapter"]["Plugged in"] && model["Is Power Supply"])
 
-            width: Math.min(root.height, root.width/view.count)
-            height: width
+            height: root.itemSize
+            width: root.width/view.count
 
             property real iconSize: Math.min(width, height)
 
             Column {
-                anchors.fill: parent
+                anchors.centerIn: parent
 
                 BatteryIcon {
                     id: batteryIcon
@@ -91,13 +90,6 @@ Item {
                     visible: false//!isConstrained()
                 }
             }
-        }
-
-        MouseArea {
-            id: mouseArea
-            anchors.fill: parent
-            hoverEnabled: true
-            onClicked: plasmoid.expanded = !plasmoid.expanded
         }
     }
 }

commit 9cbd9290ca44a12fce01bd2ea530ccfd23f665f2

Fix Compact Representation which broke in fa52f0f7379ef99dd2fc6d94fa47983f8610855b
CCMAIL: aleixpol@kde.org

applets/batterymonitor/contents/ui/CompactRepresentation.qml
@@ -26,68 +26,66 @@ import org.kde.plasma.components 2.0 as Components
 import org.kde.plasma.workspace.components 2.0
 import "plasmapackage:/code/logic.js" as Logic
 
-//Should we consider turning this into a Flow item?
-Row {
+MouseArea {
     id: root
     Layout.minimumWidth: isConstrained() ? units.iconSizes.medium : 24 // NOTE: Keep in sync with systray
     Layout.minimumHeight: isConstrained() ? units.iconSizes.medium * view.count : 24
     property real itemSize: Math.min(root.height, root.width/view.count)
 
+    onClicked: plasmoid.expanded = !plasmoid.expanded
+
     function isConstrained() {
         return (plasmoid.formFactor == PlasmaCore.Types.Vertical || plasmoid.formFactor == PlasmaCore.Types.Horizontal)
     }
 
-    MouseArea {
-        anchors.fill: parent
-        onClicked: plasmoid.expanded = !plasmoid.expanded
-    }
+    //Should we consider turning this into a Flow item?
+    Row {
+        Repeater {
+            id: view
 
-    Repeater {
-        id: view
-        anchors.fill: parent
+            property bool hasBattery: pmSource.data["Battery"]["Has Battery"]
 
-        property bool hasBattery: pmSource.data["Battery"]["Has Battery"]
+            /*property QtObject pmSource: batterymonitor.pmSource
+            property QtObject batteries: batterymonitor.batteries*/
 
-        /*property QtObject pmSource: batterymonitor.pmSource
-        property QtObject batteries: batterymonitor.batteries*/
+            property bool singleBattery: isConstrained() || !hasBattery
 
-        property bool singleBattery: isConstrained() || !hasBattery
+            model: singleBattery ? 1 : batteries
 
-        model: singleBattery ? 1 : batteries
+            Item {
+                id: batteryContainer
 
-        delegate: Item {
-            id: batteryContainer
+                property bool hasBattery: view.singleBattery ? batteries.count : model["Plugged in"]
+                property int percent: view.singleBattery ? batteries.cumulativePercent : model["Percent"]
+                property bool pluggedIn: view.singleBattery ? batteries.charging : (model["Is Power Supply"] && model["State"] != "Discharging")
 
-            property bool hasBattery: view.singleBattery ? batteries.count : model["Plugged in"]
-            property int percent: view.singleBattery ? batteries.cumulativePercent : model["Percent"]
-            property bool pluggedIn: pmSource.data["AC Adapter"] != undefined && pmSource.data["AC Adapter"]["Plugged in"] && (view.singleBattery || model["Is Power Supply"])
+                height: root.itemSize
+                width: root.width/view.count
 
-            height: root.itemSize
-            width: root.width/view.count
+                property real iconSize: Math.min(width, height)
 
-            property real iconSize: Math.min(width, height)
+                Column {
+                    anchors.centerIn: parent
 
-            Column {
-                anchors.centerIn: parent
+                    BatteryIcon {
+                        id: batteryIcon
+                        anchors.horizontalCenter: isConstrained() ? undefined : parent.horizontalCenter
+                        hasBattery: batteryContainer.hasBattery
+                        percent: batteryContainer.percent
+                        pluggedIn: batteryContainer.pluggedIn
+                        height: isConstrained() ? batteryContainer.iconSize : batteryContainer.iconSize - batteryLabel.height
+                        width: height
+                    }
 
-                BatteryIcon {
-                    id: batteryIcon
-                    anchors.horizontalCenter: isConstrained() ? undefined : parent.horizontalCenter
-                    hasBattery: batteryContainer.hasBattery
-                    percent: batteryContainer.percent
-                    pluggedIn: batteryContainer.pluggedIn
-                    height: isConstrained() ? batteryContainer.iconSize : batteryContainer.iconSize - batteryLabel.height
-                    width: height
-                }
-
-                Components.Label {
-                    id: batteryLabel
-                    width: parent.width
-                    height: visible ? paintedHeight : 0
-                    horizontalAlignment: Text.AlignHCenter
-                    text: i18nc("battery percentage below battery icon", "%1%", percent)
-                    font.pixelSize: Math.max(batteryContainer.iconSize/8, theme.mSize(theme.smallestFont).height)
-                    visible: false//!isConstrained()
+                    Components.Label {
+                        id: batteryLabel
+                        width: parent.width
+                        height: visible ? paintedHeight : 0
+                        horizontalAlignment: Text.AlignHCenter
+                        text: i18nc("battery percentage below battery icon", "%1%", percent)
+                        font.pixelSize: Math.max(batteryContainer.iconSize/8, theme.mSize(theme.smallestFont).height)
+                        visible: false//!isConstrained()
+                    }
                 }
             }
         }