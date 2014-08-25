fa8252b55522b62f1e20292f530e5c18c15d2082
Dan Vrátil <dvratil@redhat.com>
2014-08-25 14:28:22 +0200

    Fix typo in plasma.desktop.cmake

--- plasma.desktop.cmake
+++ plasma.desktop.cmake
@@ -2,7 +2,7 @@
 Encoding=UTF-8
 Type=XSession
 Exec=${CMAKE_INSTALL_FULL_BINDIR}/startkde
-TryEexec=${CMAKE_INSTALL_FULL_BINDIR}/startkde
+TryExec=${CMAKE_INSTALL_FULL_BINDIR}/startkde
 DesktopNames=KDE
 Name=Plasma
 Name[ar]=بلازما
