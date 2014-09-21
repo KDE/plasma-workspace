2f226aaddcff30543baa6bf6295bc658ef69b43a
Sebastian Kügler <sebas@kde.org>
2014-09-22 01:45:54 +0200

    Streamline name of statusnotifier engine's desktop file

--- dataengines/statusnotifieritem/statusnotifieritem_engine.cpp
+++ dataengines/statusnotifieritem/statusnotifieritem_engine.cpp
@@ -140,6 +140,6 @@ void StatusNotifierItemEngine::newItem(const QString &service)
     addSource(itemSource);
 }
 
-K_EXPORT_PLASMA_DATAENGINE_WITH_JSON(statusnotifieritem, StatusNotifierItemEngine,"plasma-engine-statusnotifieritem.json")
+K_EXPORT_PLASMA_DATAENGINE_WITH_JSON(statusnotifieritem, StatusNotifierItemEngine,"plasma-dataengine-statusnotifieritem.json")
 
 #include "statusnotifieritem_engine.moc"
