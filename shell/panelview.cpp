23a47c882fd1fae7667466c580615fb2e18ec623
David Edmundson <kde@davidedmundson.co.uk>
2014-10-21 17:11:52 +0200

    typo--

--- shell/panelview.cpp
+++ shell/panelview.cpp
@@ -673,7 +673,7 @@ bool PanelView::event(QEvent *e)
     }
 
     /*Fitt's law: if the containment has margins, and the mouse cursor clicked
-     * on the mouse edge, forwar the click in the containment boundaries
+     * on the mouse edge, forward the click in the containment boundaries
      */
     switch (e->type()) {
         case QEvent::MouseMove:
