b6c4e5aa53fa0391ccb302b886a5390615a280ad
Martin Gräßlin <mgraesslin@kde.org>
2015-02-10 10:43:53 +0100

    [screenlocker] Support global shortcut for changing keyboard layout
    BUG: 331726
    FIXED-IN: 5.3.0
    REVIEW: 122508

--- ksmserver/screenlocker/globalaccel.cpp
+++ ksmserver/screenlocker/globalaccel.cpp
@@ -50,7 +50,8 @@ static const QMap<QString, QList<QString>> s_shortcutWhitelist{
                                                        QStringLiteral("decrease_volume"),
                                                        QStringLiteral("increase_volume")}},
     {QStringLiteral("/component/kded5"), {QStringLiteral("Increase Screen Brightness"),
-                    QStringLiteral("Decrease Screen Brightness")}}
+                    QStringLiteral("Decrease Screen Brightness")}},
+    {QStringLiteral("/component/KDE_Keyboard_Layout_Switcher"), {QStringLiteral("Switch to Next Keyboard Layout")}}
 };
 
 static uint g_keyModMaskXAccel = 0;
