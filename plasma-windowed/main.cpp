47912771bf414ca17b1e865ec23b92f4607cad5a
Aleix Pol <aleixpol@kde.org>
2015-02-21 15:50:41 +0100

    Revert "Remove plasma-windowed"
    This reverts commit f07fcd07c71b720bc89a1a9e68bccc82f21f8f69.

---
+++ plasma-windowed/main.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2014  Bhushan Shah <bhush94@gmail.com>
+ * Copyright 2014 Marco Martin <notmart@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include <QApplication>
+#include <qcommandlineparser.h>
+#include <qcommandlineoption.h>
+
+#include <KDBusService>
+#include <KLocalizedString>
+
+#include "plasmawindowedview.h"
+#include "plasmawindowedcorona.h"
+
+static const char version[] = "1.0";
+
+int main(int argc, char **argv)
+{
+    QQuickWindow::setDefaultAlphaBuffer(true);
+
+    QApplication app(argc, argv);
+    app.setApplicationVersion(version);
+    app.setOrganizationDomain(QStringLiteral("kde.org"));
+
+    KDBusService service(KDBusService::Unique);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(i18n("Plasma Windowed"));
+    parser.addOption(QCommandLineOption("statusnotifier", i18n("Makes the plasmoid stay alive in the Notification Area, even when the window is closed.")));
+    parser.addPositionalArgument("applet", i18n("The applet to open."));
+    parser.addPositionalArgument("args", i18n("Arguments to pass to the plasmoid."), QStringLiteral("[args...]"));
+    parser.addVersionOption();
+    parser.addHelpOption();
+    parser.process(app);
+
+    if (parser.positionalArguments().isEmpty()) {
+        parser.showHelp(1);
+    }
+
+    PlasmaWindowedCorona *corona = new PlasmaWindowedCorona();
+
+    QVariantList args;
+    QStringList::const_iterator constIterator;
+    constIterator = parser.positionalArguments().constBegin();
+    ++constIterator;
+    for (; constIterator != parser.positionalArguments().constEnd();
+           ++constIterator) {
+        args << (*constIterator);
+    }
+    corona->setHasStatusNotifier(parser.isSet("statusnotifier"));
+    corona->loadApplet(parser.positionalArguments().first(), args);
+
+    QObject::connect(&service, &KDBusService::activateRequested, corona, &PlasmaWindowedCorona::activateRequested);
+
+    const int ret = app.exec();
+    delete corona;
+    return ret;
+}
