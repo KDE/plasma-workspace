c4c7e6d53f66fbdd6d58b40e5f3b443c6cf2e197
Martin Gräßlin <mgraesslin@kde.org>
2015-03-12 14:36:12 +0100

    Revert "Fix segfault with missing screens"
    This reverts commit aba0ad12685e2946a42d6a60d8cca58aa24b6d2a.
    
    The reason for revertion is that it leaks pixmaps.
    QX11Info::display should not return a nullptr if there is no QScreen.
    This needs fixing in Qt, not workarounds in our software. None of our
    X11 specific code in plasma-workspace or frameworks can handle the case
    that the Display* or xcb_connection_t* becomes null suddenly. Neither
    can Qt internally. If it would happen Qt would abort.
    
    The only application in our workspace which would be "somewhat" safe
    is KWin because it caches the returned Display after first invokation
    to QX11Info::display.

--- shell/panelshadows.cpp
+++ shell/panelshadows.cpp
@@ -473,9 +473,6 @@ void PanelShadows::Private::updateShadow(const QWindow *window, Plasma::FrameSvg
     }
 
     Display *dpy = QX11Info::display();
-    if (!dpy) {
-        return;
-    }
     Atom atom = XInternAtom(dpy, "_KDE_NET_WM_SHADOW", False);
 
 //     qDebug() << "going to set the shadow of" << window->winId() << "to" << data;
@@ -494,9 +491,6 @@ void PanelShadows::Private::clearShadow(const QWindow *window)
         return;
     }
     Display *dpy = QX11Info::display();
-    if (!dpy) {
-        return;
-    }
     Atom atom = XInternAtom(dpy, "_KDE_NET_WM_SHADOW", False);
     XDeleteProperty(dpy, window->winId(), atom);
 #else
