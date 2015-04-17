8b3f6ac7a959300803334f6959184a6df34360f0
Heiko Becker <heirecka@exherbo.org>
2015-04-17 12:47:04 +0200

    FindQalculate: Use FindPkgConfig instead of UsePkgConfig
    The latter is deprecated and doesn't respect the PKG_CONFIG
    environment variable which makes it easy to deal with differently
    named pkg-config executables, e.g. arch-prefixed ones.
    
    REVIEW: 123557

--- cmake/FindQalculate.cmake
+++ cmake/FindQalculate.cmake
@@ -43,20 +43,43 @@ if(QALCULATE_CFLAGS AND QALCULATE_LIBRARIES)
 
 else(QALCULATE_CFLAGS AND QALCULATE_LIBRARIES)
   if(NOT WIN32)
-    include(UsePkgConfig)
+    find_package(PkgConfig)
 
     if(QALCULATE_MIN_VERSION)
-      exec_program(${PKGCONFIG_EXECUTABLE} ARGS libqalculate --atleast-version=${QALCULATE_MIN_VERSION} RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull)
+      pkg_check_modules(_pc_QALCULATE libqalculate>=${QALCULATE_MIN_VERSION})
     else(QALCULATE_MIN_VERSION)
-      exec_program(${PKGCONFIG_EXECUTABLE} ARGS libqalculate --exists RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull)
+      pkg_check_modules(_pc_QALCULATE libqalculate)
     endif(QALCULATE_MIN_VERSION)
 
-    if(_return_VALUE STREQUAL "0")
-      exec_program(${PKGCONFIG_EXECUTABLE} ARGS libqalculate --libs OUTPUT_VARIABLE QALCULATE_LIBRARIES)
-      exec_program(${PKGCONFIG_EXECUTABLE} ARGS cln --libs OUTPUT_VARIABLE CLN_LIBRARIES)
-      exec_program(${PKGCONFIG_EXECUTABLE} ARGS libqalculate --cflags OUTPUT_VARIABLE QALCULATE_CFLAGS)
-      set(QALCULATE_FOUND TRUE)
-    endif(_return_VALUE STREQUAL "0")
+    pkg_check_modules(_pc_CLN cln)
+
+    if(_pc_QALCULATE_FOUND)
+      set(QALCULATE_CFLAGS ${_pc_QALCULATE_CFLAGS})
+    endif()
+
+    find_library(QALCULATE_LIBRARIES
+      NAMES
+      qalculate
+      PATHS
+      ${_pc_QALCULATE_LIBRARY_DIRS}
+      ${LIB_INSTALL_DIR}
+    )
+
+    find_path(QALCULATE_INCLUDE_DIR
+      NAMES
+      libqalculate
+      PATHS
+      ${_pc_QALCULATE_INCLUDE_DIRS}
+      ${INCLUDE_INSTALL_DIR}
+    )
+
+    find_library(CLN_LIBRARIES
+      NAMES
+      cln
+      PATHS
+      ${_pc_CLN_LIBRARY_DIRS}
+      ${LIB_INSTALL_DIR}
+    )
 
   else(NOT WIN32)
     # XXX: currently no libqalculate on windows
