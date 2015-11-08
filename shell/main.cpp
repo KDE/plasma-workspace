e20c4c6b7a2910bef752a19a414488e257a49419
David Edmundson <kde@davidedmundson.co.uk>
2015-11-08 21:03:05 +0000

    Catch other openGL error gracefully
    CCBUG: 354966
    CCBUG: 355054
    REVIEW: 125997

--- shell/main.cpp
+++ shell/main.cpp
@@ -40,9 +40,12 @@ void plasmaMessageHandler(QtMsgType type, const QMessageLogContext &context, con
     //if we get an openGL error, display it properly in a user visible way.
     //Qt qFatals when failing to make an openGL context; we can't catch that earlier, so we do it in the error handler and display an error message
     //otherwise we continually get bug reports
-    if (type == QtFatalMsg && message == QLatin1String("Could not initialize GLX")) {
-        QCoreApplication::setAttribute(Qt::AA_ForceRasterWidgets);
-        QMessageBox::critical(nullptr, i18n("Plasma Failed To Start"), i18n("Plasma is unable to start as it could not correctly use OpenGL 2.\nPlease check that your graphic drivers are set up correctly."));
+    if (Q_UNLIKELY(type == QtFatalMsg)) {
+            if (message == QLatin1String("Could not initialize GLX") ||
+                    message == QLatin1String("QXcbIntegration: Cannot create platform OpenGL context, neither GLX nor EGL are enabled")) {
+                QCoreApplication::setAttribute(Qt::AA_ForceRasterWidgets);
+                QMessageBox::critical(nullptr, i18n("Plasma Failed To Start"), i18n("Plasma is unable to start as it could not correctly use OpenGL 2.\nPlease check that your graphic drivers are set up correctly."));
+        }
     }
 
     //we have to reimplement the behaviour of the default message handler, as now we are always overriding it, and there's no way to access it
