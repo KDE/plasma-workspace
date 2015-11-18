49194b62294350751aaa1361d6c868c47a371cec
Bhushan Shah <bhush94@gmail.com>
2015-11-18 14:52:56 +0530

    install session file for Plasma on Wayland session
    This is known to work with at least sddm 0.13 and higher. It is listed
    as "Plasma (wayland)".
    
    REVIEW: 126098

---
+++ plasmawayland.desktop.cmake
@@ -0,0 +1,8 @@
+[Desktop Entry]
+Encoding=UTF-8
+Exec=${CMAKE_INSTALL_FULL_BINDIR}/startplasmacompositor
+TryExec=${CMAKE_INSTALL_FULL_BINDIR}/startplasmacompositor
+DesktopNames=KDE
+Name=Plasma
+Comment=Plasma by KDE
+X-KDE-PluginInfo-Version=${PROJECT_VERSION}
