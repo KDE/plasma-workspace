0059d87b6f74f950b2aac94763c2934ec710c6c4
Jan Kundrát <jkt@kde.org>
2015-12-01 19:34:59 +0100

    Do not produce negative struts on switching screens
    Turns out that this method is sometimes called at the time where the
    virtualGeometry() is invalid -- which sounds pretty much like a Qt bug,
    but it isn't exactly the first bug in screen handling in Qt5. Rather
    than producing values which are surely invalid, try to cover up the
    mess.
    
    REVIEW: 126216

--- shell/panelview.cpp
+++ shell/panelview.cpp
@@ -915,7 +915,8 @@ void PanelView::updateStruts()
 
     if (m_visibilityMode == NormalPanel) {
         const QRect thisScreen = screen()->geometry();
-        const QRect wholeScreen = screen()->virtualGeometry();
+        // QScreen::virtualGeometry() is very unreliable (Qt 5.5)
+        const QRect wholeScreen = QRect(QPoint(0, 0), m_corona->screensConfiguration()->screen()->currentSize());
 
         //Extended struts against a screen edge near to another screen are really harmful, so windows maximized under the panel is a lesser pain
         //TODO: force "windows can cover" in those cases?
