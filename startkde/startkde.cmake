1dc7f11692a4096b9815ae24f7be9cec10f8f7a5
David Edmundson <kde@davidedmundson.co.uk>
2015-12-06 19:47:12 +0000

    Move shutdown scripts into ksmserver cleanup
    Shutdown scripts are done by startkde after ksmserver quits. Which never
    happens because we've told systemd to shutdown.
    
    Old systems worked because they used to communicate with the display
    manager which then closed us before shutting down, this is no longer the
    case.
    
    This moves handling the shutdown scripts into ksmserver (which already
    handles startup scripts) and runs them before asking logind to shutdown.
    
    BUG: 356190
    REVIEW: 126268

--- startkde/startkde.cmake
+++ startkde/startkde.cmake
@@ -412,15 +412,6 @@ test -n "$ksplash_pid" && kill "$ksplash_pid" 2>/dev/null
 # Clean up
 kdeinit5_shutdown
 
-echo 'startkde: Running shutdown scripts...'  1>&2
-
-# Run scripts found in <config locations>/plasma-workspace/shutdown
-for prefix in `echo "$scriptpath"`; do
-  for file in `ls "$prefix"/shutdown 2> /dev/null | egrep -v '(~|\.bak)$'`; do
-    test -x "$prefix/shutdown/$file" && "$prefix/shutdown/$file"
-  done
-done
-
 unset KDE_FULL_SESSION
 xprop -root -remove KDE_FULL_SESSION
 unset KDE_SESSION_VERSION
