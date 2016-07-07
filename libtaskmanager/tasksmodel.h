313aede08506b87358e0d770911c562d3ac6f234
Eike Hein <hein@kde.org>
2016-07-08 05:32:27 +0900

    Return LegacyWinIdList for groups in final proxy sort order.
    TaskGroupingProxyModel appends; the proxy may sort differently
    (e.g. alphabetically by default). The win id list was returned
    in TGPM sort order, which meant the tooltips had the thumbnails
    in the wrong order, and clicking them would activate the wrong
    window.
    
    BUG:365181

--- libtaskmanager/tasksmodel.h
+++ libtaskmanager/tasksmodel.h
@@ -111,6 +111,8 @@ public:
 
     Q_INVOKABLE int rowCount(const QModelIndex &parent = QModelIndex()) const; // Invokable.
 
+    QVariant data(const QModelIndex &proxyIndex, int role) const override;
+
     /**
      * The number of launcher tasks in the tast list.
      *
