KDE/plasma-workspace

commit a697d291f98e923ef0c5d2221bf04187600d4ea8

make sure desktop containments exist before script
the script should just change the configuration of the desktop
containments, not creating them, as they are too tied to activities

shell/shellcorona.cpp
@@ -709,6 +709,7 @@ void ShellCorona::unload()
     m_panelViews.clear();
     m_desktopContainments.clear();
     m_waitingPanels.clear();
+    m_activityContainmentPlugins.clear();
 
     while (!containments().isEmpty()) {
         //deleting a containment will remove it from the list due to QObject::destroyed connect in Corona
@@ -730,6 +731,12 @@ void ShellCorona::requestApplicationConfigSync()
 
 void ShellCorona::loadDefaultLayout()
 {
+    //NOTE: Is important the containments already exist for each screen
+    // at the moment of the script execution
+    for (QScreen* screen : qGuiApp->screens()) {
+        addOutput(screen);
+    }
+
     QString script = ShellManager::s_testModeLayout;
 
     if (script.isEmpty()) {

commit b9760e852042fb113caef8ad547967b98cfdd32a

Introduce new pre-layout script hook
Summary:
layout.js used to be run before any containments were added. This was
broken in a697d291 which creates desktop containments for that activity
first.

However, some scripts rely on being able to alter global configuration
before any containments are loaded.

In order to allow the same functionality we need to provide a new hook
for whatever these scripts may need to do.

BUG: 371704

Test Plan: Not done yet.

Reviewers: #plasma, mart

Reviewed By: mart

Subscribers: apol, mart, plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D3220

shell/shellcorona.cpp
@@ -850,6 +850,30 @@ void ShellCorona::requestApplicationConfigSync()
 
 void ShellCorona::loadDefaultLayout()
 {
+    //pre-startup scripts
+    QString script = m_lookAndFeelPackage.filePath("layouts", QString(shell() + "-prelayout.js").toLatin1());
+    if (!script.isEmpty()) {
+          QFile file(script);
+            if (file.open(QIODevice::ReadOnly | QIODevice::Text) ) {
+                QString code = file.readAll();
+                qDebug() << "evaluating pre-startup script:" << script;
+
+                WorkspaceScripting::ScriptEngine scriptEngine(this);
+
+                connect(&scriptEngine, &WorkspaceScripting::ScriptEngine::printError, this,
+                        [](const QString &msg) {
+                            qWarning() << msg;
+                        });
+                connect(&scriptEngine, &WorkspaceScripting::ScriptEngine::print, this,
+                        [](const QString &msg) {
+                            qDebug() << msg;
+                        });
+                if (!scriptEngine.evaluateScript(code, script)) {
+                    qWarning() << "failed to initialize layout properly:" << script;
+                }
+            }
+    }
+
     //NOTE: Is important the containments already exist for each screen
     // at the moment of the script execution,the same loop in :load()
     // is executed too late
@@ -857,7 +881,7 @@ void ShellCorona::loadDefaultLayout()
         addOutput(screen);
     }
 
-    QString script = ShellManager::s_testModeLayout;
+    script = ShellManager::s_testModeLayout;
 
     if (script.isEmpty()) {
         script = m_lookAndFeelPackage.filePath("layouts", QString(shell() + "-layout.js").toLatin1());