2a3b74df75123678577161e4ea6b9549246ffdf0
Elvis Angelaccio <elvis.angelaccio@kde.org>
2017-02-11 18:26:49 +0100

    [kioslave/remote] Fix broken kded module
    Summary:
    Commit 72e39f083a10 broke the kded module. Turns out that the old code
    just forced a refresh of the `remote:/` QUrl, so we can just do that
    explicitly and remove all the old cruft.
    
    While at it, use a KDirWatcher that just watches the
    `.local/share/remote/` folder, instead of checking *every* unrelated
    QUrl emitted by KDirNotify.
    
    Test Plan:
    Adding/deleting/editing .desktop files in `.local/share/remoteview` now
    automatically reloads the `remote:/` url in Dolphin or Folder View.
    
    Reviewers: #plasma
    
    Subscribers: plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D4573

--- kioslave/remote/kdedmodule/remotedirnotify.cpp
+++ kioslave/remote/kdedmodule/remotedirnotify.cpp
@@ -19,118 +19,25 @@
 #include "remotedirnotify.h"
 
 #include "../debug.h"
-#include <kdesktopfile.h>
 #include <kdirnotify.h>
-#include <KIO/Global>
+#include <KDirWatch>
 
 #include <QtDBus/QtDBus>
 
 RemoteDirNotify::RemoteDirNotify()
 {
 	const QString path = QStringLiteral("%1/remoteview").arg(QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation));
-	m_baseURL.setPath(path);
 
-	QDBusConnection::sessionBus().connect(QString(), QString(), QStringLiteral("org.kde.KDirNotify"),
-				    QStringLiteral("FilesAdded"), this, SLOT(FilesAdded(QString)));
-	QDBusConnection::sessionBus().connect(QString(), QString(), QStringLiteral("org.kde.KDirNotify"),
-				    QStringLiteral("FilesRemoved"), this, SLOT(FilesRemoved(QStringList)));
-	QDBusConnection::sessionBus().connect(QString(), QString(), QStringLiteral("org.kde.KDirNotify"),
-				    QStringLiteral("FilesChanged"), this, SLOT(FilesChanged(QStringList)));
+    m_dirWatch = new KDirWatch(this);
+    m_dirWatch->addDir(path, KDirWatch::WatchFiles);
+
+    connect(m_dirWatch, &KDirWatch::created, this, &RemoteDirNotify::slotRemoteChanged);
+    connect(m_dirWatch, &KDirWatch::deleted, this, &RemoteDirNotify::slotRemoteChanged);
+    connect(m_dirWatch, &KDirWatch::dirty, this, &RemoteDirNotify::slotRemoteChanged);
 }
 
-QUrl RemoteDirNotify::toRemoteURL(const QUrl &url)
+void RemoteDirNotify::slotRemoteChanged()
 {
-	qCDebug(KIOREMOTE_LOG) << "RemoteDirNotify::toRemoteURL(" << url << ")";
-	if ( m_baseURL.isParentOf(url) )
-	{
-		QString path = QDir(m_baseURL.path()).relativeFilePath(url.path());
-		QUrl result;
-		result.setScheme(QStringLiteral("remote"));
-		result.setPath(path);
-		result.setPath(QDir::cleanPath(result.path()));
-		qCDebug(KIOREMOTE_LOG) << "result => " << result;
-		return result;
-	}
-
-	qCDebug(KIOREMOTE_LOG) << "result => QUrl()";
-	return QUrl();
-}
-
-QList<QUrl> RemoteDirNotify::toRemoteURLList(const QStringList &list)
-{
-    QList<QUrl> urls;
-    for (const QString &file : list) {
-        QUrl url = toRemoteURL(QUrl::fromLocalFile(file));
-        if (url.isValid()) {
-            urls.append(url);
-        }
-    }
-
-    return urls;
-}
-
-void RemoteDirNotify::FilesAdded(const QString &directory)
-{
-	qCDebug(KIOREMOTE_LOG) << "RemoteDirNotify::FilesAdded";
-
-	QUrl new_dir = toRemoteURL(QUrl::fromLocalFile(directory));
-
-	if (new_dir.isValid())
-	{
-		org::kde::KDirNotify::emitFilesAdded(new_dir);
-	}
-}
-
-// This hack is required because of the way we manage .desktop files with
-// Forwarding Slaves, their URL is out of the ioslave (most remote:/ files
-// have a file:/ based UDS_URL so that they are executed correctly.
-// Hence, FilesRemoved and FilesChanged does nothing... We're forced to use
-// FilesAdded to re-list the modified directory.
-inline void evil_hack(const QList<QUrl> &list)
-{
-	QList<QUrl> notified;
-
-	QList<QUrl>::const_iterator it = list.begin();
-	QList<QUrl>::const_iterator end = list.end();
-
-	for (; it!=end; ++it)
-	{
-		QUrl url = KIO::upUrl(*it);
-
-		if (!notified.contains(url))
-		{
-			org::kde::KDirNotify::emitFilesAdded(url);
-			notified.append(url);
-		}
-	}
-}
-
-
-void RemoteDirNotify::FilesRemoved(const QStringList &fileList)
-{
-	qCDebug(KIOREMOTE_LOG) << "RemoteDirNotify::FilesRemoved";
-
-	QList<QUrl> new_list = toRemoteURLList(fileList);
-
-	if (!new_list.isEmpty())
-	{
-		//KDirNotify_stub notifier("*", "*");
-		//notifier.FilesRemoved( new_list );
-		evil_hack(new_list);
-	}
-}
-
-void RemoteDirNotify::FilesChanged(const QStringList &fileList)
-{
-	qCDebug(KIOREMOTE_LOG) << "RemoteDirNotify::FilesChanged";
-
-	QList<QUrl> new_list = toRemoteURLList(fileList);
-
-	if (!new_list.isEmpty())
-	{
-		//KDirNotify_stub notifier("*", "*");
-		//notifier.FilesChanged( new_list );
-		evil_hack(new_list);
-	}
+    org::kde::KDirNotify::emitFilesAdded(QUrl("remote:/"));
 }
 
