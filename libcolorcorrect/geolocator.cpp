030897484cc0a0e3dcd9a5b45f038ba4b2046855
David Edmundson <kde@davidedmundson.co.uk>
2017-12-11 10:50:19 +0000

    Add missing include

--- libcolorcorrect/geolocator.cpp
+++ libcolorcorrect/geolocator.cpp
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 #include "geolocator.h"
+#include <cmath>
 
 namespace ColorCorrect
 {
