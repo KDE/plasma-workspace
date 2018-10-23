790f5bf48f2b6b8c23e2dafc8f0071066215d85d
Fabian Vogt <fabian@ritter-vogt.de>
2018-10-23 09:52:36 +0200

    Only use wl-shell for the ksmserver greeters on Qt < 5.12
    Summary:
    Apparently wl-shell fullscreen is broken on Qt 5.12 now, but we don't need this
    workaround anymore.
    
    BUG: 399918
    
    Test Plan:
    Clicked on logout, got the greeter. Without this patch, the greeter
    was not visible.
    
    Reviewers: #plasma, davidedmundson
    
    Reviewed By: #plasma, davidedmundson
    
    Subscribers: davidedmundson, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D16381

--- ksmserver/switchuser-greeter/main.cpp
+++ ksmserver/switchuser-greeter/main.cpp
@@ -25,6 +25,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <QGuiApplication>
 #include <QScreen>
+#include <QLibraryInfo>
 
 #include "../switchuserdialog.h"
 
@@ -135,8 +136,10 @@ bool Greeter::eventFilter(QObject *watched, QEvent *event)
 
 int main(int argc, char *argv[])
 {
-    // Qt does not currently (5.9.4) support fullscreen on xdg_shell v6.
-    qputenv("QT_WAYLAND_SHELL_INTEGRATION", "wl-shell");
+    if (QLibraryInfo::version() < QVersionNumber(5, 12)) {
+        // Before Qt 5.12, the xdg-shell v6 integration does not support fullscreen.
+        qputenv("QT_WAYLAND_SHELL_INTEGRATION", "wl-shell");
+    }
 
     QQuickWindow::setDefaultAlphaBuffer(true);
     QGuiApplication app(argc, argv);
