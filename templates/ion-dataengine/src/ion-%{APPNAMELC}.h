2fffd036fccfbd1dca8c62ec1f5ccdff5cc8dd0c
Friedrich W. H. Kossebau <kossebau@kde.org>
2019-01-31 17:43:54 +0100

    [weather ion template] Fix stray chars in license headers
    GIT_SILENT

--- templates/ion-dataengine/src/ion-%{APPNAMELC}.h
+++ templates/ion-dataengine/src/ion-%{APPNAMELC}.h
@@ -1,5 +1,6 @@
 /*
- *   Copyright (C) %{CURRENT_YEAR} by %{AUTHOR} <%{EMAIL}>                      *
+ *   Copyright (C) %{CURRENT_YEAR} by %{AUTHOR} <%{EMAIL}>
+ *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as
  *   published by the Free Software Foundation; either version 2, or
