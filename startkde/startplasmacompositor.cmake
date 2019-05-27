707fbb4c45376d6e2634f49cab03386880f9d6c1
Bhushan Shah <bhush94@gmail.com>
2019-05-27 15:54:23 +0530

    Remove unrelated code to make sure oxygen font is installed
    Summary:
    Relevant code is already removed from startkde, remove it from plasma
    wayland session as well
    
    Test Plan: none
    
    Reviewers: #plasma, davidedmundson
    
    Reviewed By: #plasma, davidedmundson
    
    Subscribers: davidedmundson, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D21426

--- startkde/startplasmacompositor.cmake
+++ startkde/startplasmacompositor.cmake
@@ -39,38 +39,6 @@ test -f $exportformatssettings && {
     . $exportformatssettings
 }
 
-# Make sure the Oxygen font is installed
-# This is necessary for setups where CMAKE_INSTALL_PREFIX
-# is not in /usr. fontconfig looks in /usr, ~/.fonts and
-# $XDG_DATA_HOME for fonts. In this case, we symlink the
-# Oxygen font under ${XDG_DATA_HOME} and make it known to
-# fontconfig
-
-usr_share="/usr/share"
-install_share="@KDE_INSTALL_FULL_DATADIR@"
-
-if [ ! $install_share = $usr_share ]; then
-
-    if [ ${XDG_DATA_HOME} ]; then
-        fontsDir="${XDG_DATA_HOME}/fonts"
-    else
-        fontsDir="${HOME}/.fonts"
-    fi
-
-    test -d $fontsDir || {
-        mkdir -p $fontsDir
-    }
-
-    oxygenDir=$fontsDir/truetype/oxygen
-    prefixDir="@KDE_INSTALL_FULL_DATADIR@/fonts/truetype/oxygen"
-
-    # if the oxygen dir doesn't exist, create a symlink to be sure that the
-    # Oxygen font is available to the user
-    test -d $oxygenDir || test -d $prefixDir && {
-        test -h $oxygenDir || ln -s $prefixDir $oxygenDir && fc-cache $oxygenDir
-    }
-fi
-
 kstartupconfig5
 returncode=$?
 if test $returncode -ne 0; then
