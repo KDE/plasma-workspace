b9734166ce481a664eec6aaaab559a3d7021abf8
Filip Fila <filipfila.kde@gmail.com>
2019-06-05 22:32:32 +0200

    [SDDM theme] Use the biggest wallpaper resolution
    Summary: Starting from Plasma 5.16 we have the Next wallpaper is available in 5K resolution so we can make use of it in the login screen as well.
    
    Reviewers: #vdg, #plasma, ngraham
    
    Reviewed By: #vdg, ngraham
    
    Subscribers: davidedmundson, ngraham, plasma-devel
    
    Tags: #plasma
    
    Maniphest Tasks: T10269
    
    Differential Revision: https://phabricator.kde.org/D18007

--- sddm-theme/theme.conf.cmake
+++ sddm-theme/theme.conf.cmake
@@ -2,4 +2,4 @@
 type=image
 color=#1d99f3
 fontSize=10
-background=${CMAKE_INSTALL_PREFIX}/${WALLPAPER_INSTALL_DIR}/Next/contents/images/3200x2000.png
+background=${CMAKE_INSTALL_PREFIX}/${WALLPAPER_INSTALL_DIR}/Next/contents/images/5120x2880.png
