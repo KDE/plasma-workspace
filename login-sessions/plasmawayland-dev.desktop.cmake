1cc0b27fb840f017300f5e7ee28a4b9d635378a3
Bhushan Shah <bhush94@gmail.com>
2019-06-23 14:51:50 +0530

    Fix wayland-dev session
    Summary: without dbus-run-session developer can't login into this session
    
    Test Plan: now login works
    
    Reviewers: apol
    
    Reviewed By: apol
    
    Subscribers: plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D22020

--- login-sessions/plasmawayland-dev.desktop.cmake
+++ login-sessions/plasmawayland-dev.desktop.cmake
@@ -1,5 +1,5 @@
 [Desktop Entry]
-Exec=@CMAKE_INSTALL_FULL_LIBEXECDIR@/startplasma-dev.sh -wayland
+Exec=dbus-run-session @CMAKE_INSTALL_FULL_LIBEXECDIR@/startplasma-dev.sh -wayland
 DesktopNames=KDE
 Name=Plasma (Development ${CMAKE_INSTALL_FULL_BINDIR})
 Comment=Plasma by KDE
