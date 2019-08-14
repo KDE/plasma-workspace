9a3b9e67040255c4df9eb0aaff26b9e330b335c6
Shawn Starr <shawn.starr@rogers.com>
2019-08-14 14:45:35 -0400

    Bump copyright here too

--- dataengines/weather/ions/noaa/ion_noaa.cpp
+++ dataengines/weather/ions/noaa/ion_noaa.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2007-2009 by Shawn Starr <shawn.starr@rogers.com>       *
+ *   Copyright (C) 2007-2009,2019 by Shawn Starr <shawn.starr@rogers.com>  *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
