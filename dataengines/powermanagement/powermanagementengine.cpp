8e8441146aa8103d0c35b668935749ace6e1efa0
Méven Car <meven29@gmail.com>
2019-08-19 09:03:49 +0200

    Do not display vendor, Product and capacity in network plasmoid
    Summary:
    Those information mostly clutter the plasmoid UI and are not very useful in the energy plasmoid
    The battery plasmoid represents the state of the battery, for battery information we have the Energy information KCM in kinfocenter that will display this after D23152 .
    
    Test Plan:
    Before:
    {F7257309}
    After:
    {F7257310}
    
    Reviewers: ngraham, #plasma, broulik, #vdg, filipf
    
    Reviewed By: ngraham, #vdg, filipf
    
    Subscribers: apol, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D23237

--- dataengines/powermanagement/powermanagementengine.cpp
+++ dataengines/powermanagement/powermanagementengine.cpp
@@ -179,9 +179,6 @@ bool PowermanagementEngine::sourceRequestEvent(const QString &name)
             updateBatteryPresentState(battery->isPresent(), deviceBattery.udi());
             updateBatteryPowerSupplyState(battery->isPowerSupply(), deviceBattery.udi());
 
-            setData(source, QStringLiteral("Vendor"), deviceBattery.vendor());
-            setData(source, QStringLiteral("Product"), deviceBattery.product());
-            setData(source, QStringLiteral("Capacity"), battery->capacity());
             setData(source, QStringLiteral("Type"), batteryType(battery));
         }
 
@@ -587,9 +584,6 @@ void PowermanagementEngine::deviceAdded(const QString& udi)
             updateBatteryPresentState(battery->isPresent(), device.udi());
             updateBatteryPowerSupplyState(battery->isPowerSupply(), device.udi());
 
-            setData(source, QStringLiteral("Vendor"), device.vendor());
-            setData(source, QStringLiteral("Product"), device.product());
-            setData(source, QStringLiteral("Capacity"), battery->capacity());
             setData(source, QStringLiteral("Type"), batteryType(battery));
 
             setData(QStringLiteral("Battery"), QStringLiteral("Sources"), sourceNames);
