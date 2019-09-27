de4197e6eecb8d3a9fe452600bce55b4b4fca842
l10n daemon script <scripty@kde.org>
2019-09-27 05:22:05 +0200

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- shell/org.kde.plasmashell.desktop.cmake
+++ shell/org.kde.plasmashell.desktop.cmake
@@ -44,6 +44,7 @@ Name[sr@ijekavian]=Плазма, радни простор површи
 Name[sr@ijekavianlatin]=Plasma, radni prostor površi
 Name[sr@latin]=Plasma, radni prostor površi
 Name[sv]=Plasma arbetsområde för skrivbordet
+Name[tg]=Фазои мизи кории Плазма
 Name[tr]=Plasma Masaüstü Çalışma Alanı
 Name[uk]=Робочий простір Плазми для комп’ютерів
 Name[x-test]=xxPlasma Desktop Workspacexx
