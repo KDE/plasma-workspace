0e4aeb50557b134d0b0e89dafcb69ff066399b6f
David Edmundson <kde@davidedmundson.co.uk>
2019-11-06 12:55:07 +0000

    handle a dev setup

--- login-sessions/startplasma-dev.sh.cmake
+++ login-sessions/startplasma-dev.sh.cmake
@@ -2,4 +2,10 @@
 
 source @CMAKE_INSTALL_FULL_LIBEXECDIR@/plasma-dev-prefix.sh
 
+# Copy systemd unit files in a custom prefix dir into the user's runtime directory
+
+mkdir -p "$XDG_RUNTIME_DIR/systemd/user.control"
+command cp -r @KDE_INSTALL_FULL_SYSTEMDUSERUNITDIR@/* $XDG_RUNTIME_DIR/systemd/user.control
+systemctl --user daemon-reload
+
 startplasma$@
