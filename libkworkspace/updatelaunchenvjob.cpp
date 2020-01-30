6253163af827e049d53b912494e04c1ea36d4e57
Kai Uwe Broulik <kde@privat.broulik.de>
2020-01-30 11:54:09 +0100

    [libkworkspace] Introduce UpdateLaunchEnvJob
    This is supposed to replace manual DBus calls to KLauncher setLaunchEnv done in e.g. the KCMs,
    so this can easily be extended in the future.
    
    Differential Revision: https://phabricator.kde.org/D26993

---
+++ libkworkspace/updatelaunchenvjob.cpp
@@ -0,0 +1,101 @@
+/*
+   Copyright (C) 2020 Kai Uwe Broulik <kde@broulik.de>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the Lesser GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+   You should have received a copy of the Lesser GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "updatelaunchenvjob.h"
+
+#include <klauncher_interface.h>
+#include <startup_interface.h>
+
+class Q_DECL_HIDDEN UpdateLaunchEnvJob::Private
+{
+public:
+    explicit Private(UpdateLaunchEnvJob *q);
+    void monitorReply(const QDBusPendingReply<> &reply);
+
+    UpdateLaunchEnvJob *q;
+    QString varName;
+    QString value;
+    int pendingReplies = 0;
+};
+
+UpdateLaunchEnvJob::Private::Private(UpdateLaunchEnvJob *q)
+    : q(q)
+{
+
+}
+
+void UpdateLaunchEnvJob::Private::monitorReply(const QDBusPendingReply<> &reply)
+{
+    ++pendingReplies;
+
+    auto *watcher = new QDBusPendingCallWatcher(reply, q);
+    QObject::connect(watcher, &QDBusPendingCallWatcher::finished, q, [this](QDBusPendingCallWatcher *watcher) {
+        watcher->deleteLater();
+        --pendingReplies;
+
+        if (pendingReplies == 0) {
+            q->emitResult();
+        }
+    });
+}
+
+UpdateLaunchEnvJob::UpdateLaunchEnvJob(const QString &varName, const QString &value)
+    : d(new Private(this))
+{
+    d->varName = varName;
+    d->value = value;
+}
+
+UpdateLaunchEnvJob::~UpdateLaunchEnvJob()
+{
+    delete d;
+}
+
+void UpdateLaunchEnvJob::start()
+{
+    // KLauncher
+    org::kde::KLauncher klauncher(QStringLiteral("org.kde.klauncher5"),
+                                  QStringLiteral("/KLauncher"),
+                                  QDBusConnection::sessionBus());
+    auto klauncherReply = klauncher.setLaunchEnv(d->varName, d->value);
+    d->monitorReply(klauncherReply);
+
+    // plasma-session
+    org::kde::Startup startup(QStringLiteral("org.kde.Startup"),
+                              QStringLiteral("/Startup"),
+                              QDBusConnection::sessionBus());
+    auto startupReply = startup.updateLaunchEnv(d->varName, d->value);
+    d->monitorReply(startupReply);
+
+    // DBus-activation environment
+    qDBusRegisterMetaType<QMap<QString, QString>>();
+    const QMap<QString, QString> dbusActivationEnv{
+        {d->varName, d->value}
+    };
+
+    QDBusMessage dbusActivationMsg = QDBusMessage::createMethodCall(QStringLiteral("org.freedesktop.DBus"),
+                                                                    QStringLiteral("/org/freedesktop/DBus"),
+                                                                    QStringLiteral("org.freedesktop.DBus"),
+                                                                    QStringLiteral("UpdateActivationEnvironment"));
+    dbusActivationMsg.setArguments({QVariant::fromValue(dbusActivationEnv)});
+
+    auto dbusActivationReply = QDBusConnection::sessionBus().asyncCall(dbusActivationMsg);
+    d->monitorReply(dbusActivationReply);
+}
+
