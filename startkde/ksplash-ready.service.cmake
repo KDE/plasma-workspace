612016836d2a851f4f589b9cf414dab7dcdfbeae
Bhushan Shah <bhush94@gmail.com>
2020-02-25 18:30:34 +0530

    Some changes
    - use QtBinariesDir
    - Actually add Wants in the ksplash-ready, so is enabled
    - Make plasma-workspace.target wait on plasma-core and ksplash-ready
    
    (What usecase does plasma-workspace.target serve even? it's just
    plasma-core?)

--- startkde/ksplash-ready.service.cmake
+++ startkde/ksplash-ready.service.cmake
@@ -1,10 +1,11 @@
 [Unit]
 Description=KSplash "ready" Stage
+Wants=plasma-core.target
 After=plasma-core.target
 
 [Service]
 Type=oneshot
-ExecStart=@CMAKE_INSTALL_FULL_BINDIR@/qdbus org.kde.KSplash /KSplash org.kde.KSplash.setStage ready
+ExecStart=@QtBinariesDir@/qdbus org.kde.KSplash /KSplash org.kde.KSplash.setStage ready
 
 # [Install]
 # WantedBy=plasma-workspace.target
