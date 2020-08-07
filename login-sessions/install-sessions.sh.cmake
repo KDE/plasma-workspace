0d95472c5117c64f4f4fb5d58bd0e8cfaae477c9
macfanpl <63730921+wlisikpl@users.noreply.github.com>
2020-08-07 16:14:14 +0200

    No sudo usage needed

--- login-sessions/install-sessions.sh.cmake
+++ login-sessions/install-sessions.sh.cmake
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-
-sudo install @CMAKE_CURRENT_BINARY_DIR@/plasmax11-dev.desktop /usr/share/xsessions/
-sudo install @CMAKE_CURRENT_BINARY_DIR@/plasmawayland-dev.desktop /usr/share/wayland-sessions/
+# No need for sudo, since user is logged as root already
+install @CMAKE_CURRENT_BINARY_DIR@/plasmax11-dev.desktop /usr/share/xsessions/
+install @CMAKE_CURRENT_BINARY_DIR@/plasmawayland-dev.desktop /usr/share/wayland-sessions/
 install @CMAKE_BINARY_DIR@/prefix.sh @CMAKE_INSTALL_FULL_LIBEXECDIR@/plasma-dev-prefix.sh
 install @CMAKE_CURRENT_BINARY_DIR@/startplasma-dev.sh @CMAKE_INSTALL_FULL_LIBEXECDIR@
