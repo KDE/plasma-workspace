KDE/plasma-workspace

commit f55b00febdd4a9ed2910b820a9f37a0083969d40

Begin porting to PlasmaComponents3
This begins the process of porting plasma-workspace to PlasmaComponents3. At
this point, only the relatively easy work is done. Every file not ported
now has been given the reason for still using PC2 in a comment on the import
line so you can see at a glance what the blocker is by running
`grep -r "org.kde.plasma.components 2.0"` in the repo.

Everything is tested and there are no breakages or functional regressions. In
the process, a few unused imports are removed.

Here are the remaining PC3 omissions blocking what's left from getting easily
ported:
- No Highlight
- No ListItem
- No PageStack
- No ContextMenu
- No QueryDialog
- No ModelContextMenu
- TabBar has no left and right tab states
- ToolButton has no built-in method to show a menu when clicked

lookandfeel/contents/runcommand/RunCommand.qml
@@ -19,7 +19,8 @@ import QtQuick 2.6
 import QtQuick.Layouts 1.1
 import QtQuick.Window 2.1
 import org.kde.plasma.core 2.0 as PlasmaCore
-import org.kde.plasma.components 2.0 as PlasmaComponents
+import org.kde.plasma.components 2.0 as PlasmaComponents // For Highlight
+import org.kde.plasma.components 3.0 as PlasmaComponents3
 import org.kde.plasma.extras 2.0 as PlasmaExtras
 import org.kde.milou 0.1 as Milou
 
@@ -52,8 +53,8 @@ ColumnLayout {
 
     RowLayout {
         Layout.alignment: Qt.AlignTop
-        PlasmaComponents.ToolButton {
-            iconSource: "configure"
+        PlasmaComponents3.ToolButton {
+            icon.name: "configure"
             onClicked: {
                 runnerWindow.visible = false
                 runnerWindow.displayConfiguration()
@@ -61,9 +62,11 @@ ColumnLayout {
             Accessible.name: i18nd("plasma_lookandfeel_org.kde.lookandfeel", "Configure")
             Accessible.description: i18nd("plasma_lookandfeel_org.kde.lookandfeel", "Configure Search Plugins")
             visible: runnerWindow.canConfigure
-            tooltip: i18nd("plasma_lookandfeel_org.kde.lookandfeel", "Configure KRunner...")
+            PlasmaComponents3.ToolTip {
+                text: i18nd("plasma_lookandfeel_org.kde.lookandfeel", "Configure KRunner...")
+            }
         }
-        PlasmaComponents.TextField {
+        PlasmaComponents3.TextField {
             id: queryField
             property bool allowCompletion: false
 
@@ -77,7 +80,7 @@ ColumnLayout {
                                                 : i18ndc("plasma_lookandfeel_org.kde.lookandfeel",
                                                          "Textfield placeholder text", "Search...")
 
-            PlasmaComponents.BusyIndicator {
+            PlasmaComponents3.BusyIndicator {
                 anchors {
                     right: parent.right
                     top: parent.top
@@ -193,12 +196,14 @@ ColumnLayout {
                 }
             }
         }
-        PlasmaComponents.ToolButton {
-            iconSource: "window-close"
+        PlasmaComponents3.ToolButton {
+            icon.name: "window-close"
             onClicked: runnerWindow.visible = false
             Accessible.name: i18nd("plasma_lookandfeel_org.kde.lookandfeel", "Close")
             Accessible.description: i18nd("plasma_lookandfeel_org.kde.lookandfeel", "Close Search")
-            tooltip: i18nd("plasma_lookandfeel_org.kde.lookandfeel", "Close")
+            PlasmaComponents3.ToolTip {
+                text: i18nd("plasma_lookandfeel_org.kde.lookandfeel", "Close")
+            }
         }
     }
 

applets/activitybar/contents/ui/main.qml
@@ -20,7 +20,7 @@ import QtQuick 2.0
 import QtQuick.Layouts 1.1
 import org.kde.plasma.plasmoid 2.0
 import org.kde.plasma.core 2.0 as PlasmaCore
-import org.kde.plasma.components 2.0 as PlasmaComponents
+import org.kde.plasma.components 2.0 as PlasmaComponents // PC3 Tabbar only has top and bottom tab positions, not left and right
 import org.kde.activities 0.1 as Activities
 
 Item {

applets/appmenu/package/contents/ui/main.qml
@@ -23,7 +23,7 @@ import QtQuick.Controls 2.8
 import org.kde.plasma.plasmoid 2.0
 import org.kde.kquickcontrolsaddons 2.0
 import org.kde.plasma.core 2.0 as PlasmaCore
-import org.kde.plasma.components 2.0 as PlasmaComponents
+import org.kde.plasma.components 3.0 as PlasmaComponents3
 import org.kde.plasma.private.appmenu 1.0 as AppMenuPrivate
 
 Item {
@@ -41,7 +41,7 @@ Item {
 
     Plasmoid.preferredRepresentation: (plasmoid.configuration.compactView || vertical) ? Plasmoid.compactRepresentation : Plasmoid.fullRepresentation
 
-    Plasmoid.compactRepresentation: PlasmaComponents.ToolButton {
+    Plasmoid.compactRepresentation: PlasmaComponents3.ToolButton {
         readonly property int fakeIndex: 0
         Layout.fillWidth: false
         Layout.fillHeight: false
@@ -50,7 +50,7 @@ Item {
         enabled:  menuAvailable
         checkable: menuAvailable && plasmoid.nativeInterface.currentIndex === fakeIndex
         checked: checkable
-        iconSource: "application-menu"
+        icon.name: "application-menu"
         onClicked: plasmoid.nativeInterface.trigger(this, 0);
     }
 
@@ -100,7 +100,7 @@ Item {
             connectedSources: ["Alt"]
         }
 
-        PlasmaComponents.ToolButton {
+        PlasmaComponents3.ToolButton {
             id: noMenuPlaceholder
             visible: buttonRepeater.count == 0
             text: plasmoid.title
@@ -113,7 +113,7 @@ Item {
             id: buttonRepeater
             model: appMenuModel.visible ? appMenuModel : null
 
-            PlasmaComponents.ToolButton {
+            PlasmaComponents3.ToolButton {
                 readonly property int buttonIndex: index
 
                 Layout.preferredWidth: minimumWidth

applets/calendar/package/contents/ui/main.qml
@@ -21,7 +21,7 @@ import QtQuick.Layouts 1.1
 
 import org.kde.plasma.plasmoid 2.0
 import org.kde.plasma.core 2.0 as PlasmaCore
-import org.kde.plasma.components 2.0 as PlasmaComponents
+import org.kde.plasma.components 3.0 as PlasmaComponents3
 
 import org.kde.plasma.calendar 2.0
 
@@ -59,12 +59,11 @@ Item {
             anchors.fill: parent
             source: Qt.resolvedUrl("../images/mini-calendar.svgz")
 
-            PlasmaComponents.Label {
+            PlasmaComponents3.Label {
                 anchors {
                     fill: parent
                     margins: Math.round(parent.width * 0.1)
                 }
-                height: undefined
                 horizontalAlignment: Text.AlignHCenter
                 verticalAlignment: Text.AlignVCenter
                 font.pixelSize: 1000

applets/clipboard/contents/ui/BarcodePage.qml
@@ -19,7 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import QtQuick 2.0
 import QtQuick.Layouts 1.1
 import org.kde.plasma.core 2.0 as PlasmaCore
-import org.kde.plasma.components 2.0 as PlasmaComponents
+import org.kde.plasma.components 2.0 as PlasmaComponents // For ContextMenu
+import org.kde.plasma.components 3.0 as PlasmaComponents3
 import org.kde.kquickcontrolsaddons 2.0
 import org.kde.plasma.extras 2.0 as PlasmaExtras
 
@@ -33,9 +34,9 @@ ColumnLayout {
     property var header: PlasmaExtras.PlasmoidHeading {
         RowLayout {
             anchors.fill: parent
-            PlasmaComponents.Button {
+            PlasmaComponents3.Button {
                 Layout.fillWidth: true
-                iconSource: "go-previous-view"
+                icon.name: "go-previous-view"
                 text: i18n("Return to Clipboard")
                 onClicked: stack.pop()
             }
@@ -78,12 +79,15 @@ ColumnLayout {
                     });
                 }
             }
-            PlasmaComponents.ToolButton {
+            PlasmaComponents3.ToolButton {
                 id: configureButton
                 checkable: true
-                iconSource: "configure"
-                tooltip: i18n("Change the barcode type")
+                icon.name: "configure"
                 onClicked: menu.openRelative()
+
+                PlasmaComponents3.ToolTip {
+                    text: i18n("Change the barcode type")
+                }
             }
         }
     }
@@ -102,7 +106,7 @@ ColumnLayout {
             opacity: valid ? 1 : 0
         }
 
-        PlasmaComponents.Label {
+        PlasmaComponents3.Label {
             anchors.fill: parent
             horizontalAlignment: Text.AlignHCenter
             verticalAlignment: Text.AlignVCenter
@@ -111,7 +115,7 @@ ColumnLayout {
             visible: barcodeItem.implicitWidth === 0 && barcodeItem.implicitHeight === 0
         }
 
-        PlasmaComponents.Label {
+        PlasmaComponents3.Label {
             anchors.fill: parent
             horizontalAlignment: Text.AlignHCenter
             verticalAlignment: Text.AlignVCenter

applets/clipboard/contents/ui/ClipboardItemDelegate.qml
@@ -21,7 +21,7 @@ import QtQuick 2.0
 import QtQuick.Layouts 1.1
 import QtGraphicalEffects 1.0
 
-import org.kde.plasma.components 2.0 as PlasmaComponents
+import org.kde.plasma.components 2.0 as PlasmaComponents // For ListItem
 import org.kde.kquickcontrolsaddons 2.0 as KQuickControlsAddons
 
 PlasmaComponents.ListItem {

applets/clipboard/contents/ui/ClipboardPage.qml
@@ -22,7 +22,7 @@ import QtQuick.Layouts 1.1
 
 import org.kde.plasma.plasmoid 2.0
 import org.kde.plasma.core 2.0 as PlasmaCore
-import org.kde.plasma.components 2.0 as PlasmaComponents
+import org.kde.plasma.components 3.0 as PlasmaComponents3
 import org.kde.plasma.extras 2.0 as PlasmaExtras
 
 ColumnLayout {
@@ -81,16 +81,19 @@ ColumnLayout {
             anchors.fill: parent
             enabled: clipboardMenu.model.count > 0
 
-            PlasmaComponents.TextField {
+            PlasmaComponents3.TextField {
                 id: filter
                 placeholderText: i18n("Search...")
                 clearButtonShown: true
                 Layout.fillWidth: true
             }
-            PlasmaComponents.ToolButton {
-                iconSource: "edit-clear-history"
-                tooltip: i18n("Clear history")
+            PlasmaComponents3.ToolButton {
+                icon.name: "edit-clear-history"
                 onClicked: clipboardSource.service("", "clearHistory")
+
+                PlasmaComponents3.ToolTip {
+                    text: i18n("Clear history")
+                }
             }
         }
     }

applets/clipboard/contents/ui/DelegateToolButtons.qml
@@ -20,35 +20,47 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import QtQuick 2.0
 
-import org.kde.plasma.components 2.0 as PlasmaComponents
+import org.kde.plasma.components 3.0 as PlasmaComponents3
 
 Row {
     id: toolButtonsLayout
     visible: menuItem.ListView.isCurrentItem
 
-    PlasmaComponents.ToolButton {
+    PlasmaComponents3.ToolButton {
         // TODO: only show for items supporting actions?
-        iconSource: "system-run"
-        tooltip: i18n("Invoke action")
+        icon.name: "system-run"
         onClicked: menuItem.action(UuidRole)
+
+        PlasmaComponents3.ToolTip {
+            text: i18n("Invoke action")
+        }
     }
-    PlasmaComponents.ToolButton {
+    PlasmaComponents3.ToolButton {
         id: barcodeToolButton
-        iconSource: "view-barcode-qr"
-        tooltip: i18n("Show barcode")
+        icon.name: "view-barcode-qr"
         visible: supportsBarcodes
         onClicked: menuItem.barcode(DisplayRole)
+
+        PlasmaComponents3.ToolTip {
+            text: i18n("Show barcode")
+        }
     }
-    PlasmaComponents.ToolButton {
-        iconSource: "document-edit"
+    PlasmaComponents3.ToolButton {
+        icon.name: "document-edit"
         enabled: !clipboardSource.editing
         visible: TypeRole === 0
-        tooltip: i18n("Edit contents")
         onClicked: menuItem.edit(UuidRole)
+
+        PlasmaComponents3.ToolTip {
+            text: i18n("Edit contents")
+        }
     }
-    PlasmaComponents.ToolButton {
-        iconSource: "edit-delete"
-        tooltip: i18n("Remove from history")
+    PlasmaComponents3.ToolButton {
+        icon.name: "edit-delete"
         onClicked: menuItem.remove(UuidRole)
+
+        PlasmaComponents3.ToolTip {
+            text: i18n("Remove from history")
+        }
     }
 }

commit 9fbc1a520f0cf97396bd9d877f7a28d94780945e

[KRunner] Set maximum width for text field
This fixes a PC3 porting regression introduced with
f55b00febdd4a9ed2910b820a9f37a0083969d40.

lookandfeel/contents/runcommand/RunCommand.qml
@@ -72,6 +72,7 @@ ColumnLayout {
 
             clearButtonShown: true
             Layout.minimumWidth: units.gridUnit * 25
+            Layout.maximumWidth: units.gridUnit * 25
 
             activeFocusOnPress: true
             placeholderText: results.runnerName ? i18ndc("plasma_lookandfeel_org.kde.lookandfeel",