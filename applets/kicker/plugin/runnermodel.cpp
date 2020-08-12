ab4aec9d2aace85b27ac558d5b58d47000408f5b
David Redondo <kde@david-redondo.de>
2020-08-12 08:58:00 +0200

    Go back to constructing two temporary sets
    Better to keep the exact semantics when porting stuff, we can change it later
    if we want. (Disregarding that I even got the boolean the wrong way around).

--- applets/kicker/plugin/runnermodel.cpp
+++ applets/kicker/plugin/runnermodel.cpp
@@ -167,7 +167,7 @@ QStringList RunnerModel::runners() const
 
 void RunnerModel::setRunners(const QStringList &runners)
 {
-    if (std::is_permutation(runners.cbegin(), runners.cend(), m_runners.cbegin(), m_runners.cend())) {
+    if (QSet<QString>(runners.cbegin(), runners.cend()) != QSet<QString>(m_runners.cbegin(), m_runners.cend())) {
         m_runners = runners;
 
         if (m_runnerManager) {
