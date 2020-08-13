577ffd6da3653061e2044a56842388eb989dd792
Bhushan Shah <bhush94@gmail.com>
2020-08-13 16:57:29 +0530

    shell: allow plasmashell process to take screenshots
    Plasma Mobile shell currently have a action in pull-down panel to take
    screenshot, without this it can not take screenshot.

--- shell/org.kde.plasmashell.desktop.cmake
+++ shell/org.kde.plasmashell.desktop.cmake
@@ -60,3 +60,4 @@ Icon=plasmashell
 NoDisplay=true
 
 X-KDE-Wayland-Interfaces=org_kde_plasma_window_management,org_kde_kwin_keystate,zkde_screencast_unstable_v1
+X-KDE-DBUS-Restricted-Interfaces=org.kde.kwin.Screenshot
