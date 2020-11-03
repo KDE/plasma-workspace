302cfef06275ca60cc4e0d48fe99ae8cb958bc3d
David Redondo <kde@david-redondo.de>
2020-11-03 15:08:30 +0100

    Remove pointless copying of data
    As far as I can tell the copied data was actually never used at least
    since importing into git.

--- dataengines/statusnotifieritem/statusnotifieritemsource.cpp
+++ dataengines/statusnotifieritem/statusnotifieritemsource.cpp
@@ -388,14 +388,6 @@ void StatusNotifierItemSource::contextMenuReady()
 
 QPixmap StatusNotifierItemSource::KDbusImageStructToPixmap(const KDbusImageStruct &image) const
 {
-    //swap from network byte order if we are little endian
-    if (QSysInfo::ByteOrder == QSysInfo::LittleEndian) {
-        uint *uintBuf = (uint *) image.data.data();
-        for (uint i = 0; i < image.data.size()/sizeof(uint); ++i) {
-            *uintBuf = ntohl(*uintBuf);
-            ++uintBuf;
-        }
-    }
     if (image.width == 0 || image.height == 0) {
         return QPixmap();
     }
