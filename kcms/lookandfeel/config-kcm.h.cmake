d64ba69cf191c43b89aeadfb1187422ada04053e
David Redondo <kde@david-redondo.de>
2020-11-19 09:40:45 +0100

    Readd searching for breeze decoration
    Was missed in move from plasma-desktop.
    We no longer write configs like
     library=org.kde.breeze
     theme=kwin4_decoration_qml_plastik
    BUG:429298

--- kcms/lookandfeel/config-kcm.h.cmake
+++ kcms/lookandfeel/config-kcm.h.cmake
@@ -1,3 +1,7 @@
 #cmakedefine HAVE_XFIXES 1
 #cmakedefine HAVE_XCURSOR 1
 #cmakedefine HAVE_BREEZE_DECO 1
+#ifdef HAVE_BREEZE_DECO
+#define BREEZE_KDECORATION_PLUGIN_ID "${BREEZE_KDECORATION_PLUGIN_ID}"
+#endif
+
