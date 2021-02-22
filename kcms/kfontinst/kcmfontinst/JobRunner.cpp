KDE/plasma-workspace

commit 567d49f55c2cd4a7dab5e5c70c688b3f3ff42203

Use QUrlQuery instead of QUrl::setQuery

kcms/kfontinst/kcmfontinst/JobRunner.cpp
@@ -103,7 +103,7 @@ QUrl CJobRunner::encode(const QString &family, quint32 style, bool system)
 {
     QUrl url(FC::encode(family, style));
 
-    url.addQueryItem("sys", system ? "true" : "false");
+    url.setQuery(QUrlQuery({{QStringLiteral("sys"), system ? QStringLiteral("true") : QStringLiteral("false")}}));
     return url;
 }
 

kcms/kfontinst/lib/Fc.cpp
@@ -49,13 +49,16 @@ namespace FC
 QUrl encode(const QString &name, quint32 style, const QString &file, int index)
 {
     QUrl url(QUrl::fromLocalFile(name));
+    QUrlQuery query;
 
     url.setScheme(FC_PROTOCOL);
-    url.addQueryItem(FC_STYLE_QUERY, QString::number(style));
+    query.addQueryItem(FC_STYLE_QUERY, QString::number(style));
     if(!file.isEmpty())
-        url.addQueryItem(FC_FILE_QUERY, file);
+        query.addQueryItem(FC_FILE_QUERY, file);
     if(index>0)
-        url.addQueryItem(FC_INDEX_QUERY, QString::number(index));
+        query.addQueryItem(FC_INDEX_QUERY, QString::number(index));
+
+    url.setQuery(query);
     return url;
 }
 

commit 99c11bbddcc9d7457bae06e5bdbf4d6a56208fc0

Fix font installation
Regression caused by 567d49f55c2cd4a7dab5e5c70c688b3f3ff42203 in a port
to QUrl::setQuery(QUrlQuery) from QUrl::addQueryItem

The bug is that FC::encode may return a URL with a query already set. We
want to append not replace.

kcms/kfontinst/kcmfontinst/JobRunner.cpp
@@ -102,7 +102,9 @@ QUrl CJobRunner::encode(const QString &family, quint32 style, bool system)
 {
     QUrl url(FC::encode(family, style));
 
-    url.setQuery(QUrlQuery({{QStringLiteral("sys"), system ? QStringLiteral("true") : QStringLiteral("false")}}));
+    QUrlQuery query(url);
+    query.addQueryItem(QStringLiteral("sys"), system ? QStringLiteral("true") : QStringLiteral("false"));
+    url.setQuery(query);
     return url;
 }
 