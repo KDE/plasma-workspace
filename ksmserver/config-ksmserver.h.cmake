e2f5b3c8acbbb8368ccee932715f1cce6421f928
Ahmad Samir <a.samirh78@gmail.com>
2021-03-06 16:11:01 +0200

    Clean up CMake variables
    - Port away from cmake macro macro_bool_to_01, which was being used from
    the copy of that macro that KDELibs4Support has
    - Clean up redundant CMake variables

--- ksmserver/config-ksmserver.h.cmake
+++ ksmserver/config-ksmserver.h.cmake
@@ -1,5 +1,3 @@
 /* Define to 1 if you have the `_IceTransNoListen' function. */
 #cmakedefine HAVE__ICETRANSNOLISTEN 1
 #define LOGOUT_GREETER_BIN "${CMAKE_INSTALL_FULL_LIBEXECDIR}/ksmserver-logout-greeter"
-
-#define KWIN_BIN "${KWIN_BIN}"
--- shell/config-plasma.h.cmake
+++ shell/config-plasma.h.cmake
@@ -1,3 +1,1 @@
 #cmakedefine01 HAVE_X11
-#cmakedefine01 HAVE_GLX
-#cmakedefine01 HAVE_EGL
