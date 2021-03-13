23a830244bebab8637dc0c42271674f3410fb957
Albert Astals Cid <aacid@kde.org>
2021-03-13 19:07:41 +0100

    Don't extract QML as Java
    Doing so causes some trouble for our translators since %1% is marked as
    "this is a java printf" and then they can't translate it properly since
    well, this is not really a java printf
    
    (cherry picked from commit 2d6b8da162e3a30880a12bb2ffdfcd48cde621f4)

--- lookandfeel/Messages.sh
+++ lookandfeel/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.qml` `find ../sddm-theme -name \*.qml` -L Java -o $podir/plasma_lookandfeel_org.kde.lookandfeel.pot
+$XGETTEXT `find . -name \*.qml` `find ../sddm-theme -name \*.qml` -o $podir/plasma_lookandfeel_org.kde.lookandfeel.pot
