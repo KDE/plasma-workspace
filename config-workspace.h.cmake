8df0b1695df742971305a67693d2bc02b78c0216
Nicolas Fella <nicolas.fella@gmx.de>
2021-03-22 02:18:00 +0100

    Remove KDE_BINDIR definition
    It serves no purpose

--- config-workspace.h.cmake
+++ config-workspace.h.cmake
@@ -6,9 +6,6 @@
 /* Define to 1 if you have the <sys/time.h> header file. */
 #cmakedefine HAVE_SYS_TIME_H 1
 
-/* KDE's binaries directory */
-#define KDE_BINDIR "${KDE_INSTALL_BINDIR}"
-
 /** place where plasma-frameworks things are installed */
 #define PLASMA_RELATIVE_DATA_INSTALL_DIR "@PLASMA_RELATIVE_DATA_INSTALL_DIR@"
 
