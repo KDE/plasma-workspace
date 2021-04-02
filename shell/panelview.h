e6fe8b0e9330a3c49562740fcd41b2e8fc561dd4
Ahmad Samir <a.samirh78@gmail.com>
2021-04-02 21:29:33 +0200

    Remove extra '/' in comment block
    GIT_SILENT

--- shell/panelview.h
+++ shell/panelview.h
@@ -108,7 +108,7 @@ class PanelView : public PlasmaQuick::ContainmentView
     Q_PROPERTY(OpacityMode opacityMode READ opacityMode WRITE setOpacityMode NOTIFY opacityModeChanged)
 
     /**
-    /*  Property that determines whether adaptive opacity is used.
+     *  Property that determines whether adaptive opacity is used.
      */
     Q_PROPERTY(bool adaptiveOpacityEnabled READ adaptiveOpacityEnabled NOTIFY adaptiveOpacityEnabledChanged)
 
