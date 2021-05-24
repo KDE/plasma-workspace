1206651a385d48bea4e7c8ae2c7f9ae8b28e73b0
Nate Graham <nate@kde.org>
2021-05-24 12:18:24 -0600

    Add inline comment to install-sessions-sh
    GIT_SILENT

--- login-sessions/install-sessions.sh.cmake
+++ login-sessions/install-sessions.sh.cmake
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+# Make built-from-source sessions appear in login screen
 sudo install @CMAKE_CURRENT_BINARY_DIR@/plasmax11-dev.desktop /usr/share/xsessions/
 sudo install @CMAKE_CURRENT_BINARY_DIR@/plasmawayland-dev.desktop /usr/share/wayland-sessions/
 install @CMAKE_BINARY_DIR@/prefix.sh @CMAKE_INSTALL_FULL_LIBEXECDIR@/plasma-dev-prefix.sh
