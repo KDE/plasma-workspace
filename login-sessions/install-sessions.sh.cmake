02459d3e1c20fc0b9c503e84d182f1504bd26a74
Nate Graham <nate@kde.org>
2021-05-24 12:20:08 -0600

    Set up DBus stuff properly in install-sessions.sh script
    This is an extremely fiddly procedure well-suited for being automated
    rather than documented.
    
    The approach taken here should work for all distros, even those that
    freak out if system DBus files are modified or symlinked into a home
    directory.
    
    BUG: 425272

--- login-sessions/install-sessions.sh.cmake
+++ login-sessions/install-sessions.sh.cmake
@@ -6,3 +6,23 @@ sudo install @CMAKE_CURRENT_BINARY_DIR@/plasmax11-dev.desktop /usr/share/xsessio
 sudo install @CMAKE_CURRENT_BINARY_DIR@/plasmawayland-dev.desktop /usr/share/wayland-sessions/
 install @CMAKE_BINARY_DIR@/prefix.sh @CMAKE_INSTALL_FULL_LIBEXECDIR@/plasma-dev-prefix.sh
 install @CMAKE_CURRENT_BINARY_DIR@/startplasma-dev.sh @CMAKE_INSTALL_FULL_LIBEXECDIR@
+
+# Make the system DBus able to see any new DBus files that have been added to
+# the built-from-source plasma session which are not yet present in the system
+# DBus locations. We copy the built-from-source DBus files rather than
+# symlinking them because some distros have security policies which prevents
+# this from working.
+sudo mkdir -p /opt/kde-dbus-scripts/
+sudo cp -r @KDE_INSTALL_FULL_DBUSDIR@/* /opt/kde-dbus-scripts/
+if [ ! -f /etc/dbus-1/session-local.conf ]
+then
+    cat > session-local.conf << EOF
+<busconfig>
+	<servicedir>/opt/kde-dbus-scripts/services</servicedir>
+	<servicedir>/opt/kde-dbus-scripts/system-services</servicedir>
+	<includedir>/opt/kde-dbus-scripts/system.d/</includedir>
+	<includedir>/opt/kde-dbus-scripts/interfaces/</includedir>
+</busconfig>
+EOF
+    sudo mv session-local.conf /etc/dbus-1/
+fi
