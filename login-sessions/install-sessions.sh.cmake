d0de8b9972d9a9c16b0238b3981b5ebe9431e17a
Nate Graham <nate@kde.org>
2021-05-26 17:45:06 +0000

    Clarify DBus message in install-sessions.sh script

--- login-sessions/install-sessions.sh.cmake
+++ login-sessions/install-sessions.sh.cmake
@@ -9,9 +9,9 @@ install @CMAKE_CURRENT_BINARY_DIR@/startplasma-dev.sh @CMAKE_INSTALL_FULL_LIBEXE
 
 # Make the system DBus able to see any new DBus files that have been added to
 # the built-from-source plasma session which are not yet present in the system
-# DBus locations. We copy the built-from-source DBus files rather than
-# symlinking them because some distros have security policies which prevents
-# this from working.
+# DBus locations. Because some distros have security policies which prevent the
+# use of DBus files in a user's homedir, and even symlinks outside,
+# we have to copy the files into a system-owned location.
 sudo mkdir -p /opt/kde-dbus-scripts/
 sudo cp -r @KDE_INSTALL_FULL_DBUSDIR@/* /opt/kde-dbus-scripts/
 if [ ! -f /etc/dbus-1/session-local.conf ]
