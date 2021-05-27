d03db2ffd8d66051f16079e2217257db61b767a8
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2021-05-27 15:32:19 +0200

    Fix license identifier statement

--- applets/digital-clock/plugin/applicationintegration.cpp
+++ applets/digital-clock/plugin/applicationintegration.cpp
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2021 Carl Schwan <carlschwan@kde.org>
-// SPDX-Licence-Identifier: SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
 #include "applicationintegration.h"
 #include <KIO/ApplicationLauncherJob>
