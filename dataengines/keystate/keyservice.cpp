1e93f9ae15ff663b799e3725dc16833e404c0552
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-06-29 21:08:31 +0200

    Convert keyservice.cpp license header to SPDX

--- dataengines/keystate/keyservice.cpp
+++ dataengines/keystate/keyservice.cpp
@@ -1,20 +1,7 @@
 /*
     SPDX-FileCopyrightText: 2009 Aaron Seigo <aseigo@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    This library is free software; you can redistribute it and/or
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor,
-    Boston, MA  02110-1301  USA
+    SPDX-License-Identifier: LGPL-2.1-or-later
 */
 
 #include "keyservice.h"
