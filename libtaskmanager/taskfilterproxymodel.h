f71419cd17448df3d9be9455816fb75a610957a9
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-06-29 19:34:43 +0200

    Correct email address of Eike Hein

--- libtaskmanager/taskfilterproxymodel.h
+++ libtaskmanager/taskfilterproxymodel.h
@@ -1,5 +1,5 @@
 /*
-SPDX-FileCopyrightText: 2016 Eike Hein <hein.org>
+SPDX-FileCopyrightText: 2016 Eike Hein <hein@kde.org>
 
 SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
