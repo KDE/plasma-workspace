02f1a797a8bf911099828e933b97be832e6e07ca
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-07-02 16:52:48 +0200

    Correct years and mail addresses in copyright statements

--- applets/kicker/plugin/autotests/qmltest.cpp
+++ applets/kicker/plugin/autotests/qmltest.cpp
@@ -1,5 +1,5 @@
 /*
-    SPDX-FileCopyrightText: 2020 David Edmundson <ahiemstra@heimr.nl>
+    SPDX-FileCopyrightText: 2020 David Edmundson <kde@davidedmundson.co.uk>
 
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
