7956d7b2e18133b92ec5d6cab674b1390daac9b8
David Edmundson <kde@davidedmundson.co.uk>
2021-10-18 18:36:41 +0100

    [shell] Ignore placeholder screens
    A placeholder screen is created by Qt when no real screens exist. We
    don't want to create panels and containments for those, it is a whole
    world of pain.
    
    Plasma has a hook to ignore some screens which is extended with this
    check.
    
    CCBUG: 419492
    
    
    (cherry picked from commit 13fb70fec16efca4f54788cd9c87c8ece4700d03)

--- shell/shellcorona.cpp
+++ shell/shellcorona.cpp
@@ -1126,6 +1126,9 @@ bool ShellCorona::isOutputRedundant(QScreen *screen) const
         if (screen == s) {
             continue;
         }
+        if (s->geometry().isNull()) {
+            return false;
+        }
 
         const QRect otherGeometry = s->geometry();
 
