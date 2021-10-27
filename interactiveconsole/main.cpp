63bea079c5d614e612ae5e2cafd7f6e8b1aae5a9
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-10-27 18:14:45 +0200

    interactiveconsole: Allow specifying mode from command line parameter
    This is needed for the runner, which has two different keywords for the two scripting modes
    
    CCBUG: 444366

--- interactiveconsole/main.cpp
+++ interactiveconsole/main.cpp
@@ -1,16 +1,37 @@
 /*
     SPDX-FileCopyrightText: 2021 David Edmundson <davidedmundson@kde.org>
+    SPDX-FileCopyrightText: 2021 Alexander Lohnau <alexander.lohnau@gmx.de>
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #include "interactiveconsole.h"
 #include <QApplication>
+#include <QCommandLineParser>
+#include <QDebug>
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+    InteractiveConsole::ConsoleMode mode = InteractiveConsole::PlasmaConsole;
+
+    QCommandLineParser parser;
+    QCommandLineOption plasmaOpt(QStringLiteral("plasma"));
+    QCommandLineOption kwinOpt(QStringLiteral("kwin"));
+    parser.addOption(plasmaOpt);
+    parser.addOption(kwinOpt);
+    parser.addHelpOption();
+    parser.process(app);
+
+    if (parser.isSet(kwinOpt)) {
+        mode = InteractiveConsole::KWinConsole;
+    } else if (parser.isSet(plasmaOpt)) {
+        mode = InteractiveConsole::PlasmaConsole;
+    } else if (parser.isSet(plasmaOpt) && parser.isSet(kwinOpt)) {
+        qWarning() << "Only one mode can be specified when launching the interactive console";
+        exit(1);
+    }
     // set to delete on close
-    auto console = new InteractiveConsole;
+    auto console = new InteractiveConsole(mode);
     console->show();
     app.exec();
 }
