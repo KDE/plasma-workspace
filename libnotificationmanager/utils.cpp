25f4cce3bf81ed8412e5053eeddd22d4366fe6ab
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-11-10 19:12:00 +0100

    Port away from deprecated QConcatenateTablesProxyModel

--- libnotificationmanager/utils.cpp
+++ libnotificationmanager/utils.cpp
@@ -10,6 +10,7 @@
 
 #include <QAbstractItemModel>
 #include <QAbstractProxyModel>
+#include <QConcatenateTablesProxyModel>
 #include <QCoreApplication>
 #include <QDBusConnection>
 #include <QDBusConnectionInterface>
@@ -18,8 +19,6 @@
 #include <QMetaEnum>
 #include <QSettings>
 #include <QTextStream>
-
-#include <KConcatenateRowsProxyModel>
 
 #include <KProcessList>
 
@@ -109,8 +108,8 @@ QModelIndex Utils::mapToModel(const QModelIndex &idx, const QAbstractItemModel *
     while (resolvedIdx.isValid() && resolvedIdx.model() != sourceModel) {
         if (auto *proxyModel = qobject_cast<const QAbstractProxyModel *>(resolvedIdx.model())) {
             resolvedIdx = proxyModel->mapToSource(resolvedIdx);
-            // KConcatenateRowsProxyModel isn't a "real" proxy model, so we need to special case for it :(
-        } else if (auto *concatenateModel = qobject_cast<const KConcatenateRowsProxyModel *>(resolvedIdx.model())) {
+            // QConcatenateTablesProxyModel isn't a "real" proxy model, so we need to special case for it :(
+        } else if (auto *concatenateModel = qobject_cast<const QConcatenateTablesProxyModel *>(resolvedIdx.model())) {
             resolvedIdx = concatenateModel->mapToSource(resolvedIdx);
         } else {
             if (resolvedIdx.model() != sourceModel) {
--- libtaskmanager/concatenatetasksproxymodel.cpp
+++ libtaskmanager/concatenatetasksproxymodel.cpp
@@ -9,7 +9,7 @@
 namespace TaskManager
 {
 ConcatenateTasksProxyModel::ConcatenateTasksProxyModel(QObject *parent)
-    : KConcatenateRowsProxyModel(parent)
+    : QConcatenateTablesProxyModel(parent)
 {
 }
 
