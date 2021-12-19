079552b315752d775d0d9e250e0651ba1cb0096c
David Edmundson <kde@davidedmundson.co.uk>
2021-12-19 08:14:58 +0000

    [libnotificationmanager] Fix memory leak
    In an &operator= we already have an object. It will have the d-pointer
    already allocated.
    
    If we create a new private implementation our old one leaks

--- libnotificationmanager/notification.cpp
+++ libnotificationmanager/notification.cpp
@@ -449,7 +449,7 @@ Notification::Notification(Notification &&other) noexcept
 
 Notification &Notification::operator=(const Notification &other)
 {
-    d = new Private(*other.d);
+    *d = *other.d;
     return *this;
 }
 
