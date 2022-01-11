KDE/plasma-workspace

commit f9e39815dcf64c6b425737b51257b46cd400d5db

applets/batterymonitor: Improve for desktop systems
As it was so far, it looked like the system was malfunctioning because
we were showing a battery with a big fat red mark. If there's no battery
we can show the default icon.

As for the name, we only say "battery and brightness" when there's
both of them. Otherwise we show either title.

If neither batteries nor brightness are available, we mark the applet as
passive so people can at least still access the power management blocking
checkbox which lives here.

BUG: 415073
FIXED-IN: 5.24

applets/batterymonitor/package/contents/ui/main.qml
@@ -20,6 +20,9 @@ Item {
     id: batterymonitor
     Plasmoid.switchWidth: PlasmaCore.Units.gridUnit * 10
     Plasmoid.switchHeight: PlasmaCore.Units.gridUnit * 10
+    Plasmoid.title: (hasBatteries === hasBrightness ? i18n("Battery and Brightness") :
+                                      hasBrightness ? i18n("Brightness") :
+                                      hasBatteries ? i18n("Battery") : "")
 
     LayoutMirroring.enabled: Qt.application.layoutDirection == Qt.RightToLeft
     LayoutMirroring.childrenInherit: true
@@ -37,8 +40,11 @@ Item {
         return PlasmaCore.Types.PassiveStatus;
     }
 
+    readonly property bool hasBatteries: batteries.count > 0 && pmSource.data["Battery"]["Has Cumulative"]
+    readonly property bool hasBrightness: isBrightnessAvailable || isKeyboardBrightnessAvailable
+
     Plasmoid.toolTipMainText: {
-        if (batteries.count === 0 || !pmSource.data["Battery"]["Has Cumulative"]) {
+        if (!hasBatteries) {
             return plasmoid.title
         } else if (pmSource.data["Battery"]["State"] === "FullyCharged") {
             return i18n("Fully Charged");
@@ -87,7 +93,7 @@ Item {
         return parts.join("\n");
     }
 
-    Plasmoid.icon: "battery"
+    Plasmoid.icon: !hasBatteries && hasBrightness ? "high-brightness" : "battery"
 
     property bool disableBrightnessUpdate: true
 
@@ -212,8 +218,15 @@ Item {
         }
     }
 
-    Plasmoid.compactRepresentation: CompactRepresentation {}
+    Component {
+        id: batteryCompact
+        CompactRepresentation {}
+    }
+    Plasmoid.compactRepresentation: hasBatteries ? batteryCompact : null
 
+
+    readonly property bool isBrightnessAvailable: pmSource.data["PowerDevil"] && pmSource.data["PowerDevil"]["Screen Brightness Available"] ? true : false
+    readonly property bool isKeyboardBrightnessAvailable: pmSource.data["PowerDevil"] && pmSource.data["PowerDevil"]["Keyboard Brightness Available"] ? true : false
     Plasmoid.fullRepresentation: PopupDialog {
         id: dialogItem
         Layout.minimumWidth: PlasmaCore.Units.iconSizes.medium * 9
@@ -223,8 +236,8 @@ Item {
         model: plasmoid.expanded ? batteries : null
         focus: true
 
-        isBrightnessAvailable: pmSource.data["PowerDevil"] && pmSource.data["PowerDevil"]["Screen Brightness Available"] ? true : false
-        isKeyboardBrightnessAvailable: pmSource.data["PowerDevil"] && pmSource.data["PowerDevil"]["Keyboard Brightness Available"] ? true : false
+        isBrightnessAvailable: batterymonitor.isBrightnessAvailable
+        isKeyboardBrightnessAvailable: batterymonitor.isKeyboardBrightnessAvailable
 
         pluggedIn: pmSource.data["AC Adapter"] !== undefined && pmSource.data["AC Adapter"]["Plugged in"]
         remainingTime: batterymonitor.remainingTime

commit 471cc3745b3a51a8b427fc1b44a22de228cbd945

applets/batterymonitor: fix CompactRepresentation
f9e39815dcf64c6b425737b51257b46cd400d5db introduced a few regressions by
blowing away the custom CompactRepresentation when there are no
batteries. This introduced a race condition that sometimes set the icon
incorrectly and also broke scrolling on the applet to change the volume.

This commit fixes those issues by using the existing infrastructure for
the CompactPepresentation, but simply swapping out its custom battery
icon for normal icon when there are no batteries.

applets/batterymonitor/package/contents/ui/CompactRepresentation.qml
@@ -18,6 +18,7 @@ MouseArea {
     property real itemSize: Math.min(root.height, root.width/view.count)
     readonly property bool isConstrained: plasmoid.formFactor === PlasmaCore.Types.Vertical || plasmoid.formFactor === PlasmaCore.Types.Horizontal
     property real brightnessError: 0
+    property bool hasBatteries: true
 
     onClicked: plasmoid.expanded = !plasmoid.expanded
 
@@ -44,8 +45,17 @@ MouseArea {
         batterymonitor.screenBrightness = Math.max(minimumBrightness, Math.min(maximumBrightness, newBrightness));
     }
 
+    // "No Batteries" case
+    PlasmaCore.IconItem {
+        anchors.fill: parent
+        visible: !root.hasBatteries
+        source: plasmoid.icon
+    }
+
+    // We have any batteries; show their status
     //Should we consider turning this into a Flow item?
     Row {
+        visible: root.hasBatteries
         anchors.centerIn: parent
         Repeater {
             id: view

applets/batterymonitor/package/contents/ui/main.qml
@@ -219,11 +219,9 @@ Item {
         }
     }
 
-    Component {
-        id: batteryCompact
-        CompactRepresentation {}
+    Plasmoid.compactRepresentation: CompactRepresentation {
+        hasBatteries: batterymonitor.hasBatteries
     }
-    Plasmoid.compactRepresentation: hasBatteries ? batteryCompact : null
 
 
     readonly property bool isBrightnessAvailable: pmSource.data["PowerDevil"] && pmSource.data["PowerDevil"]["Screen Brightness Available"] ? true : false