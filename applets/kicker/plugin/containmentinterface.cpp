3c2ad999901db5126121e34e2d9537f75e84cb4e
Alexander Lohnau <alexander.lohnau@gmx.de>
2022-01-27 22:08:34 +0100

    Fix wrong arg type in addLauncher method call
    We expect a QVariant instead of a QUrl
    
    Regression from 14d10c7a05be33f31214195369e069d40f547437
    
    BUG: 449243
    
    
    (cherry picked from commit 4f285e5d0b499e8d4b058a01c6e23c2437260de5)

--- applets/kicker/plugin/containmentinterface.cpp
+++ applets/kicker/plugin/containmentinterface.cpp
@@ -134,7 +134,7 @@ void ContainmentInterface::addLauncher(QObject *appletInterface, ContainmentInte
                 return;
             }
 
-            QMetaObject::invokeMethod(rootItem, "addLauncher", Q_ARG(QUrl, QUrl::fromLocalFile(entryPath)));
+            QMetaObject::invokeMethod(rootItem, "addLauncher", Q_ARG(QVariant, QUrl::fromLocalFile(entryPath)));
         } else {
             containment->createApplet(QStringLiteral("org.kde.plasma.icon"), QVariantList() << entryPath);
         }
