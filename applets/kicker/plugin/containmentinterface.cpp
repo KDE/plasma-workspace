KDE/plasma-workspace

commit 14d10c7a05be33f31214195369e069d40f547437

[applets/kicker] Port away from Qt's foreach & refactor child object retrieval
Qt's foreach is deprecated and should be replaced with modern C++
range-for loops. Const qualifiers are added to variable declarations
and wrapper in std::as_const as required to prevent unnecessary
clones/detach.

applets/kicker/plugin/containmentinterface.cpp
@@ -72,21 +72,19 @@ bool ContainmentInterface::mayAddLauncher(QObject *appletInterface, ContainmentI
         if (!entryPath.isEmpty() && containment->pluginMetaData().pluginId() == QLatin1String("org.kde.panel")) {
             const Plasma::Applet *taskManager = findTaskManagerApplet(containment);
 
-            if (taskManager) {
-                QQuickItem *gObj = qobject_cast<QQuickItem *>(taskManager->property("_plasma_graphicObject").value<QObject *>());
-
-                if (!gObj || !gObj->childItems().count()) {
-                    return false;
-                }
-
-                QQuickItem *rootItem = gObj->childItems().first();
-
-                QVariant ret;
-
-                QMetaObject::invokeMethod(rootItem, "hasLauncher", Q_RETURN_ARG(QVariant, ret), Q_ARG(QVariant, QUrl::fromLocalFile(entryPath)));
-
-                return !ret.toBool();
+            if (!taskManager) {
+                return false;
             }
+
+            QQuickItem *rootItem = firstPlasmaGraphicObjectChild(taskManager);
+
+            if (!rootItem) {
+                return false;
+            }
+
+            QVariant ret;
+            QMetaObject::invokeMethod(rootItem, "hasLauncher", Q_RETURN_ARG(QVariant, ret), Q_ARG(QVariant, QUrl::fromLocalFile(entryPath)));
+            return !ret.toBool();
         }
 
         break;
@@ -126,25 +124,15 @@ void ContainmentInterface::addLauncher(QObject *appletInterface, ContainmentInte
         const QStringList &containmentProvides = containment->pluginMetaData().value(QStringLiteral("X-Plasma-Provides"), QStringList());
 
         if (containmentProvides.contains(QLatin1String("org.kde.plasma.filemanagement"))) {
-            QQuickItem *gObj = qobject_cast<QQuickItem *>(containment->property("_plasma_graphicObject").value<QObject *>());
+            QQuickItem *rootItem = findPlasmaGraphicObjectChildIf(containment, [](QQuickItem *item) {
+                return item->objectName() == QLatin1String("folder");
+            });
 
-            if (!gObj || !gObj->childItems().count()) {
+            if (!rootItem) {
                 return;
             }
 
-            QQuickItem *rootItem = nullptr;
-
-            foreach (QQuickItem *item, gObj->childItems()) {
-                if (item->objectName() == QLatin1String("folder")) {
-                    rootItem = item;
-
-                    break;
-                }
-            }
-
-            if (rootItem) {
-                QMetaObject::invokeMethod(rootItem, "addLauncher", Q_ARG(QVariant, QUrl::fromLocalFile(entryPath)));
-            }
+            QMetaObject::invokeMethod(rootItem, "addLauncher", Q_ARG(QUrl, QUrl::fromLocalFile(entryPath)));
         } else {
             containment->createApplet(QStringLiteral("org.kde.plasma.icon"), QVariantList() << entryPath);
         }
@@ -162,17 +150,17 @@ void ContainmentInterface::addLauncher(QObject *appletInterface, ContainmentInte
         if (containment->pluginMetaData().pluginId() == QLatin1String("org.kde.panel")) {
             const Plasma::Applet *taskManager = findTaskManagerApplet(containment);
 
-            if (taskManager) {
-                QQuickItem *gObj = qobject_cast<QQuickItem *>(taskManager->property("_plasma_graphicObject").value<QObject *>());
-
-                if (!gObj || !gObj->childItems().count()) {
-                    return;
-                }
-
-                QQuickItem *rootItem = gObj->childItems().first();
-
-                QMetaObject::invokeMethod(rootItem, "addLauncher", Q_ARG(QVariant, QUrl::fromLocalFile(entryPath)));
+            if (!taskManager) {
+                return;
             }
+
+            QQuickItem *rootItem = firstPlasmaGraphicObjectChild(taskManager);
+
+            if (!rootItem) {
+                return;
+            }
+
+            QMetaObject::invokeMethod(rootItem, "addLauncher", Q_ARG(QVariant, QUrl::fromLocalFile(entryPath)));
         }
 
         break;
@@ -218,6 +206,27 @@ void ContainmentInterface::ensureMutable(Plasma::Containment *containment)
     if (containment && containment->immutability() != Plasma::Types::Mutable) {
         containment->actions()->action(QStringLiteral("lock widgets"))->trigger();
     }
+}
+
+template<class UnaryPredicate>
+QQuickItem *ContainmentInterface::findPlasmaGraphicObjectChildIf(const Plasma::Applet *applet, UnaryPredicate predicate)
+{
+    QQuickItem *gObj = qobject_cast<QQuickItem *>(applet->property("_plasma_graphicObject").value<QObject *>());
+
+    if (!gObj) {
+        return nullptr;
+    }
+
+    const QList<QQuickItem *> children = gObj->childItems();
+    const auto found = std::find_if(children.cbegin(), children.cend(), predicate);
+    return found != children.cend() ? *found : nullptr;
+}
+
+QQuickItem *ContainmentInterface::firstPlasmaGraphicObjectChild(const Plasma::Applet *applet)
+{
+    return findPlasmaGraphicObjectChildIf(applet, [](QQuickItem *) {
+        return true;
+    });
 }
 
 Plasma::Applet *ContainmentInterface::findTaskManagerApplet(Plasma::Containment *containment)

applets/kicker/plugin/containmentinterface.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <QObject>
+#include <QQuickItem>
 #include <QUrl>
 
 namespace Plasma
@@ -40,6 +41,10 @@ public:
     static Q_INVOKABLE void ensureMutable(Plasma::Containment *containment);
 
 private:
+    template<class UnaryPredicate>
+    static QQuickItem *findPlasmaGraphicObjectChildIf(const Plasma::Applet *applet, UnaryPredicate predicate);
+    static QQuickItem *firstPlasmaGraphicObjectChild(const Plasma::Applet *applet);
+
     static Plasma::Applet *findTaskManagerApplet(Plasma::Containment *containment);
     static QStringList m_knownTaskManagers;
 };

commit 4f285e5d0b499e8d4b058a01c6e23c2437260de5

Fix wrong arg type in addLauncher method call
We expect a QVariant instead of a QUrl

Regression from 14d10c7a05be33f31214195369e069d40f547437

BUG: 449243

applets/kicker/plugin/containmentinterface.cpp
@@ -132,7 +132,7 @@ void ContainmentInterface::addLauncher(QObject *appletInterface, ContainmentInte
                 return;
             }
 
-            QMetaObject::invokeMethod(rootItem, "addLauncher", Q_ARG(QUrl, QUrl::fromLocalFile(entryPath)));
+            QMetaObject::invokeMethod(rootItem, "addLauncher", Q_ARG(QVariant, QUrl::fromLocalFile(entryPath)));
         } else {
             containment->createApplet(QStringLiteral("org.kde.plasma.icon"), QVariantList() << entryPath);
         }