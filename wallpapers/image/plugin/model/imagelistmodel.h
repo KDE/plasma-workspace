d4a1c07a8105660cd8612ad2cc2b565575a38d7c
Fushan Wen <qydwhotmail@gmail.com>
2022-04-09 23:13:50 +0800

    wallpapers/image: Add ImageListModel
    List images that do not belong to any KPackages.

---
+++ wallpapers/image/plugin/model/imagelistmodel.h
@@ -0,0 +1,44 @@
+/*
+    SPDX-FileCopyrightText: 2007 Paolo Capriotti <p.capriotti@gmail.com>
+    SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#ifndef IMAGELISTMODEL_H
+#define IMAGELISTMODEL_H
+
+#include "abstractimagelistmodel.h"
+
+/**
+ * List images that do not belong to any KPackages.
+ */
+class ImageListModel : public AbstractImageListModel
+{
+    Q_OBJECT
+
+public:
+    explicit ImageListModel(const QSize &targetSize, QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
+
+    int indexOf(const QString &path) const override;
+
+    void load(const QStringList &customPaths = {}) override;
+
+public Q_SLOTS:
+    QStringList addBackground(const QString &path) override;
+    QStringList removeBackground(const QString &path) override;
+
+protected Q_SLOTS:
+    void slotHandleImageFound(const QStringList &paths);
+
+private:
+    QStringList m_data;
+
+    friend class ImageListModelTest;
+};
+
+#endif // IMAGELISTMODEL_H
