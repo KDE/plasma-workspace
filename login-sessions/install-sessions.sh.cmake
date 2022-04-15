5da3d758101d90dbd7cc3ce3d0be0f871c0689aa
Andrey Butirsky <butirsky@gmail.com>
2022-04-15 14:13:23 +0300

    install-sessions.sh: do not pollute system location
    - /usr/local/.. is a better place for custom .desktop files
    - target file doesn't need to be executable

--- login-sessions/install-sessions.sh.cmake
+++ login-sessions/install-sessions.sh.cmake
@@ -2,8 +2,8 @@
 set -e
 
 # Make built-from-source sessions appear in login screen
-sudo install @CMAKE_CURRENT_BINARY_DIR@/plasmax11-dev.desktop /usr/share/xsessions/
-sudo install @CMAKE_CURRENT_BINARY_DIR@/plasmawayland-dev.desktop /usr/share/wayland-sessions/
+sudo install --mode=644 -D --target-directory=/usr/local/share/xsessions @CMAKE_CURRENT_BINARY_DIR@/plasmax11-dev.desktop
+sudo install --mode=644 -D --target-directory=/usr/local/share/wayland-sessions @CMAKE_CURRENT_BINARY_DIR@/plasmawayland-dev.desktop
 install @CMAKE_BINARY_DIR@/prefix.sh @CMAKE_INSTALL_FULL_LIBEXECDIR@/plasma-dev-prefix.sh
 install @CMAKE_CURRENT_BINARY_DIR@/startplasma-dev.sh @CMAKE_INSTALL_FULL_LIBEXECDIR@
 
