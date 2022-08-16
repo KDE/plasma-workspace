KDE/plasma-workspace

commit e64c13729bd1e2de43689b310caafac62f748c87

Fix systemmonitor preferences being reset
See https://invent.kde.org/frameworks/plasma-framework/-/commit/3078bc25dfe49bb627ae9a65cd18020054a5950e#note_497421

BUG: 454004


(cherry picked from commit 262bf0af8479f04fdbc41e4c8c2a11dcd403564f)

applets/systemmonitor/systemmonitor/systemmonitor.cpp
@@ -86,6 +86,6 @@ void SystemMonitor::openSystemMonitor()
     job->start();
 }
 
-K_PLUGIN_CLASS(SystemMonitor)
+K_PLUGIN_CLASS_WITH_JSON(SystemMonitor, "package/metadata.json")
 
 #include "systemmonitor.moc"

commit 870ecbef9e05b6e06bb1b06a63bcb928cc609ab3

Take the pluginId from the kpackage metadata
https://invent.kde.org/plasma/plasma-workspace/-/commit/e64c13729bd1e2de43689b310caafac62f748c87
caused pluginMetadata() to be from parent applet, losing a way to load
the proper preset

BUG:457657

applets/systemmonitor/systemmonitor/systemmonitor.cpp
@@ -42,9 +42,9 @@ void SystemMonitor::init()
 {
     configChanged();
 
-    // NOTE: taking the pluginId this way, we take it from the child applet (cpu monitor, memory, whatever) rather than the parent fallback applet
-    // (systemmonitor)
-    const QString pluginId = pluginMetaData().pluginId();
+    // NOTE: taking the pluginId from the package instead of applet metadata, we take it from the child applet (cpu monitor, memory, whatever) rather than the
+    // parent fallback applet (systemmonitor)
+    const QString pluginId = kPackage().metadata().pluginId();
 
     // FIXME HACK: better way to get the engine At least AppletQuickItem should have an engine() getter
     KDeclarative::QmlObjectSharedEngine *qmlObject = new KDeclarative::QmlObjectSharedEngine();