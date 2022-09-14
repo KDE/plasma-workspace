KDE/plasma-workspace

commit aa10c402f213269094532ee516b48951fe5b9066

delay ksplash until after env is set up
otherwise we can dbus invoke with the wrong environment. specifically
this happens with the latest qtbase changes that introduced color
picking support on wayland. when we start a qguiapplication with
incomplete environment that dbus invokes the xdg-portal system and that
in turn has an incomplete environment resulting in theming and the likes
not properly applying because the portal doesn't know that it runs
inside a plasma session.

https://invent.kde.org/qt/qt/qtbase/-/commit/2dc083df009a45c5dacfea27b0affeb85b01f847

BUG: 458865
(cherry picked from commit 9bf0e56da84de5e9bd2b3ff28bdb2cb1af6de91e)

startkde/startplasma-x11.cpp
@@ -57,9 +57,6 @@ int main(int argc, char **argv)
         }
     }
 
-    setupCursor(false);
-    QScopedPointer<QProcess, KillBeforeDeleter> ksplash(setupKSplash());
-
     runEnvironmentScripts();
 
     out << "startkde: Starting up...\n";
@@ -78,6 +75,11 @@ int main(int argc, char **argv)
     // Otherwise it may leads to some unwanted order of applying environment
     // variables (e.g. LANG and LC_*)
     importSystemdEnvrionment();
+
+    // NOTE: Do not start QGuiApplications before setting up the environment. We'd be at risk of dbus invoking other
+    // processes with an incomplete environment.
+    setupCursor(false);
+    std::unique_ptr<QProcess, KillBeforeDeleter> ksplash(setupKSplash());
 
     if (!startPlasmaSession(false))
         return 1;

commit b03a2bc6dd5d752338edbddca738f2ada595786e

restore previous scope pointer
broke in aa10c402f213269094532ee516b48951fe5b9066

startkde/startplasma-x11.cpp
@@ -79,7 +79,7 @@ int main(int argc, char **argv)
     // NOTE: Do not start QGuiApplications before setting up the environment. We'd be at risk of dbus invoking other
     // processes with an incomplete environment.
     setupCursor(false);
-    std::unique_ptr<QProcess, KillBeforeDeleter> ksplash(setupKSplash());
+    QScopedPointer<QProcess, KillBeforeDeleter> ksplash(setupKSplash());
 
     if (!startPlasmaSession(false))
         return 1;