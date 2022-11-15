6d590b32f8e46ee42a01fe51b33ecabe5bc44ca8
Nate Graham <nate@kde.org>
2022-11-15 14:28:01 -0700

    Only consider screens to be mirrored if they are completely overlapping
    Right now libnotificationmanager considers screens to be mirrored--and
    hence automatically enables Do Not Disturb Mode--when they are
    overlapping at all. But this doesn't match KScreen's definition of when
    screens are mirrored, which is that one screen is *completely* covered
    up by the other. Let's use that same logic here so we're not enabling
    DND mode when screens are simply overlapping a bit, which is a valid if
    unusual configuration in KScreen.

--- libnotificationmanager/mirroredscreenstracker.cpp
+++ libnotificationmanager/mirroredscreenstracker.cpp
@@ -75,9 +75,9 @@ void MirroredScreensTracker::checkScreensMirrored()
                 continue;
             }
 
-            if (output->geometry().intersects(checkOutput->geometry())) {
-                qCDebug(NOTIFICATIONMANAGER) << "Screen geometry" << checkOutput->geometry() << "intersects" << output->geometry()
-                                             << "- considering them to be mirrored";
+            if (output->geometry().contains(checkOutput->geometry()) || checkOutput->geometry().contains(output->geometry())) {
+                qCDebug(NOTIFICATIONMANAGER) << "Screen geometry" << checkOutput->geometry() << "and" << output->geometry()
+                                             << "are completely overlapping - considering them to be mirrored";
                 setScreensMirrored(true);
                 return;
             }
