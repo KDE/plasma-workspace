e4eff93cd05b1f26d8cb58286cea8e9c2911f85a
Fushan Wen <qydwhotmail@gmail.com>
2022-11-22 20:09:52 +0800

    applets/batterymonitor: use fallback icon in inhibition list
    This makes items without valid icons align with other items.

--- dataengines/powermanagement/powermanagementengine.cpp
+++ dataengines/powermanagement/powermanagementengine.cpp
@@ -30,6 +30,7 @@
 #include <QDBusMetaType>
 #include <QDBusPendingCallWatcher>
 #include <QDBusReply>
+#include <QIcon>
 
 #include "powermanagementservice.h"
 #include <Plasma/DataContainer>
@@ -833,6 +834,9 @@ void PowermanagementEngine::populateApplicationData(const QString &name, QString
         } else {
             *prettyName = name;
             *icon = name.section(QLatin1Char('/'), -1).toLower();
+            if (!QIcon::hasThemeIcon(*icon)) {
+                *icon = QStringLiteral("application-x-executable");
+            }
         }
     }
 }
