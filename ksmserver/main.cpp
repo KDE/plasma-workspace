ba6d1ad7c3c2aa3b3b6abf792b631a5c60ba0f5d
Nate Graham <nate@kde.org>
2022-12-07 07:59:35 -0700

    Revert "ksmserver: Don't restore session on mobile"
    This reverts commit b2acd975450cf36c1147c360b239fda082019520.
    
    This doesn't compile.

--- ksmserver/main.cpp
+++ ksmserver/main.cpp
@@ -16,7 +16,6 @@
 #include <unistd.h>
 
 #include <KMessageBox>
-#include <KRuntimePlatform>
 
 #include "server.h"
 #include <KLocalizedString>
@@ -293,11 +292,6 @@ int main(int argc, char *argv[])
     KConfigGroup config(KSharedConfig::openConfig(), "General");
 
     QString loginMode = config.readEntry("loginMode", "restorePreviousLogout");
-
-    // we don't need session restoring in Plasma Mobile
-    if (KRuntimePlatform::runtimePlatform().contains("phone")) {
-        loginMode = QStringLiteral("emptySession");
-    }
 
     if (parser.isSet(restoreOption))
         server->setRestoreSession(QStringLiteral(SESSION_BY_USER));
