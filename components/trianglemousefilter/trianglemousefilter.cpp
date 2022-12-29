6c18efd13cabde92f82fa881a7ebb29beda7344b
Derek Christ <christ.derek@gmail.com>
2022-12-29 16:16:11 +0100

    Improve responsiveness of triangular filter
    The responsiveness of the filter is improved by adding two new
    metrics to the filter logic:     - Firstly, the direction of the cursor
    movement is considered. If (in the default configuration) the filter is
    currently active and the user moves the cursor to the left (away from the
    content area), the filter stops intercepting the movement events. This
    results in a noticeable improvement in responsiveness without compromise
    the objective of the filter.     - Secondly, a velocity metric (as
    already brought up in BUG 438950) is added. This ensures that a very slow
    movement of the cursor results in disabling the triangular filter. This,
    also, improves the the feeling of responsiveness significantly. Still,
    there is a need for the timer in the filter, as when the cursor has
    stopped its movement completely, it will no longer send HoverMove events
    to the filter, thus it wouldn't be possible to detect a complete stop
    of the cursor.

--- components/trianglemousefilter/trianglemousefilter.cpp
+++ components/trianglemousefilter/trianglemousefilter.cpp
@@ -9,6 +9,7 @@
 #include "trianglemousefilter.h"
 
 #include <QPolygonF>
+#include <cmath>
 
 TriangleMouseFilter::TriangleMouseFilter(QQuickItem *parent)
     : QQuickItem(parent)
@@ -20,8 +21,17 @@ TriangleMouseFilter::TriangleMouseFilter(QQuickItem *parent)
 
     m_resetTimer.setSingleShot(true);
     connect(&m_resetTimer, &QTimer::timeout, this, [this]() {
-        m_interceptionPos.reset();
+        if (!m_firstEntered) {
+            m_interceptionPos.reset();
+        } else {
+            m_firstEntered = false;
+        }
+
+        m_lastCursorPosition.reset();
+        m_lastTimestamp.reset();
+
         update();
+
         if (!m_interceptedHoverItem) {
             return;
         }
@@ -31,9 +41,6 @@ TriangleMouseFilter::TriangleMouseFilter(QQuickItem *parent)
             qApp->sendEvent(m_interceptedHoverItem, &e);
             m_interceptedHoverEnterPosition.reset();
         }
-        const auto targetPosition = mapToItem(m_interceptedHoverItem, m_lastCursorPosition);
-        QHoverEvent e(QEvent::HoverMove, targetPosition, targetPosition);
-        qApp->sendEvent(m_interceptedHoverItem, &e);
     });
 };
 
@@ -41,7 +48,6 @@ bool TriangleMouseFilter::childMouseEventFilter(QQuickItem *item, QEvent *event)
 {
     update();
     if (!m_active) {
-        event->setAccepted(false);
         return false;
     }
 
@@ -57,14 +63,63 @@ bool TriangleMouseFilter::childMouseEventFilter(QQuickItem *item, QEvent *event)
         return true;
     case QEvent::HoverEnter:
     case QEvent::HoverMove: {
-        QHoverEvent *he = static_cast<QHoverEvent *>(event);
+        QHoverEvent &he = *static_cast<QHoverEvent *>(event);
 
-        const QPointF position = item->mapToItem(this, he->posF());
+        const QPointF position = item->mapToItem(this, he.posF());
 
-        if (filterContains(position)) {
-            if (event->type() == QEvent::HoverEnter) {
-                m_interceptedHoverEnterPosition = position;
+        // This clause means that we block focus when first entering a given position
+        // in the case of kickoff it's so that we can move the mouse from the bottom tabbar to the side view
+        bool firstEnter = m_blockFirstEnter && event->type() == QEvent::HoverEnter && !m_interceptionPos;
+
+        if (m_lastCursorPosition && m_lastTimestamp && !firstEnter) {
+            const QPointF deltaPosition = position - m_lastCursorPosition.value();
+            const ulong deltaTime = he.timestamp() - m_lastTimestamp.value();
+
+            // As a first metric, we check the direction in which the cursor has been moved
+            bool directionMetric = false;
+            switch (m_edge) {
+            case Qt::RightEdge:
+                directionMetric = deltaPosition.x() < -JITTER_THRESHOLD;
+                break;
+            case Qt::TopEdge:
+                directionMetric = deltaPosition.y() > JITTER_THRESHOLD;
+                break;
+            case Qt::LeftEdge:
+                directionMetric = deltaPosition.x() > JITTER_THRESHOLD;
+                break;
+            case Qt::BottomEdge:
+                directionMetric = deltaPosition.y() < -JITTER_THRESHOLD;
+                break;
+            }
+            if (directionMetric) {
+                resendHoverEvents(he, position);
+                return false;
+            }
+
+            // As a second metric, we use the velocity of the cursor to disable the filter
+            if (deltaTime != 0 && he.timestamp() != 0) {
+                const double velocity = std::pow(deltaPosition.x(), 2) + std::pow(deltaPosition.y(), 2) / deltaTime;
+                if (velocity < VELOCITY_THRESHOLD) {
+                    resendHoverEvents(he, position);
+                    return false;
+                }
+            }
+        }
+
+        m_lastCursorPosition = position;
+        m_lastTimestamp = he.timestamp();
+
+        // Finally, we check if the cursor movement was inside the filtered region
+        if (firstEnter || filterContains(position)) {
+            if (firstEnter) {
+                // In case of a firstEnter, set the interceptionPos but not the interceptedHoverEnterPosition
+                // so that the timer does not reselect the intercepted item
                 m_interceptedHoverItem = item;
+                m_interceptionPos = position;
+                m_firstEntered = true;
+            } else if (event->type() == QEvent::HoverEnter) {
+                m_interceptedHoverItem = item;
+                m_interceptedHoverEnterPosition = position;
             }
 
             if (m_filterTimeout > 0) {
@@ -72,37 +127,10 @@ bool TriangleMouseFilter::childMouseEventFilter(QQuickItem *item, QEvent *event)
             }
 
             m_lastCursorPosition = position;
-            event->setAccepted(true);
-
             return true;
         } else {
-            if (m_blockFirstEnter && event->type() == QEvent::HoverEnter && !m_interceptionPos) {
-                // this clause means that we block focus when first entering a given position
-                // in the case of kickoff it's so that we can move the mouse from the bottom tabbar to the side view
-                m_interceptedHoverItem = item;
-                m_interceptedHoverEnterPosition = position;
-                if (m_filterTimeout > 0) {
-                    m_resetTimer.start(m_filterTimeout);
-                }
-                event->setAccepted(true);
-                m_lastCursorPosition = position;
-                m_interceptionPos = position;
-                return true;
-            }
-
             m_interceptionPos = position;
-            m_lastCursorPosition = position;
-
-            // if we are no longer inhibiting events and have previously intercepted a hover enter
-            // we manually send the hover enter to that item
-            if (event->type() == QEvent::HoverMove && m_interceptedHoverItem) {
-                const auto targetPosition = mapToItem(m_interceptedHoverItem, position);
-                QHoverEvent e(QEvent::HoverEnter, targetPosition, targetPosition);
-                qApp->sendEvent(m_interceptedHoverItem, &e);
-                m_interceptedHoverItem.clear();
-            }
-
-            event->setAccepted(false);
+            resendHoverEvents(he, position);
             return false;
         }
     }
@@ -111,14 +139,24 @@ bool TriangleMouseFilter::childMouseEventFilter(QQuickItem *item, QEvent *event)
     }
 }
 
+void TriangleMouseFilter::resendHoverEvents(const QHoverEvent &event, QPointF cursorPosition)
+{
+    // If we are no longer inhibiting events and have previously intercepted a hover enter
+    // we manually send the hover enter to that item
+    if (event.type() == QEvent::HoverMove && m_interceptedHoverItem) {
+        const auto targetPosition = mapToItem(m_interceptedHoverItem, cursorPosition);
+        QHoverEvent e(QEvent::HoverEnter, targetPosition, targetPosition);
+        qApp->sendEvent(m_interceptedHoverItem, &e);
+    }
+
+    m_interceptedHoverItem.clear();
+}
+
 bool TriangleMouseFilter::filterContains(const QPointF &p) const
 {
     if (!m_interceptionPos) {
         return false;
     }
-
-    // We add some jitter protection by extending our triangle out slight past the mouse position in the opposite direction of the edge;
-    const int jitterThreshold = 3;
 
     // QPolygonF.contains returns false if we're on the edge, so we pad our main item
     const QRectF shape = (m_edgeLine.size() == 4) ? QRect(m_edgeLine[0] - 1, m_edgeLine[1] - 1, width() + m_edgeLine[2] + 1, height() + m_edgeLine[3] + 1)
@@ -126,18 +164,19 @@ bool TriangleMouseFilter::filterContains(const QPointF &p) const
 
     QPolygonF poly;
 
+    // We use some jitter protection by extending our triangle out slight past the mouse position in the opposite direction of the edge;
     switch (m_edge) {
     case Qt::RightEdge:
-        poly << m_interceptionPos.value() + QPointF(-jitterThreshold, 0) << shape.topRight() << shape.bottomRight();
+        poly << m_interceptionPos.value() + QPointF(-JITTER_THRESHOLD, 0) << shape.topRight() << shape.bottomRight();
         break;
     case Qt::TopEdge:
-        poly << m_interceptionPos.value() + QPointF(0, -jitterThreshold) << shape.topLeft() << shape.topRight();
+        poly << m_interceptionPos.value() + QPointF(0, -JITTER_THRESHOLD) << shape.topLeft() << shape.topRight();
         break;
     case Qt::LeftEdge:
-        poly << m_interceptionPos.value() + QPointF(jitterThreshold, 0) << shape.topLeft() << shape.bottomLeft();
+        poly << m_interceptionPos.value() + QPointF(JITTER_THRESHOLD, 0) << shape.topLeft() << shape.bottomLeft();
         break;
     case Qt::BottomEdge:
-        poly << m_interceptionPos.value() + QPointF(0, jitterThreshold) << shape.bottomLeft() << shape.bottomRight();
+        poly << m_interceptionPos.value() + QPointF(0, JITTER_THRESHOLD) << shape.bottomLeft() << shape.bottomRight();
     }
 
     bool firstCheck = poly.containsPoint(p, Qt::OddEvenFill);
