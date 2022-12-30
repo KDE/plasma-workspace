a7c91d949a2bb35bf660d2d95b6886abea6fb4a7
Fushan Wen <qydwhotmail@gmail.com>
2022-12-30 19:26:30 +0800

    wallpapers/image: fix reuse compliance

--- wallpapers/image/Messages.sh
+++ wallpapers/image/Messages.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
+# SPDX-FileCopyrightText: None
+# SPDX-License-Identifier: CC0-1.0
 $XGETTEXT `find . -name \*.js -o -name \*.qml -o -name \*.cpp` -o $podir/plasma_wallpaper_org.kde.image.pot
--- wallpapers/image/plugin/config-KF5KExiv2.h.cmake
+++ wallpapers/image/plugin/config-KF5KExiv2.h.cmake
@@ -1,1 +1,3 @@
+// SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+// SPDX-License-Identifier: LGPL-2.0-or-later
 #cmakedefine01 HAVE_KF5KExiv2
