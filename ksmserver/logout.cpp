a5f0975900ad022308a24271374c16efafe14280
Roland Pallai <dap78@magex.hu>
2023-01-10 13:03:05 +0100

    Fix manual session saving
    Seems like this bug have been introduced accidentally by 3558eca7
    
    BUG: 450756
    FIXED-IN: 5.27

--- ksmserver/logout.cpp
+++ ksmserver/logout.cpp
@@ -238,6 +238,7 @@ void KSMServer::saveCurrentSession()
 
     const auto pendingClients = clients;
     for (KSMClient *c : pendingClients) {
+        c->resetState();
         SmsSaveYourself(c->connection(), saveType, false, SmInteractStyleNone, false);
     }
     if (clients.isEmpty())
