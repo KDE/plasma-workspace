b4c671c696aaf6b356d07d55c777ebcbfb86d0c8
Fushan Wen <qydwhotmail@gmail.com>
2023-01-12 20:08:41 +0800

    components: fix sentences in QML not being translated
    Add qml files to Messages.sh

--- components/Messages.sh
+++ components/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.kcfg` >>rc.cpp
-$XGETTEXT  `find . -name \*.cpp` -o $podir/plasmashellprivateplugin.pot
+$XGETTEXT  `find . -name \*.cpp -o -name \*.qml` -o $podir/plasmashellprivateplugin.pot
 rm -f rc.cpp
