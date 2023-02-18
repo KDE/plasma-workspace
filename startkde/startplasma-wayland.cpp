14c5b464f41f2b858453fd102ed57397755f9c26
Fushan Wen <qydwhotmail@gmail.com>
2023-02-18 17:25:44 +0800

    Revert "Revert "startplasma-wayland: Don't set GDK_SCALE and GDK_DPI_SCALE""
    This reverts commit 34e3efd85252182977178ab99435b99a1ba987b9 and bd9c257dcdf050f74a691c31663cbca68ce8d8e7
    
    CCBUG: 465733
    CCBUG: 443215
    
    
    (cherry picked from commit cdf2e1a9abf7cdee24828f59aa7cc2295dee4877)

--- startkde/startplasma-wayland.cpp
+++ startkde/startplasma-wayland.cpp
@@ -67,18 +67,6 @@ int main(int argc, char **argv)
     setupPlasmaEnvironment();
     runStartupConfig();
     qputenv("PLASMA_USE_QT_SCALING", "1");
-
-    // Required or else XWayland-using Electron apps are scaled too small;
-    // see https://github.com/electron/electron/issues/37312.
-    // But let the user override these if they're feeling adventurous.
-    if (qEnvironmentVariableIsEmpty("GDK_SCALE")) {
-        qputenv("GDK_SCALE", "1");
-    }
-
-    if (qEnvironmentVariableIsEmpty("GDK_DPI_SCALE")) {
-        qputenv("GDK_DPI_SCALE", "1");
-    }
-
     qputenv("XDG_SESSION_TYPE", "wayland");
 
     auto oldSystemdEnvironment = getSystemdEnvironment();
