7a4cb70a2e3a3bf6e9ca3a7c2b0e25a4b4992296
Nicolas Fella <nicolas.fella@gmx.de>
2023-03-04 17:38:17 +0100

    Make Qt6 login session coinstallable with Qt5
    This allows to have both a self-built Qt6/master and Qt5/5.27 session
    
    This is useful for the time being where master isn't perfectly stable

--- login-sessions/install-sessions.sh.cmake
+++ login-sessions/install-sessions.sh.cmake
@@ -7,8 +7,8 @@
 set -e
 
 # Make built-from-source sessions appear in login screen
-sudo install -D @CMAKE_CURRENT_BINARY_DIR@/plasmax11-dev.desktop /usr/share/xsessions/
-sudo install -D @CMAKE_CURRENT_BINARY_DIR@/plasmawayland-dev.desktop /usr/share/wayland-sessions/
+sudo install -D @CMAKE_CURRENT_BINARY_DIR@/plasmax11-dev6.desktop /usr/share/xsessions/
+sudo install -D @CMAKE_CURRENT_BINARY_DIR@/plasmawayland-dev6.desktop /usr/share/wayland-sessions/
 install @CMAKE_BINARY_DIR@/prefix.sh @CMAKE_INSTALL_FULL_LIBEXECDIR@/plasma-dev-prefix.sh
 install @CMAKE_CURRENT_BINARY_DIR@/startplasma-dev.sh @CMAKE_INSTALL_FULL_LIBEXECDIR@
 
