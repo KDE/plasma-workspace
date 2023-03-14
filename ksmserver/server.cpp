35e11117a2543316527fe17501e6a192f3676ccc
Nate Graham <nate@kde.org>
2023-03-14 14:53:28 -0600

    ksmserver: replace "Halt" with "Shut Down" in user-facing text
    "Halt" is technically unclear to the user and sounds vaguely
    threatening. Let's instead use the more common, clear, and user-friendly
    term "Shut Down" that we use everywhere else.
    
    CCBUG: 467017

--- ksmserver/server.cpp
+++ ksmserver/server.cpp
@@ -893,8 +893,8 @@ void KSMServer::setupShortcuts()
         KGlobalAccel::self()->setGlobalShortcut(a, QKeySequence());
         connect(a, &QAction::triggered, this, &KSMServer::logoutWithoutConfirmation);
 
-        a = actionCollection->addAction(QStringLiteral("Halt Without Confirmation"));
-        a->setText(i18n("Halt Without Confirmation"));
+        a = actionCollection->addAction(QStringLiteral("Shut Down Without Confirmation"));
+        a->setText(i18n("Shut Down Without Confirmation"));
         KGlobalAccel::self()->setGlobalShortcut(a, QKeySequence());
         connect(a, &QAction::triggered, this, &KSMServer::haltWithoutConfirmation);
 
