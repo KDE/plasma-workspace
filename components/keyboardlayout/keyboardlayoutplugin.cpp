46835c1f95926e12ca7ce746751d11f7e309ba09
Alexander Volkov <avolkov@astralinux.ru>
2023-03-14 13:39:44 +0300

    [keyboardlayout] Fix forced activation of vkbd

--- components/keyboardlayout/keyboardlayoutplugin.cpp
+++ components/keyboardlayout/keyboardlayoutplugin.cpp
@@ -9,10 +9,17 @@
 #include "virtualkeyboard.h"
 
 #include <QQmlEngine>
+#include <QDBusPendingReply>
+
+// register type for Keyboards.KWinVirtualKeyboard.forceActivate();
+Q_DECLARE_METATYPE(QDBusPendingReply<>)
 
 void KeyboardLayoutPlugin::registerTypes(const char *uri)
 {
     Q_ASSERT(uri == QLatin1String("org.kde.plasma.workspace.keyboardlayout"));
+
+    // register type for Keyboards.KWinVirtualKeyboard.forceActivate();
+    qRegisterMetaType<QDBusPendingReply<>>();
 
     qmlRegisterType<KeyboardLayout>(uri, 1, 0, "KeyboardLayout");
     qmlRegisterSingletonType<KwinVirtualKeyboardInterface>(uri, 1, 0, "KWinVirtualKeyboard", [](QQmlEngine *, QJSEngine *) -> QObject * {
