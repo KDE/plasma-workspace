63dd197e5c4e51c585dd5a8ae50fa979d94c9a13
Fushan Wen <qydwhotmail@gmail.com>
2023-04-28 20:30:00 +0800

    applets/systemtray: add workaround for QTBUG-59044
    This avoids the same SNI menu being opened again after user long presses
    to open a SNI menu.

--- applets/systemtray/systemtray.cpp
+++ applets/systemtray/systemtray.cpp
@@ -299,6 +299,10 @@ void SystemTray::showStatusNotifierContextMenu(KJob *job, QQuickItem *statusNoti
         menu->winId();
         menu->windowHandle()->setTransientParent(statusNotifierIcon->window());
         menu->popup(QPoint(x, y));
+        // Workaround for QTBUG-59044
+        if (auto item = statusNotifierIcon->window()->mouseGrabberItem()) {
+            item->ungrabMouse();
+        }
     }
 }
 
