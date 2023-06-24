45b4add5fa15258f73ce630c3cd2af7c07ff60d4
Fushan Wen <qydwhotmail@gmail.com>
2023-06-24 12:12:19 +0800

    wallpapers/image: fix QStringLiteral data duplication in tests

--- wallpapers/image/plugin/autotests/commontestdata.h
+++ wallpapers/image/plugin/autotests/commontestdata.h
@@ -11,29 +11,29 @@
 
 namespace ImageBackendTestData
 {
-constexpr int defaultImageCount = 5;
-constexpr int defaultPackageCount = 2;
-constexpr int defaultTotalCount = defaultImageCount + defaultPackageCount;
+inline constexpr int defaultImageCount = 5;
+inline constexpr int defaultPackageCount = 2;
+inline constexpr int defaultTotalCount = defaultImageCount + defaultPackageCount;
 
-constexpr int alternateImageCount = 1;
-constexpr int alternatePackageCount = 1;
-constexpr int alternateTotalCount = alternateImageCount + alternatePackageCount;
+inline constexpr int alternateImageCount = 1;
+inline constexpr int alternatePackageCount = 1;
+inline constexpr int alternateTotalCount = alternateImageCount + alternatePackageCount;
 
-inline const QString defaultHiddenImageFileName = QStringLiteral(".hiddenfolder/hidden.jpg");
-inline const QString defaultImageFileName1 = QStringLiteral("wallpaper.jpg.jpg");
-inline const QString defaultImageFileName2 = QStringLiteral("# BUG454692 file name with hash char.png");
-inline const QString defaultImageFileName3 = QStringLiteral(".BUG460287/BUG460287.webp");
-inline const QString defaultImageFileName4 = QStringLiteral(".BUG460287/BUG461940.webp");
-inline const QString defaultImageFileName5_orig = QStringLiteral("BUG454692 file name with backslash.png");
-inline const QString defaultImageFileName5 = QStringLiteral("\\ BUG454692 file name with backslash.png");
-inline const QString defaultPackageFolderName1 = QStringLiteral("FEATURE207976-dark-wallpaper");
-inline const QString defaultPackageFolderName2 = QStringLiteral("package");
+static const QString defaultHiddenImageFileName = QStringLiteral(".hiddenfolder/hidden.jpg");
+static const QString defaultImageFileName1 = QStringLiteral("wallpaper.jpg.jpg");
+static const QString defaultImageFileName2 = QStringLiteral("# BUG454692 file name with hash char.png");
+static const QString defaultImageFileName3 = QStringLiteral(".BUG460287/BUG460287.webp");
+static const QString defaultImageFileName4 = QStringLiteral(".BUG460287/BUG461940.webp");
+static const QString defaultImageFileName5_orig = QStringLiteral("BUG454692 file name with backslash.png");
+static const QString defaultImageFileName5 = QStringLiteral("\\ BUG454692 file name with backslash.png");
+static const QString defaultPackageFolderName1 = QStringLiteral("FEATURE207976-dark-wallpaper");
+static const QString defaultPackageFolderName2 = QStringLiteral("package");
 
-inline const QString alternateImageFileName1 = QStringLiteral("dummy.jpg");
-inline const QString alternatePackageFolderName1 = QStringLiteral("dummy");
+static const QString alternateImageFileName1 = QStringLiteral("dummy.jpg");
+static const QString alternatePackageFolderName1 = QStringLiteral("dummy");
 
-inline const QString customAccentColorPackage1 = QStringLiteral("testdata/customaccentcolor/case1/");
-inline const QString customAccentColorPackage2 = QStringLiteral("testdata/customaccentcolor/case2/");
+static const QString customAccentColorPackage1 = QStringLiteral("testdata/customaccentcolor/case1/");
+static const QString customAccentColorPackage2 = QStringLiteral("testdata/customaccentcolor/case2/");
 }
 
 // Fix illegal filename on Windows
