080aea9d31b8dad37b0b2024dc8f5e7f47736dda
Laurent Montel <montel@kde.org>
2023-06-26 13:43:53 +0200

    We use qt6 only

--- cmake/FindKIOExtras.cmake
+++ cmake/FindKIOExtras.cmake
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2021 Alexander Lohnau <alexander.lohnau@gmx.de>
 # SPDX-License-Identifier: BSD-3-Clause
 
-find_path(KIOExtras_PATH thumbnail.so PATHS ${KDE_INSTALL_FULL_PLUGINDIR}/kf${QT_MAJOR_VERSION}/kio/)
+find_path(KIOExtras_PATH thumbnail.so PATHS ${KDE_INSTALL_FULL_PLUGINDIR}/kf6/kio/)
 
 if (KIOExtras_PATH)
     set(KIOExtras_FOUND TRUE)
