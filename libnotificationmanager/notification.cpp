5df62e545cccba84edca5cce0cb60b147f2fb60b
Fushan Wen <qydwhotmail@gmail.com>
2023-07-30 22:50:36 +0800

    libtaskmanager: make allowedTags static

--- libnotificationmanager/notification.cpp
+++ libnotificationmanager/notification.cpp
@@ -59,7 +59,7 @@ QString Notification::Private::sanitize(const QString &text)
     QString result;
     QXmlStreamWriter out(&result);
 
-    const QVector<QString> allowedTags = {"b", "i", "u", "img", "a", "html", "br", "table", "tr", "td"};
+    static const QStringList allowedTags{"b", "i", "u", "img", "a", "html", "br", "table", "tr", "td"};
 
     out.writeStartDocument();
     while (!r.atEnd()) {
