8be31b5cb408cb6be2cdf15ff9500af2162d9892
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-08-27 17:53:47 +0200

    runners/bookmarks: Do not read profile path from config file
    This feature is nowhere documented. The key was written some Plasma
    versions ago and people might have a stale entry because of it.
    
    In any case, a magic entry in kdeglobals is not the way to go if we want
    tho have the profile customizable.
    
    CCBUG: 473568

--- runners/bookmarks/browsers/firefox.cpp
+++ runners/bookmarks/browsers/firefox.cpp
@@ -1,6 +1,7 @@
 /*
     SPDX-FileCopyrightText: 2007 Glenn Ergeerts <glenn.ergeerts@telenet.be>
     SPDX-FileCopyrightText: 2012 Marco Gulino <marco.gulino@xpeppers.com>
+    SPDX-FileCopyrightText: 2023 Alexander Lohnau <alexander.lohnau@gmx.de>
 
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
@@ -11,10 +12,11 @@
 #include "favicon.h"
 #include "faviconfromblob.h"
 #include "fetchsqlite.h"
+#include <KConfig>
 #include <KConfigGroup>
-#include <KSharedConfig>
 #include <QFile>
 #include <QRegularExpression>
+#include <QStandardPaths>
 
 Firefox::Firefox(const QString &firefoxConfigDir, QObject *parent)
     : QObject(parent)
@@ -31,52 +33,41 @@ Firefox::Firefox(const QString &firefoxConfigDir, QObject *parent)
         return;
     }
 
-    KConfigGroup grp(KSharedConfig::openConfig(QStringLiteral("kdeglobals")), QStringLiteral("General"));
-    /* This allows the user to specify a profile database */
-    m_dbFile = grp.readEntry("dbfile", QString());
-    if (m_dbFile.isEmpty() || !QFile::exists(m_dbFile)) {
-        // Try to get the right database file, the default profile is used
-        KConfig firefoxProfile(firefoxConfigDir + "/profiles.ini", KConfig::SimpleConfig);
-        QStringList profilesList = firefoxProfile.groupList();
-        profilesList = profilesList.filter(QRegularExpression(QStringLiteral("^Profile\\d+$")));
+    // Try to get the right database file, the default profile is used
+    KConfig firefoxProfile(firefoxConfigDir + "/profiles.ini", KConfig::SimpleConfig);
+    QStringList profilesList = firefoxProfile.groupList();
+    profilesList = profilesList.filter(QRegularExpression(QStringLiteral("^Profile\\d+$")));
 
-        QString profilePath;
-        if (profilesList.size() == 1) {
-            // There is only 1 profile so we select it
-            KConfigGroup fGrp = firefoxProfile.group(profilesList.first());
-            profilePath = fGrp.readEntry("Path");
+    QString profilePath;
+    if (profilesList.size() == 1) {
+        // There is only 1 profile so we select it
+        KConfigGroup fGrp = firefoxProfile.group(profilesList.first());
+        profilePath = fGrp.readEntry("Path");
+    } else {
+        const QStringList installConfig = firefoxProfile.groupList().filter(QRegularExpression("^Install.*"));
+        // The profile with Default=1 is not always the default profile, see BUG: 418526
+        // If there is only one Install* group it contains the default profile
+        if (installConfig.size() == 1) {
+            profilePath = firefoxProfile.group(installConfig.first()).readEntry("Default");
         } else {
-            const QStringList installConfig = firefoxProfile.groupList().filter(QRegularExpression("^Install.*"));
-            // The profile with Default=1 is not always the default profile, see BUG: 418526
-            // If there is only one Install* group it contains the default profile
-            if (installConfig.size() == 1) {
-                profilePath = firefoxProfile.group(installConfig.first()).readEntry("Default");
-            } else {
-                // There are multiple profiles, find the default one
-                for (const QString &profileName : qAsConst(profilesList)) {
-                    KConfigGroup fGrp = firefoxProfile.group(profileName);
-                    if (fGrp.readEntry<int>("Default", 0)) {
-                        profilePath = fGrp.readEntry("Path");
-                        break;
-                    }
+            // There are multiple profiles, find the default one
+            for (const QString &profileName : qAsConst(profilesList)) {
+                KConfigGroup fGrp = firefoxProfile.group(profileName);
+                if (fGrp.readEntry<int>("Default", 0)) {
+                    profilePath = fGrp.readEntry("Path");
+                    break;
                 }
             }
         }
-
-        if (profilePath.isEmpty()) {
-            qCWarning(RUNNER_BOOKMARKS) << "No default firefox profile found";
-            return;
-        }
-        profilePath.prepend(firefoxConfigDir + "/");
-        m_dbFile = profilePath + "/places.sqlite";
-        m_dbFile_fav = profilePath + "/favicons.sqlite";
-    } else {
-        auto dir = QDir(m_dbFile);
-        if (dir.cdUp()) {
-            QString profilePath = dir.absolutePath();
-            m_dbFile_fav = profilePath + "/favicons.sqlite";
-        }
     }
+    if (profilePath.isEmpty()) {
+        qCWarning(RUNNER_BOOKMARKS) << "No default firefox profile found";
+        return;
+    }
+    profilePath.prepend(firefoxConfigDir + "/");
+    m_dbFile = profilePath + "/places.sqlite";
+    m_dbFile_fav = profilePath + "/favicons.sqlite";
+
     // We can reuse the favicon instance over the lifetime of the plugin consequently the
     // icons that are already written to disk can be reused in multiple match sessions
     updateCacheFile(m_dbFile_fav, m_dbCacheFile_fav);
