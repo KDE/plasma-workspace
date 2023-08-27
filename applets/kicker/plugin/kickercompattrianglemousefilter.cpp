c3550e7f32ba4b919c73c4631eaba17e86d27486
Fushan Wen <qydwhotmail@gmail.com>
2023-08-27 08:16:57 +0800

    applets/kicker: deprecate KickerCompatTriangleMouseFilter

--- applets/kicker/plugin/kickercompattrianglemousefilter.cpp
+++ applets/kicker/plugin/kickercompattrianglemousefilter.cpp
@@ -6,8 +6,12 @@
 
 #include "kickercompattrianglemousefilter.h"
 
+#include "debug.h"
+
 KickerCompatTriangleMouseFilter::KickerCompatTriangleMouseFilter(QQuickItem *parent)
     : TriangleMouseFilter(parent)
 {
+    qCWarning(KICKER_DEBUG) << "Kicker.TriangleMouseFilter is deprecated and will be removed in Plasma 6. Import TriangleMouseFilter from "
+                               "org.kde.plasma.workspace.trianglemousefilter instead";
     setProperty("blockFirstEnter", true);
 }
