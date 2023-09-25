KDE/plasma-workspace

commit ecfc27c245a84085127982b43af92b8148740922

runners/recentddocuments: match only inside file names

runners/recentdocuments/recentdocuments.cpp
@@ -84,6 +84,8 @@ void RecentDocuments::match(Plasma::RunnerContext &context)
         } else if (url.fileName().startsWith(term, Qt::CaseInsensitive)) {
             match.setRelevance(relevance + 0.1);
             match.setType(Plasma::QueryMatch::PossibleMatch);
+        } else if (!url.fileName().contains(term)) {
+            continue;
         }
 
         match.setIconName(KIO::iconNameForUrl(url));

commit 28ced0e1e0bc5f62f107b3e6871fda60277297e8

runners/recentdocuments: Fix results being discarded when casing is different
BUG: 474782
Regression from ecfc27c245a84085127982b43af92b8148740922

Test Plan:
- Create a file called MyTextFile.txt
- Open it in any KDE app so it is in the recent files list
- "MyTextFile" should work as before and provide the result
- "textfile" as a query should only provide a result with this change

runners/recentdocuments/recentdocuments.cpp
@@ -74,7 +74,7 @@ void RecentDocuments::match(KRunner::RunnerContext &context)
         } else if (url.fileName().startsWith(term, Qt::CaseInsensitive)) {
             match.setRelevance(relevance + 0.1);
             match.setType(KRunner::QueryMatch::PossibleMatch);
-        } else if (!url.fileName().contains(term)) {
+        } else if (!url.fileName().contains(term, Qt::CaseInsensitive)) {
             continue;
         }
 