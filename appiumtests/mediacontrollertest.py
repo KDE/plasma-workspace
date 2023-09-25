f54df45fbc9ec7a008c8a7b473a6d5b24a8077b7
Fushan Wen <qydwhotmail@gmail.com>
2023-09-25 18:23:44 +0800

    applets/mediacontroller: fix binding loop warnings and consolidate it in test
    This allows the test to detect any potential binding errors in QML.

--- appiumtests/mediacontrollertest.py
+++ appiumtests/mediacontrollertest.py
@@ -47,6 +47,10 @@ class MediaControllerTests(unittest.TestCase):
         desired_caps: dict[str, Any] = {}
         desired_caps["app"] = "plasmawindowed -p org.kde.plasma.nano org.kde.plasma.mediacontroller"
         desired_caps["timeouts"] = {'implicit': 5000}
+        desired_caps["environ"] = {
+            "QT_FATAL_WARNINGS": "1",
+            "QT_LOGGING_RULES": "qt.accessibility.atspi.warning=false;kf.plasma.core.warning=false;kf.windowsystem.warning=false;kf.kirigami.warning=false",
+        }
         cls.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', desired_capabilities=desired_caps)
         cls.driver.implicitly_wait = 10
 
