2d08920ee9f15321df91a6d03fdfc1e27756db09
Fushan Wen <qydwhotmail@gmail.com>
2023-09-26 09:16:32 +0800

    applets/cameraindicator: Use BSD-2-Clause for Messages.sh
    It's also used in other places.

--- applets/cameraindicator/Messages.sh
+++ applets/cameraindicator/Messages.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 # SPDX-FileCopyrightText: 2023 Fushan Wen <qydwhotmail@gmail.com>
-# SPDX-License-Identifier: CC0-1.0
+# SPDX-License-Identifier: BSD-2-Clause
 $XGETTEXT `find . -name \*.qml` -o $podir/plasma_applet_org.kde.plasma.cameraindicator.pot
