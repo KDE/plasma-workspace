553d6f56c79dc6c135fa8ec8d557a4ae3a9dec0b
Fushan Wen <qydwhotmail@gmail.com>
2023-10-19 01:30:06 +0800

    clipboardtest: consolidate fd5c0faee24aac222a71c061f3bb72482635e591
    CCBUG: 475696

--- appiumtests/clipboardtest.py
+++ appiumtests/clipboardtest.py
@@ -12,6 +12,7 @@ import sys
 import tempfile
 import time
 import unittest
+from enum import Enum
 from typing import Final
 
 import gi
@@ -27,45 +28,36 @@ from gi.repository import Gdk, GLib
 WIDGET_ID: Final = "org.kde.plasma.clipboard"
 
 
-def keyval_to_keycode(key_val: int):
+class XKeyCode(Enum):
     """
-    @param key_val see https://www.cl.cam.ac.uk/~mgk25/ucs/keysymdef.h
+    @see https://www.cl.cam.ac.uk/~mgk25/ucs/keysymdef.h
     """
+    Tab = 0xff09
+    Space = 0x0020
+    Return = 0xff0d
+    Enter = 0xff8d
+    Escape = 0xff1b
+    Up = 0xff52
+    Right = 0xff53
+    Down = 0xff54
+
+
+def keyval_to_keycode(key_val: XKeyCode):
     keymap = Gdk.Keymap.get_default()
-    ret, keys = keymap.get_entries_for_keyval(key_val)
+    ret, keys = keymap.get_entries_for_keyval(key_val.value)
     if not ret:
         raise RuntimeError("Failed to map key!")
     return keys[0]
 
 
-def press_key(key_val: int, repeat_num: int = 1) -> None:
+def press_key(key_val: XKeyCode, repeat_num: int = 1) -> None:
     with tempfile.NamedTemporaryFile() as handler:
         keymap = keyval_to_keycode(key_val)
         handler.write(json.dumps([{'type': 'keyboard', 'level': keymap.level, 'keycode': keymap.keycode}]).encode())
         handler.flush()
         for _ in range(repeat_num):
             assert subprocess.Popen(["selenium-webdriver-at-spi-inputsynth", handler.name], stdout=sys.stderr, stderr=sys.stderr).wait() == 0
-            time.sleep(1)
-
-
-def press_tab_key(repeat_num: int = 1) -> None:
-    press_key(0xff09, repeat_num)
-
-
-def press_space_key() -> None:
-    press_key(0x0020)
-
-
-def press_escape_key() -> None:
-    press_key(0xff1b)
-
-
-def press_down_key(repeat_num: int = 1) -> None:
-    press_key(0xff54, repeat_num)
-
-
-def press_right_key(repeat_num: int = 1) -> None:
-    press_key(0xff53, repeat_num)
+            time.sleep(0.5)
 
 
 class ClipboardTest(unittest.TestCase):
@@ -99,7 +91,7 @@ class ClipboardTest(unittest.TestCase):
         options.set_capability("app", f"plasmawindowed -p org.kde.plasma.nano {WIDGET_ID}")
         options.set_capability("environ", {
             "QT_FATAL_WARNINGS": "1",
-            "QT_LOGGING_RULES": "qt.accessibility.atspi.warning=false;kf.plasma.core.warning=false;kf.windowsystem.warning=false;kf.kirigami.warning=false",
+            "QT_LOGGING_RULES": "qt.accessibility.atspi.warning=false;kf.plasma.core.warning=false;kf.windowsystem.warning=false;kf.kirigami.warning=false;org.kde.klipper.debug=true",
         })
         options.set_capability("timeouts", {'implicit': 10000})
         cls.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', options=options)
@@ -123,20 +115,21 @@ class ClipboardTest(unittest.TestCase):
         Tests the widget can be opened
         """
         self.driver.find_element(AppiumBy.NAME, "Fushan Wen")
+        self.driver.find_element(AppiumBy.NAME, "clipboard")
 
     def test_1_barcode_1_open_barcode_page(self) -> None:
         """
         Tests the barcode page can be opened
         """
-        press_down_key()
+        press_key(XKeyCode.Down)
         # Wait until the first item is selected
         try:
             self.driver.find_element(AppiumBy.NAME, "Show QR code")
         except NoSuchElementException:
-            press_down_key()  # Try pressing down key again
+            press_key(XKeyCode.Down)  # Try pressing down key again
             self.driver.find_element(AppiumBy.NAME, "Show QR code")
-        press_right_key(2)
-        press_space_key()
+        press_key(XKeyCode.Right, 2)
+        press_key(XKeyCode.Space)
 
         self.driver.find_element(AppiumBy.NAME, "QR Code")
         self.driver.find_element(AppiumBy.NAME, "Return to Clipboard")
@@ -146,8 +139,8 @@ class ClipboardTest(unittest.TestCase):
         """
         Open the barcode type menu
         """
-        press_tab_key(2)
-        press_space_key()
+        press_key(XKeyCode.Tab, 2)
+        press_key(XKeyCode.Space)
         self.driver.find_element(AppiumBy.NAME, "QR Code")
         self.driver.find_element(AppiumBy.NAME, "Data Matrix")
         self.driver.find_element(AppiumBy.NAME, "Aztec")
@@ -162,8 +155,8 @@ class ClipboardTest(unittest.TestCase):
         menu_item = self.driver.find_element(AppiumBy.NAME, "Aztec")
 
         # Switch to Aztec
-        press_down_key(3)
-        press_space_key()
+        press_key(XKeyCode.Down, 3)
+        press_key(XKeyCode.Space)
         WebDriverWait(self.driver, 5).until(lambda _: not menu_item.is_displayed())
         self.driver.find_element(AppiumBy.NAME, "Aztec")  # This is from barcodeItem
 
@@ -171,11 +164,11 @@ class ClipboardTest(unittest.TestCase):
         """
         Switch the current barcode type to Code 39
         """
-        press_tab_key(2)
-        press_space_key()
+        press_key(XKeyCode.Tab, 2)
+        press_key(XKeyCode.Space)
         menu_item = self.driver.find_element(AppiumBy.NAME, "Code 39")
-        press_down_key(4)
-        press_space_key()
+        press_key(XKeyCode.Down, 4)
+        press_key(XKeyCode.Space)
         WebDriverWait(self.driver, 5).until(lambda _: not menu_item.is_displayed())
         self.driver.find_element(AppiumBy.NAME, "Code 39")  # This is from barcodeItem
 
@@ -183,11 +176,11 @@ class ClipboardTest(unittest.TestCase):
         """
         Switch the current barcode type to Code 93
         """
-        press_tab_key(2)
-        press_space_key()
+        press_key(XKeyCode.Tab, 2)
+        press_key(XKeyCode.Space)
         menu_item = self.driver.find_element(AppiumBy.NAME, "Code 93")
-        press_down_key(5)
-        press_space_key()
+        press_key(XKeyCode.Down, 5)
+        press_key(XKeyCode.Space)
         WebDriverWait(self.driver, 5).until(lambda _: not menu_item.is_displayed())
         self.driver.find_element(AppiumBy.NAME, "Code 93")  # This is from barcodeItem
 
@@ -195,11 +188,11 @@ class ClipboardTest(unittest.TestCase):
         """
         Switch the current barcode type to Code 128
         """
-        press_tab_key(2)
-        press_space_key()
+        press_key(XKeyCode.Tab, 2)
+        press_key(XKeyCode.Space)
         menu_item = self.driver.find_element(AppiumBy.NAME, "Code 128")
-        press_down_key(6)
-        press_space_key()
+        press_key(XKeyCode.Down, 6)
+        press_key(XKeyCode.Space)
         WebDriverWait(self.driver, 5).until(lambda _: not menu_item.is_displayed())
         self.driver.find_element(AppiumBy.NAME, "Code 128")  # This is from barcodeItem
 
@@ -207,11 +200,11 @@ class ClipboardTest(unittest.TestCase):
         """
         Switch the current barcode type to QRCode
         """
-        press_tab_key(2)
-        press_space_key()
+        press_key(XKeyCode.Tab, 2)
+        press_key(XKeyCode.Space)
         menu_item = self.driver.find_element(AppiumBy.NAME, "QR Code")
-        press_down_key(1)
-        press_space_key()
+        press_key(XKeyCode.Down, 1)
+        press_key(XKeyCode.Space)
         WebDriverWait(self.driver, 5).until(lambda _: not menu_item.is_displayed())
         self.driver.find_element(AppiumBy.NAME, "QR Code")  # This is from barcodeItem
 
@@ -221,10 +214,36 @@ class ClipboardTest(unittest.TestCase):
         """
         button_item = self.driver.find_element(AppiumBy.NAME, "Return to Clipboard")
         self.assertTrue(button_item.is_displayed())
-        press_tab_key()
-        press_space_key()
+        press_key(XKeyCode.Tab)
+        press_key(XKeyCode.Space)
         self.driver.find_element(AppiumBy.NAME, "Fushan Wen")
         self.assertFalse(button_item.is_displayed())
+
+    def test_2_list_1_bug475696(self) -> None:
+        """
+        Pressing Return on an item should trigger the copy action
+        @see https://bugs.kde.org/show_bug.cgi?id=475696
+        """
+        press_key(XKeyCode.Tab)
+        press_key(XKeyCode.Down)
+        self.driver.find_element(AppiumBy.NAME, "Show QR code")
+        press_key(XKeyCode.Return)
+        self.assertEqual(self.driver.get_clipboard_text(), "clipboard")
+        press_key(XKeyCode.Enter)
+        self.assertEqual(self.driver.get_clipboard_text(), "Fushan Wen")
+        press_key(XKeyCode.Space)
+        self.assertEqual(self.driver.get_clipboard_text(), "clipboard")
+
+    def test_2_list_2_delete(self) -> None:
+        """
+        Deletes the top item and tests if the current clipboard changes
+        @see https://bugs.kde.org/show_bug.cgi?id=475696
+        """
+        # Now "clipboard" is the first item
+        press_key(XKeyCode.Up)
+        self.driver.find_element(AppiumBy.NAME, "Remove from history").click()
+        # The first item becomes the current clipboard item
+        self.assertEqual(self.driver.get_clipboard_text(), "Fushan Wen")
 
 
 if __name__ == '__main__':
