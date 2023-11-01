359d3cd0110c6871fd4024e70b846bcc5432f53a
Aurélien Couderc <coucouf@coucouf.fr>
2023-11-01 17:46:59 +0000

    Fix KCMs common license from LGPL-2+3 to LGPL-2.1+3
    See !1930 for the permission from Ahmad Samir to change
    the license to the preferred one.

--- kcms/kcms-common_p.h
+++ kcms/kcms-common_p.h
@@ -1,7 +1,7 @@
 /*
     SPDX-FileCopyrightText: 2021 Ahmad Samir <a.samirh78@gmail.com>
 
-    SPDX-License-Identifier: LGPL-2.0-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
 
 #pragma once
