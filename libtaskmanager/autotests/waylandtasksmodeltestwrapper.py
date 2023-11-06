2c7389e53d020005de79f8cbdf7e16c29e6175fb
David Edmundson <kde@davidedmundson.co.uk>
2023-11-06 17:32:20 +0000

    Simplifiy kwin spawning

--- libtaskmanager/autotests/waylandtasksmodeltestwrapper.py
+++ libtaskmanager/autotests/waylandtasksmodeltestwrapper.py
@@ -14,33 +14,11 @@ if __name__ == '__main__':
 
     environ = os.environ.copy()
     environ["KWIN_WAYLAND_NO_PERMISSION_CHECKS"] = "1"
-    kwin_process = subprocess.Popen(["kwin_wayland", "--no-lockscreen", "--no-global-shortcuts", "--no-kactivities", "--socket", "waylandtasksmodeltest"], env=environ, stderr=subprocess.PIPE)
-
-    for _ in range(20):
-        kwin_window_shown: bool = False
-
-        while kwin_process.stderr.readable():
-            line: str = kwin_process.stderr.readline().decode(encoding="utf-8")
-            print(line)
-            if "Mesa version" in line or "successfully initialized" in line:
-                kwin_window_shown = True
-                break
-
-        if kwin_window_shown:
-            break
-        time.sleep(0.5)
-
-    assert kwin_process.poll() is None
-
-    test_environ = os.environ.copy()
-    test_environ["QT_QPA_PLATFORM"] = "wayland"
-    test_environ["WAYLAND_DISPLAY"] = "waylandtasksmodeltest"
-    test_process = subprocess.Popen([test_executable_path], env=test_environ)
-    assert test_process.poll() is None
+    kwin_process = subprocess.Popen(["kwin_wayland", "--virtual", "--no-lockscreen", "--no-global-shortcuts", "--no-kactivities", "--exit-with-session", test_executable_path], env=environ)
 
     result: int = 1
     try:
-        result = test_process.wait(timeout=60)
+        result = kwin_process.wait(timeout=60)
     except subprocess.TimeoutExpired as e:
         print("Timeout", e)
     finally:
