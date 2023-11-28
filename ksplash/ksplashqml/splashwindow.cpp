eeb7823caa1a6c6992db143cfa7197cad6bea6b0
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2023-11-28 09:45:53 +0200

    ksplash: Set explicit layer shell keyboard interactivity
    It makes code more expicit about the desired behavior.

--- ksplash/ksplashqml/splashwindow.cpp
+++ ksplash/ksplashqml/splashwindow.cpp
@@ -37,6 +37,7 @@ SplashWindow::SplashWindow(bool testing, bool window, const QString &theme, QScr
             layerShellWindow->setScope(QStringLiteral("ksplashqml"));
             layerShellWindow->setLayer(LayerShellQt::Window::LayerOverlay);
             layerShellWindow->setExclusiveZone(-1);
+            layerShellWindow->setKeyboardInteractivity(LayerShellQt::Window::KeyboardInteractivityExclusive);
         }
     }
 
