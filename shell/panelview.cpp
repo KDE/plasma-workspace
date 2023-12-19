9cc3da6e480f48d350e8d89723bc336f6655ebb2
David Redondo <kde@david-redondo.de>
2023-12-19 15:46:59 +0100

    Give fit content panels a better minimum size
    A panel could become shorter than thick which doesn't look good.
    This makes it at least as long as thick as a minimum size.

--- shell/panelview.cpp
+++ shell/panelview.cpp
@@ -679,9 +679,9 @@ void PanelView::resizePanel()
     } else if (m_lengthMode == PanelView::LengthMode::FitContent) {
         if (formFactor() == Plasma::Types::Vertical) {
             s = QSize(totalThickness(),
-                      std::max(10, std::min(m_screenToFollow->size().height(), m_contentLength + m_topFloatingPadding + m_bottomFloatingPadding)));
+                      std::max(totalThickness(), std::min(m_screenToFollow->size().height(), m_contentLength + m_topFloatingPadding + m_bottomFloatingPadding)));
         } else {
-            s = QSize(std::max(10, std::min(m_screenToFollow->size().width(), m_contentLength + m_leftFloatingPadding + m_rightFloatingPadding)),
+            s = QSize(std::max(totalThickness(), std::min(m_screenToFollow->size().width(), m_contentLength + m_leftFloatingPadding + m_rightFloatingPadding)),
                       totalThickness());
         }
         setMinimumSize(QSize(10, 10));
