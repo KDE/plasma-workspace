7c169d1410dbf695d633dd8dfd4f88024518d1f1
Ismael Asensio <isma.af@gmail.com>
2023-12-27 19:01:40 +0100

    dataengines/weather: Specify forecast link for wetter.com
    Currently 'wetter.com' doesn't provide a link to the place
    forecast, just to their homepage.
    
    We can build and offer such link using the place id, so the
    behavior is similar to other providers.
    
    BUG: 449933
    FIXED-IN: 5.91.90

--- dataengines/weather/ions/wetter.com/ion_wettercom.cpp
+++ dataengines/weather/ions/wetter.com/ion_wettercom.cpp
@@ -704,7 +704,7 @@ void WetterComIon::updateWeather(const QString &source, bool parseError)
         data.insert(QStringLiteral("Total Weather Days"), i);
 
         data.insert(QStringLiteral("Credit"), weatherData.credits); // FIXME i18n?
-        data.insert(QStringLiteral("Credit Url"), weatherData.creditsUrl);
+        data.insert(QStringLiteral("Credit Url"), QStringLiteral("%1/place/%2.html").arg(weatherData.creditsUrl).arg(placeInfo.placeCode));
 
         qCDebug(IONENGINE_WETTERCOM) << "updated weather data:" << weatherSource << data;
     } else {
