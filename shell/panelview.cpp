541056c8cd251a3efc2d13ab0638216253bd6ca4
Marco Martin <notmart@gmail.com>
2024-02-06 08:13:36 +0000

    Fix Panel renouncing focus
    On x11, removing the acceptfocus doesn't actually remove the focus from
    the window
    Use also here m_corona->restorePreviousWindow() to focus the old window
    
    This is a more effective bugfix thanthe ugly workaround present in the
    Panel.qml shell package
    
    See https://invent.kde.org/plasma/plasma-desktop/-/merge_requests/2026
    
    CCBUG:472909

--- shell/panelview.cpp
+++ shell/panelview.cpp
@@ -1516,11 +1516,15 @@ void PanelView::refreshStatus(Plasma::Types::ItemStatus status)
     if (status == Plasma::Types::NeedsAttentionStatus) {
         showTemporarily();
         setFlags(flags() | Qt::WindowDoesNotAcceptFocus);
+        if (isActive()) {
+            m_corona->restorePreviousWindow();
+        }
         if (m_layerWindow) {
             m_layerWindow->setKeyboardInteractivity(LayerShellQt::Window::KeyboardInteractivityNone);
             requestUpdate();
         }
     } else if (status == Plasma::Types::AcceptingInputStatus) {
+        m_corona->savePreviousWindow();
         setFlags(flags() & ~Qt::WindowDoesNotAcceptFocus);
 
         if (KWindowSystem::isPlatformX11()) {
@@ -1533,8 +1537,6 @@ void PanelView::refreshStatus(Plasma::Types::ItemStatus status)
             m_layerWindow->setKeyboardInteractivity(LayerShellQt::Window::KeyboardInteractivityOnDemand);
             requestUpdate();
         }
-
-        m_corona->savePreviousWindow();
 
         const auto nextItem = rootObject()->nextItemInFocusChain();
         if (nextItem) {
