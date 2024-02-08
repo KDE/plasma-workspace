2510a22b1ede937a225e058ade434e49e900d29d
Fushan Wen <qydwhotmail@gmail.com>
2024-02-08 09:02:46 +0800

    libkmpris: also find i18n strings in subdirectories
    This adds a few missing i18n strings that should have been presented in Plasma
    6.0.
    
    BUG: 481010
    FIXED-IN: 6.0
    CCMAIL: kde-i18n-doc@kde.org

--- libkmpris/Messages.sh
+++ libkmpris/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 # SPDX-FileCopyrightText: 2023 Fushan Wen <qydwhotmail@gmail.com>
 # SPDX-License-Identifier: BSD-2-Clause
-$XGETTEXT *.cpp -o $podir/libkmpris.pot
+$XGETTEXT `find . -name \*.cpp` -o $podir/libkmpris.pot
