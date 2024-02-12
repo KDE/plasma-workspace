bbe3d49816a97f39fd1df986df1a1e1aa4277481
Fushan Wen <qydwhotmail@gmail.com>
2024-02-12 20:34:17 +0800

    appiumtests/notificationstest: ensure malformed image data can't cause a crash
    CCBUG: 481033
    
    
    (cherry picked from commit 5e964798da63304cb97b3647f71f893a7be4be3e)

--- appiumtests/notificationstest.py
+++ appiumtests/notificationstest.py
@@ -9,6 +9,9 @@ from typing import Final
 from appium import webdriver
 from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
+from gi.repository import Gio, GLib
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 WIDGET_ID: Final = "org.kde.plasma.notifications"
 
@@ -49,6 +52,29 @@ class NotificationsTest(unittest.TestCase):
         Tests the widget can be opened
         """
         self.driver.find_element(AppiumBy.NAME, "Do not disturb")
+
+    def test_1_bug481033_malformed_image_data(self) -> None:
+        """
+        If `image-data` is not a structure, the broken image hint would crash plasmashell.
+        """
+        app_name = "Appium Test"
+        replaces_id = 0
+        app_icon = "wayland"
+        summary = "Bug481033"
+        body = "Will it crash"
+        actions: list[str] = []
+        hints: dict[str, GLib.Variant] = {
+            "desktop-entry": GLib.Variant("s", "systemsettings"),
+            "image-data": GLib.Variant("s", "not a structure"),
+        }
+        timeout = -1
+        parameters = GLib.Variant("(susssasa{sv}i)", [app_name, replaces_id, app_icon, summary, body, actions, hints, timeout])
+
+        session_bus: Gio.DBusConnection = Gio.bus_get_sync(Gio.BusType.SESSION)
+        session_bus.call_sync("org.freedesktop.Notifications", "/org/freedesktop/Notifications", "org.freedesktop.Notifications", "Notify", parameters, None, Gio.DBusSendMessageFlags.NONE, 1000)
+
+        wait = WebDriverWait(self.driver, 5)
+        wait.until(EC.presence_of_element_located((AppiumBy.NAME, summary)))
 
 
 if __name__ == '__main__':
