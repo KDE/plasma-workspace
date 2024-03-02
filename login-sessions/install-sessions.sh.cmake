2287ebba765fc1134ec8331e440233befea532a3
Jin Liu <m.liu.jin@gmail.com>
2024-03-02 11:16:44 +0800

    Verify that install prefix is named "usr"
    Because we use the parent dir of prefix as the image root
    for systemd-sysext. So if it's named "kde", then systemd-sysext
    won't find a "usr" dir in image, and consider the image invalid.
    
    We can't build a image by symlinking the prefix to "usr" in
    image dir, because systemd-sysext doesn't follow links inside image
    for security concerns:
    https://github.com/systemd/systemd/issues/31523
    
    Also, moved the check for systemd to the beginning of the script,
    so all checks are done before actually modify anything.

--- login-sessions/install-sessions.sh.cmake
+++ login-sessions/install-sessions.sh.cmake
@@ -7,13 +7,30 @@
 
 set -ex
 
+# - polkit exclusively looks in the system prefix and has no facilities to change that
+#   https://gitlab.freedesktop.org/polkit/polkit/-/blob/92b910ce2273daf6a76038f6bd764fa6958d4e8e/src/polkitbackend/polkitbackendinteractiveauthority.c#L302
+# - dbus exclusively looks in the system prefix for **system** services and offers no facilities to change that
+#   https://gitlab.freedesktop.org/dbus/dbus/-/blob/9722d621497b2e7324e696f4095f56e2a9307a7e/bus/activation-helper.c#
+if [ ! -x /usr/bin/systemd-sysext ]; then
+    echo 'Only systemd based systems are supported. You need systemd-sysext on your system or figure things out for yourself.'
+    exit 1
+fi
+
+# https://github.com/systemd/systemd/issues/31523
+if [ $(basename @CMAKE_INSTALL_PREFIX@) != "usr" ]; then
+    echo "The basename of your install prefix must be 'usr' to be compatible with the systemd-sysext setup."
+    echo "This is a limitation of the systemd-sysext implementation."
+    echo "If you are using kdesrc-build or kde-builder, please set the install-dir option to paths like ~/kde/usr, /usr/local/kde/usr, etc."
+    exit 1
+fi
+
 if [ "$(id -u)" != "0" ]; then
   install @CMAKE_BINARY_DIR@/prefix.sh @CMAKE_INSTALL_FULL_LIBEXECDIR@/plasma-dev-prefix.sh
-  install @CMAKE_CURRENT_BINARY_DIR@/startplasma-dev.sh @CMAKE_INSTALL_FULL_LIBEXECDIR@
+  install @CMAKE_CURRENT_BINARY_DIR@/startplasma-dev.sh --target-directory=@CMAKE_INSTALL_FULL_LIBEXECDIR@
   install --mode=755 -D @CMAKE_CURRENT_SOURCE_DIR@/plasma-systemd-sysext-merge --target-directory=@CMAKE_INSTALL_FULL_LIBEXECDIR@
   install --mode=755 -D @CMAKE_CURRENT_SOURCE_DIR@/plasma-systemd-sysext-unmerge --target-directory=@CMAKE_INSTALL_FULL_LIBEXECDIR@
-  install --mode=755 @CMAKE_CURRENT_BINARY_DIR@/run-unmerged @CMAKE_INSTALL_FULL_BINDIR@
-  install --mode=755 @CMAKE_CURRENT_SOURCE_DIR@/run-unmerged-script @CMAKE_INSTALL_FULL_LIBEXECDIR@
+  install --mode=755 @CMAKE_CURRENT_BINARY_DIR@/run-unmerged --target-directory=@CMAKE_INSTALL_FULL_BINDIR@
+  install --mode=755 @CMAKE_CURRENT_SOURCE_DIR@/run-unmerged-script --target-directory=@CMAKE_INSTALL_FULL_LIBEXECDIR@
   exec pkexec @CMAKE_INSTALL_FULL_BINDIR@/run-unmerged $(dirname "$0")/install-sessions.sh
 fi
 
@@ -24,25 +41,16 @@ if [ -d /opt/kde-dbus-scripts/ ]; then
   rm -f /etc/dbus-1/session.d/00-plasma.conf
 fi
 
-# - polkit exclusively looks in the system prefix and has no facilities to change that
-#   https://gitlab.freedesktop.org/polkit/polkit/-/blob/92b910ce2273daf6a76038f6bd764fa6958d4e8e/src/polkitbackend/polkitbackendinteractiveauthority.c#L302
-# - dbus exclusively looks in the system prefix for **system** services and offers no facilities to change that
-#   https://gitlab.freedesktop.org/dbus/dbus/-/blob/9722d621497b2e7324e696f4095f56e2a9307a7e/bus/activation-helper.c#
-if [ -x /usr/bin/systemd-sysext ]; then
-  install --mode=644 -D @CMAKE_CURRENT_SOURCE_DIR@/extension-release.plasma-dev6 --target-directory=@CMAKE_INSTALL_PREFIX@/lib/extension-release.d
-  install --mode=644 -D @CMAKE_CURRENT_SOURCE_DIR@/extension-release.plasma-dev6 --target-directory=@CMAKE_INSTALL_PREFIX@/etc/extension-release.d
+install --mode=644 -D @CMAKE_CURRENT_SOURCE_DIR@/extension-release.plasma-dev6 --target-directory=@CMAKE_INSTALL_PREFIX@/lib/extension-release.d
+install --mode=644 -D @CMAKE_CURRENT_SOURCE_DIR@/extension-release.plasma-dev6 --target-directory=@CMAKE_INSTALL_PREFIX@/etc/extension-release.d
 
-  prefix=/var/local/extensions-plasma-dev6
-  rm -rf $prefix
-  ln -sf @CMAKE_INSTALL_PREFIX@/../ $prefix
+prefix=/var/local/extensions-plasma-dev6
+rm -rf $prefix
+ln -sf @CMAKE_INSTALL_PREFIX@/../ $prefix
 
-  # Let us pkexec our merge and unmerge
-  install --mode=644 -D @CMAKE_CURRENT_BINARY_DIR@/org.kde.plasma.systemd-sysext.policy --target-directory=/usr/share/polkit-1/actions
+# Let us pkexec our merge and unmerge
+install --mode=644 -D @CMAKE_CURRENT_BINARY_DIR@/org.kde.plasma.systemd-sysext.policy --target-directory=/usr/share/polkit-1/actions
 
-  # Make built-from-source sessions appear in login screen
-  install --mode=644 -D @CMAKE_CURRENT_BINARY_DIR@/plasmax11-dev6.desktop --target-directory=/usr/local/share/xsessions
-  install --mode=644 -D @CMAKE_CURRENT_BINARY_DIR@/plasmawayland-dev6.desktop --target-directory=/usr/local/share/wayland-sessions
-else # legacy compat
-  echo 'Only systemd based systems are supported. You need systemd-sysext on your system or figure things out for yourself.'
-fi
-
+# Make built-from-source sessions appear in login screen
+install --mode=644 -D @CMAKE_CURRENT_BINARY_DIR@/plasmax11-dev6.desktop --target-directory=/usr/local/share/xsessions
+install --mode=644 -D @CMAKE_CURRENT_BINARY_DIR@/plasmawayland-dev6.desktop --target-directory=/usr/local/share/wayland-sessions
