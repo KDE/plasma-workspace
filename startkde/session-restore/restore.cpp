6e0343c873df504d20926a6e2afc146ea0e14938
David Edmundson <kde@davidedmundson.co.uk>
2024-03-08 16:07:33 +0000

    session-restore: Avoid apps managed by ksmserver
    If a session is saved manually plasma-fallback-session-restore and
    ksmserver can contain the same entries. To avoid double-starting, the
    fallback-session-restore introspects the ksmserver config and skips
    accordingly.
    
    This also migrates to kcfg to keep future defaults in sync easier.
    
    It's not the best architecture, but it's intended as a fallback mechanism.
    
    CCBUG: 4009

--- startkde/session-restore/restore.cpp
+++ startkde/session-restore/restore.cpp
@@ -5,15 +5,18 @@
 SPDX-License-Identifier: LGPL-2.1-or-later
 */
 
+#include <QDir>
 #include <QGuiApplication>
 
+#include <KApplicationTrader>
 #include <KConfigGroup>
 #include <KIO/ApplicationLauncherJob>
+#include <KIO/DesktopExecParser>
 #include <KService>
 #include <KSharedConfig>
 
 #include "debug.h"
-#include "qdir.h"
+#include "smserversettings.h"
 
 using namespace Qt::StringLiterals;
 
@@ -23,9 +26,9 @@ int main(int argc, char *argv[])
     a.setDesktopSettingsAware(false);
     a.setApplicationName(u"plasmasessionrestore"_s);
 
-    KSharedConfig::Ptr ksmserverConfig = KSharedConfig::openConfig(u"ksmserverrc"_s);
-    if (ksmserverConfig->group(u"General"_s).readEntry("loginMode", u"restorePreviousLogout"_s) == u"emptySession"_s) {
-        return 0;
+    SMServerSettings ksmserverSettings;
+    if (ksmserverSettings.loginMode() == SMServerSettings::emptySession) {
+        return EXIT_SUCCESS;
     }
 
     KSharedConfig::Ptr config = KSharedConfig::openStateConfig();
@@ -44,6 +47,35 @@ int main(int argc, char *argv[])
         }
     }
 
+    QSet<QString> ksmserverAutostart;
+    // make a list of all .desktop files that ksmserver is going to launch
+    // then list
+
+    KConfigGroup sessionGroup;
+
+    if (ksmserverSettings.loginMode() == SMServerSettings::restorePreviousLogout) {
+        sessionGroup = ksmserverSettings.config()->group(u"Session: saved at previous logout"_s);
+    } else if (ksmserverSettings.loginMode() == SMServerSettings::restoreSavedSession) {
+        sessionGroup = ksmserverSettings.config()->group(u"Session: saved by user"_s);
+    }
+
+    int count = sessionGroup.readEntry("count", 0);
+    if (count < 0) {
+        qFatal() << "invalid config";
+    }
+    for (int i = 0; i < count; i++) {
+        const QString app = sessionGroup.readEntry(QStringLiteral("program%1").arg(QString::number(i + 1)), QString());
+
+        auto apps = KApplicationTrader::query([&app](const KService::Ptr &service) {
+            const QString binary = KIO::DesktopExecParser::executablePath(service->exec());
+            return !service->noDisplay() && !binary.isEmpty() && app.endsWith(binary);
+        });
+        if (!apps.isEmpty()) {
+            ksmserverAutostart.insert(apps.first()->desktopEntryName());
+        }
+    }
+
+    // finally look through our list and do the actual launching
     for (const QString &groupName : config->groupList()) {
         const QString appId = config->group(groupName).readEntry("appId");
         auto service = KService::serviceByMenuId(appId);
@@ -56,6 +88,11 @@ int main(int argc, char *argv[])
             continue;
         }
         if (autoStartFiles.contains(service->desktopEntryName())) {
+            qCDebug(FALLBACK_SESSION_RESTORE) << "skipping " << appId << "already started by autostart.";
+            continue;
+        }
+        if (ksmserverAutostart.contains(service->desktopEntryName())) {
+            qCDebug(FALLBACK_SESSION_RESTORE) << "skipping " << appId << "already started by ksmserver.";
             continue;
         }
 
