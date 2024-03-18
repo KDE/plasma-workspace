61b623f16abd6258f104548343315d56ed269186
Nicolas Fella <nicolas.fella@gmx.de>
2024-03-18 19:12:50 +0100

    [interactiveconsole] Fix syntax in Messages.sh
    (cherry picked from commit 596fef98ab3f618573909fcb53acb11fba0397ae)

--- interactiveconsole/Messages.sh
+++ interactiveconsole/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT "$(find . -name \*.cpp -o -name \*.h)" -o $podir/plasma_interactiveconsole.pot
+$XGETTEXT $(find . -name \*.cpp -o -name \*.h) -o $podir/plasma_interactiveconsole.pot
