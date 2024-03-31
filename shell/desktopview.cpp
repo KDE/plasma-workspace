2b10053ec878d915abd937256084dace2fd69d73
Fushan Wen <qydwhotmail@gmail.com>
2024-03-31 16:01:09 +0800

    DesktopView: remove duplicate `requestActivate`
    (cherry picked from commit 71af22ef2872e8548a2d25f1a68152ae585fd893)

--- shell/desktopview.cpp
+++ shell/desktopview.cpp
@@ -396,7 +396,6 @@ void DesktopView::showConfigurationInterface(Plasma::Applet *applet)
     m_configView->init();
     m_configView->setTransientParent(this);
     m_configView->show();
-    m_configView->requestActivate();
 
     auto window = qobject_cast<QWindow *>(m_configView);
     if (window && QX11Info::isPlatformX11()) {
