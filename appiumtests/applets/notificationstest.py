210bc4d1134844b5353b1974c5dbeb00563cd212
Fushan Wen <qydwhotmail@gmail.com>
2024-04-05 15:49:52 +0800

    appiumtests: force English locale in notificationstest
    (cherry picked from commit 26cc3ba5816a9e5a02e273618fd3fa390da35efb)

--- appiumtests/applets/notificationstest.py
+++ appiumtests/applets/notificationstest.py
@@ -31,6 +31,9 @@ class NotificationsTest(unittest.TestCase):
         options = AppiumOptions()
         options.set_capability("app", f"plasmawindowed -p org.kde.plasma.nano {WIDGET_ID}")
         options.set_capability("timeouts", {'implicit': 10000})
+        options.set_capability("environ", {
+            "LC_ALL": "en_US.UTF-8",
+        })
         cls.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', options=options)
 
     def tearDown(self) -> None:
