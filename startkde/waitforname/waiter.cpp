3976b2ff0e908ce8bf0a9001533fc23787f9b71c
David Edmundson <kde@davidedmundson.co.uk>
2024-04-08 22:15:36 +0100

    waitforname: Add timeout and option to wait for unregistration
    waitforname can then be used as a guard for when a service exits.

--- startkde/waitforname/waiter.cpp
+++ startkde/waitforname/waiter.cpp
@@ -34,42 +34,81 @@ Waiter::Waiter(int argc, char **argv)
     parser.addPositionalArgument(QStringLiteral("service"),
                                  QStringLiteral("Optionally listen for a service different than '%1'").arg(mService),
                                  QStringLiteral("[service]"));
+
+    QCommandLineOption timeoutOption(QStringList() << QStringLiteral("timeout"),
+                                     QStringLiteral("Timeout in seconds. Use -1 to disable"),
+                                     QStringLiteral("seconds"),
+                                     QString::number(dbusTimeoutSec));
+
+    QCommandLineOption unregisterModeOption(QStringList() << QStringLiteral("unregistered"),
+                                            QStringLiteral("Wait for service unregistration, instead of registration"));
+
+    parser.addOption(timeoutOption);
+    parser.addOption(unregisterModeOption);
     parser.process(*this);
 
     const QStringList args = parser.positionalArguments();
     if (!args.isEmpty()) {
         mService = args.at(0);
+    } else {
+        exit(-1);
+    }
+
+    bool intIsOk;
+    int timeout = parser.value(timeoutOption).toInt(&intIsOk);
+    if (!intIsOk) {
+        qCWarning(LOG_PLASMA) << "Invalid timeout value";
+        exit(-1);
+    }
+    if (timeout > 0) {
+        mTimeoutTimer.setSingleShot(true);
+        mTimeoutTimer.setInterval(timeout * 1000);
+        connect(&mTimeoutTimer, &QTimer::timeout, this, &Waiter::timeout);
+        mTimeoutTimer.start();
+    }
+
+    if (parser.isSet(unregisterModeOption)) {
+        mMode = WaitForUnregistration;
     }
 }
 
 bool Waiter::waitForService()
 {
     QDBusConnection sessionBus = QDBusConnection::sessionBus();
-
-    if (sessionBus.interface()->isServiceRegistered(mService)) {
-        qCDebug(LOG_PLASMA) << "WaitForName: Service" << mService << "is already registered";
-        return false;
-    }
-
-    qCDebug(LOG_PLASMA) << "WaitForName: Waiting for appearance of service" << mService << "for" << dbusTimeoutSec << "seconds";
-
     QDBusServiceWatcher *watcher = new QDBusServiceWatcher(this);
     watcher->setConnection(sessionBus);
-    watcher->setWatchMode(QDBusServiceWatcher::WatchForRegistration);
     watcher->addWatchedService(mService);
-    connect(watcher, &QDBusServiceWatcher::serviceRegistered, this, &Waiter::registered);
 
-    mTimeoutTimer.setSingleShot(true);
-    mTimeoutTimer.setInterval(dbusTimeoutSec * 1000);
-    connect(&mTimeoutTimer, &QTimer::timeout, this, &Waiter::timeout);
-    mTimeoutTimer.start();
+    if (mMode == Waiter::WaitForRegistration) {
+        if (sessionBus.interface()->isServiceRegistered(mService)) {
+            qCDebug(LOG_PLASMA) << "WaitForName: Service" << mService << "is already registered";
+            return false;
+        }
 
+        qCDebug(LOG_PLASMA) << "WaitForName: Waiting for appearance of service" << mService << "for" << dbusTimeoutSec << "seconds";
+        watcher->setWatchMode(QDBusServiceWatcher::WatchForRegistration);
+        connect(watcher, &QDBusServiceWatcher::serviceRegistered, this, &Waiter::registered);
+    } else {
+        if (!sessionBus.interface()->isServiceRegistered(mService)) {
+            qCDebug(LOG_PLASMA) << "WaitForName: Service" << mService << "is not yet registered";
+            return false;
+        }
+        watcher->setWatchMode(QDBusServiceWatcher::WatchForUnregistration);
+        connect(watcher, &QDBusServiceWatcher::serviceUnregistered, this, &Waiter::unregistered);
+    }
     return true;
 }
 
 void Waiter::registered()
 {
-    qCDebug(LOG_PLASMA) << "WaitForName: Service was registered after" << (dbusTimeoutSec - (mTimeoutTimer.remainingTime() / 1000)) << "seconds";
+    qCDebug(LOG_PLASMA) << "WaitForName: Service was registered after" << (mTimeoutTimer.interval() - (mTimeoutTimer.remainingTime() / 1000)) << "seconds";
+    mTimeoutTimer.stop();
+    exit(0);
+}
+
+void Waiter::unregistered()
+{
+    qCDebug(LOG_PLASMA) << "WaitForName: Service was unregistered after" << (mTimeoutTimer.interval() - (mTimeoutTimer.remainingTime() / 1000)) << "seconds";
     mTimeoutTimer.stop();
     exit(0);
 }
