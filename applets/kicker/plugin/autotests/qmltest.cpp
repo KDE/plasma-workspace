c963b1d6dead0a57c45f694dac0c162a56003af9
Fushan Wen <qydwhotmail@gmail.com>
2024-04-27 13:10:47 +0800

    applets/kicker: use qmltestrunner to run qml tests
    There is no need to build another qmltestrunner.

--- applets/kicker/plugin/autotests/qmltest.cpp
+++
@@ -1,8 +0,0 @@
-/*
-    SPDX-FileCopyrightText: 2020 David Edmundson <kde@davidedmundson.co.uk>
-
-    SPDX-License-Identifier: LGPL-2.0-or-later
-*/
-
-#include <QtQuickTest>
-QUICK_TEST_MAIN(Kicker)
