d7ca21690b389e79419bcd1a73cdb4eb910e5c30
Marco Martin <notmart@gmail.com>
2024-05-08 12:45:03 +0200

    don't accomodate exclusive zones

--- shell/panelview.cpp
+++ shell/panelview.cpp
@@ -73,6 +73,7 @@ PanelView::PanelView(ShellCorona *corona, QScreen *targetScreen, QWindow *parent
         m_layerWindow->setKeyboardInteractivity(LayerShellQt::Window::KeyboardInteractivityNone);
         m_layerWindow->setScope(QStringLiteral("dock"));
         m_layerWindow->setCloseOnDismissed(false);
+        m_layerWindow->setAccomodateExclusiveZones(false);
     }
     if (targetScreen) {
         setPosition(targetScreen->geometry().center());
