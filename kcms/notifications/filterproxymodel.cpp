0677da7899c8d8726c18d52e6aa1267014a84e54
Kai Uwe Broulik <kde@privat.broulik.de>
2024-05-09 15:28:20 +0200

    kcms/notifications: More thoroughly filter plasma_workspace
    Not only when there is a query...
    
    Because of recursive filtering, the entry would still be shown because
    its event children never have a notifyRcName and thus never get filtered.
    Ensure to check the parent notifyRcName, too.

--- kcms/notifications/filterproxymodel.cpp
+++ kcms/notifications/filterproxymodel.cpp
@@ -33,16 +33,22 @@ void FilterProxyModel::setQuery(const QString &query)
 
 bool FilterProxyModel::filterAcceptsRow(int source_row, const QModelIndex &source_parent) const
 {
-    if (m_query.isEmpty()) {
-        return true;
-    }
-
     const QModelIndex idx = sourceModel()->index(source_row, 0, source_parent);
 
     // We remove "Plasma Workspace" in the filter model, so it's not shown in the apps list
-    // but still addressable by model index in the root model ("System notificatons" settings).
-    if (idx.data(SourcesModel::NotifyRcNameRole).toString() == KCMNotifications::plasmaWorkspaceNotifyRcName()) {
+    // but still addressable by model index in the root model ("System notifications" settings).
+    QString notifyRcName = idx.data(SourcesModel::NotifyRcNameRole).toString();
+    // Recursive filtering would still show it because its events never have a notifyRcName.
+    if (notifyRcName.isEmpty() && source_parent.isValid()) {
+        notifyRcName = source_parent.data(SourcesModel::NotifyRcNameRole).toString();
+    }
+
+    if (notifyRcName == KCMNotifications::plasmaWorkspaceNotifyRcName()) {
         return false;
+    }
+
+    if (m_query.isEmpty()) {
+        return true;
     }
 
     const QString display = idx.data(Qt::DisplayRole).toString();
