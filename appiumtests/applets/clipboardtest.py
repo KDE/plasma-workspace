2796e85d0e4e98ab0345890d3c58847ea26c22f6
Fushan Wen <qydwhotmail@gmail.com>
2024-06-03 23:47:48 +0800

    appiumtests: test empty clip crashes klipper
    CCBUG: 487843
    CCBUG: 466414

--- appiumtests/applets/clipboardtest.py
+++ appiumtests/applets/clipboardtest.py
@@ -6,6 +6,8 @@
 
 import os
 import shutil
+import subprocess
+import sys
 import time
 import unittest
 from typing import Final
@@ -19,10 +21,15 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
-gi.require_version('Gdk', '3.0')
-from gi.repository import GLib
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, "utils"))
+from GLibMainLoopThread import GLibMainLoopThread
+
+gi.require_version('Gdk', '4.0')
+gi.require_version('Gtk', '4.0')
+from gi.repository import Gdk, GLib, Gtk
 
 WIDGET_ID: Final = "org.kde.plasma.clipboard"
+KDE_VERSION: Final = 6
 
 
 class ClipboardTest(unittest.TestCase):
@@ -31,6 +38,8 @@ class ClipboardTest(unittest.TestCase):
     """
 
     driver: webdriver.Remote
+    loop_thread: GLibMainLoopThread
+    appium_options: AppiumOptions
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -55,7 +64,11 @@ class ClipboardTest(unittest.TestCase):
             "QT_LOGGING_RULES": "qt.accessibility.atspi.warning=false;qt.qml.typeresolution.cycle.warning=false;kf.plasma.core.warning=false;kf.windowsystem.warning=false;kf.kirigami.platform.warning=false;org.kde.klipper.debug=true",
         })
         options.set_capability("timeouts", {'implicit': 10000})
+        cls.appium_options = options
         cls.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', options=options)
+
+        cls.loop_thread = GLibMainLoopThread()
+        cls.loop_thread.start()
 
     def tearDown(self) -> None:
         """
@@ -70,6 +83,7 @@ class ClipboardTest(unittest.TestCase):
         Make sure to terminate the driver again, lest it dangles.
         """
         cls.driver.quit()
+        cls.loop_thread.quit()
 
     def test_0_open(self) -> None:
         """
@@ -144,6 +158,33 @@ class ClipboardTest(unittest.TestCase):
         self.driver.find_element(AppiumBy.NAME, "Remove from history").click()
         # The first item becomes the current clipboard item
         self.assertEqual(self.driver.get_clipboard_text(), "Fushan Wen")
+
+    def test_3_bug487843_bug466414_empty_clip_crash(self) -> None:
+        """
+        When "Text selection - Always save in history" is enabled, a clip with empty text can crash klipper.
+        @see https://bugs.kde.org/show_bug.cgi?id=487843
+        @see https://bugs.kde.org/show_bug.cgi?id=466414
+        """
+        self.driver.quit()
+
+        # Enable "Text selection - Always save in history" to test the two bugs
+        subprocess.check_call([f"kwriteconfig{KDE_VERSION}", "--file", "klipperrc", "--group", "General", "--key", "IgnoreSelection", "false"])
+        subprocess.check_call([f"kwriteconfig{KDE_VERSION}", "--file", "klipperrc", "--group", "General", "--key", "SyncClipboards", "true"])
+
+        # Restart the widget to take effect
+        self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', options=self.appium_options)
+        self.driver.find_element(AppiumBy.NAME, "Fushan Wen")
+
+        window = Gtk.Window()
+        clipboard: Gdk.Clipboard = window.get_display().get_clipboard()
+        content_text = Gdk.ContentProvider.new_for_bytes("text/plain", GLib.Bytes.new(bytes("", "utf-8")))
+        # Clip data from Firefox have additional mime types, which cause the crash
+        content_application = Gdk.ContentProvider.new_for_bytes("application/whatever", GLib.Bytes.new(bytes("abc", "utf-8")))
+        content_union = Gdk.ContentProvider.new_union([content_text, content_application])
+        self.assertTrue(clipboard.set_content(content_union))
+        time.sleep(3)  # roundtrip
+        self.driver.find_element(AppiumBy.NAME, "Fushan Wen")  # Still alive
+        self.assertEqual(self.driver.get_clipboard_text(), "Fushan Wen")  # No change to the clipboard
 
 
 if __name__ == '__main__':
