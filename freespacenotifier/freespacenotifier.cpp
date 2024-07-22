KDE/plasma-workspace

commit 6af29669c2c211b981481da237d3919463fe323d

[freespacenotifier] impr: no notification for ro filesystems

freespacenotifier/module.cpp
@@ -31,12 +31,18 @@ FreeSpaceNotifierModule::FreeSpaceNotifierModule(QObject *parent, const QList<QV
     const QString rootPath = QStringLiteral("/");
     const QString homePath = QDir::homePath();
 
-    auto *homeNotifier = new FreeSpaceNotifier(homePath, ki18n("Your Home folder is running out of disk space, you have %1 MiB remaining (%2%)."), this);
-    connect(homeNotifier, &FreeSpaceNotifier::configureRequested, this, &FreeSpaceNotifierModule::showConfiguration);
+    const auto homeMountPoint = KMountPoint::currentMountPoints().findByPath(homePath);
+
+    if ( !homeMountPoint || !homeMountPoint->mountOptions().contains(QLatin1String("ro")) ) {
+        auto *homeNotifier = new FreeSpaceNotifier(homePath, ki18n("Your Home folder is running out of disk space, you have %1 MiB remaining (%2%)."), this);
+        connect(homeNotifier, &FreeSpaceNotifier::configureRequested, this, &FreeSpaceNotifierModule::showConfiguration);
+    }
 
     // If Home is on a separate partition from Root, warn for it, too.
-    auto homeMountPoint = KMountPoint::currentMountPoints().findByPath(homePath);
-    if (!homeMountPoint || homeMountPoint->mountPoint() != rootPath) {
+    if (KMountPoint::Ptr rootMountPoint; !homeMountPoint ||
+                                        ( homeMountPoint->mountPoint() != rootPath &&
+                                          ( !(rootMountPoint = KMountPoint::currentMountPoints().findByPath(rootPath)) ||
+                                            !rootMountPoint->mountOptions().contains(QLatin1String("ro")) ) ) ) {
         auto *rootNotifier = new FreeSpaceNotifier(rootPath, ki18n("Your Root partition is running out of disk space, you have %1 MiB remaining (%2%)."), this);
         connect(rootNotifier, &FreeSpaceNotifier::configureRequested, this, &FreeSpaceNotifierModule::showConfiguration);
     }

commit 028a9f5da9282c3ee9636e0246bbb2d2c08adff0

freespacenotifier: Rework to not warn for read only partitions
Rework using QCoro and enable exceptions only for this module.

Fixes: 6af29669c2 [freespacenotifier] impr: no notification for ro filesystems
See: https://pagure.io/fedora-kde/SIG/issue/539

freespacenotifier/CMakeLists.txt
@@ -2,6 +2,13 @@ add_definitions(-DTRANSLATION_DOMAIN=\"freespacenotifier\")
 
 set(kded_freespacenotifier_SRCS freespacenotifier.cpp module.cpp freespacenotifier.h module.h)
 
+ecm_qt_declare_logging_category(kded_freespacenotifier_SRCS
+    HEADER freespacenotifier_logging.h
+    IDENTIFIER FSN
+    CATEGORY_NAME org.kde.freespacenotifier
+    DESCRIPTION "Free space notifier kded module"
+)
+
 ki18n_wrap_ui(kded_freespacenotifier_SRCS freespacenotifier_prefs_base.ui)
 
 qt_add_dbus_interface(kded_freespacenotifier_SRCS ${KDED_DBUS_INTERFACE} kded_interface)
@@ -10,7 +17,11 @@ kconfig_add_kcfg_files(kded_freespacenotifier_SRCS settings.kcfgc)
 
 kcoreaddons_add_plugin(freespacenotifier SOURCES ${kded_freespacenotifier_SRCS} INSTALL_NAMESPACE "kf6/kded")
 
+kde_target_enable_exceptions(freespacenotifier PRIVATE)
+
 target_link_libraries(freespacenotifier
+    Qt6::Concurrent
+    QCoro::Core
     KF6::ConfigWidgets
     KF6::DBusAddons
     KF6::I18n

freespacenotifier/freespacenotifier.cpp
@@ -8,13 +8,19 @@
 */
 
 #include "freespacenotifier.h"
+#include "freespacenotifier_logging.h"
 
 #include <KNotification>
 #include <KNotificationJobUiDelegate>
 
 #include <KIO/ApplicationLauncherJob>
-#include <KIO/FileSystemFreeSpaceJob>
 #include <KIO/OpenUrlJob>
+
+#include <QStorageInfo>
+#include <QtConcurrent>
+
+#include <QCoroFuture>
+#include <QCoroTask>
 
 #include <chrono>
 
@@ -45,14 +51,40 @@ void FreeSpaceNotifier::checkFreeDiskSpace()
         return;
     }
 
-    auto *job = KIO::fileSystemFreeSpace(QUrl::fromLocalFile(m_path));
-    connect(job, &KJob::result, this, [this, job]() {
-        if (job->error()) {
+    if (m_checking) {
+        qCWarning(FSN) << "Obtaining storage info is taking a long while for" << m_path;
+        return;
+    }
+    m_checking = true;
+
+    // Load the QStorageInfo in a co-routine in case the filesystem is having performance issues.
+    auto future = QtConcurrent::run([path = m_path]() -> std::optional<QStorageInfo> {
+        QStorageInfo info(path);
+        if (!info.isValid()) {
+            qCWarning(FSN) << "Failed to obtain storage info for" << path;
+            return {};
+        }
+        if (!info.isReady()) {
+            qCWarning(FSN) << "Storage info is not ready for" << path;
+            return {};
+        }
+        return info;
+    });
+    QCoro::connect(std::move(future), this, [this](const auto &optionalInfo) {
+        m_checking = false;
+        if (!optionalInfo.has_value()) {
+            qCDebug(FSN) << "Empty QStorageInfo for" << m_path;
+            return;
+        }
+        const QStorageInfo &info = optionalInfo.value();
+        if (info.isReadOnly()) {
+            qCDebug(FSN) << "Not checking for free space for read only mount point" << m_path;
             return;
         }
 
         const int limit = FreeSpaceNotifierSettings::minimumSpace(); // MiB
-        const qint64 avail = job->availableSize() / (1024 * 1024); // to MiB
+        const qint64 avail = info.bytesAvailable() / (1024 * 1024); // to MiB
+        qCDebug(FSN) << "Available MiB for" << m_path << ":" << avail;
 
         if (avail >= limit) {
             if (m_notification) {
@@ -61,8 +93,9 @@ void FreeSpaceNotifier::checkFreeDiskSpace()
             return;
         }
 
-        const int availPercent = int(100 * job->availableSize() / job->size());
+        const int availPercent = int(100 * info.bytesAvailable() / info.bytesTotal());
         const QString text = m_notificationText.subs(avail).subs(availPercent).toString();
+        qCDebug(FSN) << "Available percentage for" << m_path << ":" << availPercent;
 
         // Make sure the notification text is always up to date whenever we checked free space
         if (m_notification) {

freespacenotifier/freespacenotifier.h
@@ -36,6 +36,9 @@ private:
     void exploreDrive();
     void onNotificationClosed();
 
+    // Only run one check at a time
+    bool m_checking = false;
+
     QString m_path;
     KLocalizedString m_notificationText;
 

freespacenotifier/module.cpp
@@ -31,17 +31,15 @@ FreeSpaceNotifierModule::FreeSpaceNotifierModule(QObject *parent, const QList<QV
     const QString rootPath = QStringLiteral("/");
     const QString homePath = QDir::homePath();
 
-    const auto homeMountPoint = KMountPoint::currentMountPoints().findByPath(homePath);
+    const QStorageInfo rootInfo(rootPath);
+    const QStorageInfo homeInfo(homePath);
 
-    if (!homeMountPoint || !homeMountPoint->mountOptions().contains(QLatin1String("ro"))) {
-        auto *homeNotifier = new FreeSpaceNotifier(homePath, ki18n("Your Home folder is running out of disk space, you have %1 MiB remaining (%2%)."), this);
-        connect(homeNotifier, &FreeSpaceNotifier::configureRequested, this, &FreeSpaceNotifierModule::showConfiguration);
-    }
+    // Always monitor home
+    auto *homeNotifier = new FreeSpaceNotifier(homePath, ki18n("Your Home folder is running out of disk space, you have %1 MiB remaining (%2%)."), this);
+    connect(homeNotifier, &FreeSpaceNotifier::configureRequested, this, &FreeSpaceNotifierModule::showConfiguration);
 
-    // If Home is on a separate partition from Root, warn for it, too.
-    if (KMountPoint::Ptr rootMountPoint; !homeMountPoint
-        || (homeMountPoint->mountPoint() != rootPath
-            && (!(rootMountPoint = KMountPoint::currentMountPoints().findByPath(rootPath)) || !rootMountPoint->mountOptions().contains(QLatin1String("ro"))))) {
+    // Monitor '/' when it is different from home
+    if (rootInfo != homeInfo) {
         auto *rootNotifier = new FreeSpaceNotifier(rootPath, ki18n("Your Root partition is running out of disk space, you have %1 MiB remaining (%2%)."), this);
         connect(rootNotifier, &FreeSpaceNotifier::configureRequested, this, &FreeSpaceNotifierModule::showConfiguration);
     }