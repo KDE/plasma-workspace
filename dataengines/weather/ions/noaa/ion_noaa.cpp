KDE/plasma-workspace

commit c8cd82cc860e8c9fc462c9155604d7d8fa12bdb7

dataengines/weather: Fix stalled data on weather updates
Due to the weather's ion and the DataEngine's infrastructure it was
not possible to remove outdated keys from the data, which resulted
in the applet showing stalled info that were no longer relevant, like
"wind gust" or the "feels like" indices.

The root cause is double:

- A dataengine's `setData()` will not remove keys that are no longer
  present in the new data, so we need to force removing them manually
  via `removeData()` or `removeAllData()`

- There are two concurrent dataengines involved:
  - The master `weather` engine which the applet is connected to and
    serves as a general interface
  - An internal `ion` engine loaded by the master which does the
    actual weather job

To fix the issue let's provide a signal for the ions to request
cleaning up the data on the master engine, and call it before setting
the new weather updates.

BUG: 481492
FIXED-IN: 6.2

dataengines/weather/ions/noaa/ion_noaa.cpp
@@ -200,7 +200,7 @@ void NOAAIon::getStationList()
 
 void NOAAIon::setUpStation(const QString &source)
 {
-    removeAllData(source);
+    Q_EMIT cleanUpData(source);
 
     QString dataKey = source;
     dataKey.remove(u"noaa|weather|"_s);
@@ -516,6 +516,7 @@ void NOAAIon::updateWeather(const QString &source)
 
     data.insert(u"Credit"_s, i18nc("credit line, keep string short)", "Data from NOAA National\302\240Weather\302\240Service"));
 
+    Q_EMIT cleanUpData(source);
     setData(source, data);
 }
 

dataengines/weather/ions/bbcukmet/ion_bbcukmet.cpp
@@ -1033,6 +1033,7 @@ void UKMETIon::updateWeather(const QString &source)
     data.insert(QStringLiteral("Credit"), i18nc("credit line, keep string short", "Data from BBC\302\240Weather"));
     data.insert(QStringLiteral("Credit Url"), place.forecastHTMLUrl);
 
+    Q_EMIT cleanUpData(source);
     setData(weatherSource, data);
 }
 

dataengines/weather/ions/dwd/ion_dwd.cpp
@@ -659,6 +659,7 @@ void DWDIon::updateWeather(const QString &source)
     data.insert(QStringLiteral("Credit"), i18nc("credit line, don't change name!", "Source: Deutscher Wetterdienst"));
     data.insert(QStringLiteral("Credit Url"), QStringLiteral("https://www.dwd.de/"));
 
+    Q_EMIT cleanUpData(source);
     setData(weatherSource, data);
 }
 

dataengines/weather/ions/envcan/ion_envcan.cpp
@@ -1562,6 +1562,8 @@ void EnvCanadaIon::updateWeather(const QString &source)
 
     data.insert(QStringLiteral("Credit"), i18nc("credit line, keep string short", "Data from Environment and Climate Change\302\240Canada"));
     data.insert(QStringLiteral("Credit Url"), weatherData.creditUrl);
+
+    Q_EMIT cleanUpData(source);
     setData(source, data);
 
     qCDebug(IONENGINE_ENVCAN) << "Updated weather:" << source << data;

dataengines/weather/ions/ion.cpp
@@ -27,6 +27,7 @@ IonInterface::IonInterface(QObject *parent)
     : Plasma5Support::DataEngine(parent)
     , d(new Private(this))
 {
+    connect(this, &IonInterface::cleanUpData, this, &IonInterface::removeAllData);
 }
 
 IonInterface::~IonInterface()

dataengines/weather/ions/ion.h
@@ -205,6 +205,10 @@ public Q_SLOTS:
 
 Q_SIGNALS:
     void forceUpdate(IonInterface *ion, const QString &source);
+    /**
+     * Provides a way for the ion to request clean-up data in the Wether Engine
+     */
+    void cleanUpData(const QString &source);
 
 protected:
     /**

dataengines/weather/ions/wetter.com/ion_wettercom.cpp
@@ -697,6 +697,7 @@ void WetterComIon::updateWeather(const QString &source, bool parseError)
         qCDebug(IONENGINE_WETTERCOM) << "Something went wrong when parsing weather data for source:" << source << placeInfo.displayName;
     }
 
+    Q_EMIT cleanUpData(source);
     setData(weatherSource, data);
 }
 

dataengines/weather/weatherengine.cpp
@@ -74,6 +74,8 @@ void WeatherEngine::removeIonSource(const QString &source)
                 // forget about it
                 m_ionUsage.erase(it);
                 disconnect(ion, &IonInterface::forceUpdate, this, &WeatherEngine::forceUpdate);
+                disconnect(ion, &IonInterface::cleanUpData, this, &WeatherEngine::removeAllData);
+
                 qCDebug(WEATHER) << "Ion no longer used as source:" << ionName;
             } else {
                 --(it.value());
@@ -111,6 +113,8 @@ bool WeatherEngine::sourceRequestEvent(const QString &source)
     if (it == m_ionUsage.end()) {
         m_ionUsage.insert(ionName, 1);
         connect(ion, &IonInterface::forceUpdate, this, &WeatherEngine::forceUpdate);
+        connect(ion, &IonInterface::cleanUpData, this, &WeatherEngine::removeAllData);
+
         qCDebug(WEATHER) << "Ion now used as source:" << ionName;
     } else {
         ++(*it);

commit 314a0fb5960f3e972fbd29148d43b0362ed63354

dataengines/weather/noaa: Improve handling of server errors
This makes the ion more robust against wrong updates because, on any
server error on the chain, we will keep the previous data instead of
cleaning everything up and display an awful timeout message.

Also improve the logging of received data and errors to help future
debugging on these issues

Fixes a bug unintentionally introduced in c8cd82cc86 because the
reset mechanism didn't actually work before

dataengines/weather/ions/noaa/ion_noaa.cpp
@@ -155,16 +155,23 @@ KJob *NOAAIon::requestAPIJob(const QString &source, const QUrl &url, Callback on
     }
 
     connect(getJob, &KJob::result, this, [this, source, onResult](KJob *job) {
-        if (!job->error()) {
-            QJsonDocument doc = QJsonDocument::fromJson(m_jobData.value(job));
-            if (!doc.isEmpty()) {
-                (this->*onResult)(source, doc);
-            }
-        } else {
+        if (job->error()) {
             qCWarning(IONENGINE_NOAA) << "Error retrieving data" << job->errorText();
+            m_jobData.remove(job);
+            return;
         }
 
+        QJsonParseError jsonError;
+        QJsonDocument doc = QJsonDocument::fromJson(m_jobData.value(job), &jsonError);
         m_jobData.remove(job);
+
+        if (doc.isNull()) {
+            qCWarning(IONENGINE_NOAA) << "Received invalid JSON data:" << jsonError.errorString();
+            return;
+        }
+
+        // Invoke callback method
+        (this->*onResult)(source, doc);
     });
 
     return getJob;
@@ -200,8 +207,6 @@ void NOAAIon::getStationList()
 
 void NOAAIon::setUpStation(const QString &source)
 {
-    Q_EMIT cleanUpData(source);
-
     QString dataKey = source;
     dataKey.remove(u"noaa|weather|"_s);
     // If this is empty we have no valid data, send out an error and abort.
@@ -319,16 +324,14 @@ void NOAAIon::getObservation(const QString &source)
 
 void NOAAIon::readObservation(const QString &source, const QJsonDocument &doc)
 {
-    WeatherData::Observation &data = m_weatherData[source].observation;
-
-    if (doc.isEmpty()) {
-        return;
-    }
-
     const QJsonValue properties = doc[u"properties"_s];
     if (!properties.isObject()) {
+        qCWarning(IONENGINE_NOAA) << "Malformed observation report" << doc;
         return;
     }
+
+    WeatherData::Observation &data = m_weatherData[source].observation;
+    data = WeatherData::Observation{};
 
     data.weather = properties[u"textDescription"_s].toString();
     data.timestamp = QDateTime::fromString(properties[u"timestamp"_s].toString(), Qt::ISODate);
@@ -345,6 +348,8 @@ void NOAAIon::readObservation(const QString &source, const QJsonDocument &doc)
     data.dewpoint_F = parseQV(properties[u"dewpoint"_s], Fahrenheit);
     data.heatindex_F = parseQV(properties[u"heatIndex"_s], Fahrenheit);
     data.windchill_F = parseQV(properties[u"windChill"_s], Fahrenheit);
+
+    qCDebug(IONENGINE_NOAA) << "Received observation data:" << data.timestamp << data.weather;
 
     Q_EMIT observationUpdated(source);
 }
@@ -518,6 +523,8 @@ void NOAAIon::updateWeather(const QString &source)
 
     Q_EMIT cleanUpData(source);
     setData(source, data);
+
+    qCDebug(IONENGINE_NOAA) << "Updated weather data for" << source;
 }
 
 /**
@@ -758,20 +765,16 @@ void NOAAIon::getForecast(const QString &source)
 void NOAAIon::readForecast(const QString &source, const QJsonDocument &doc)
 {
     WeatherData &weatherData = m_weatherData[source];
-    QList<WeatherData::Forecast> &forecasts = weatherData.forecasts;
-
-    // Clear the current forecasts
-    forecasts.clear();
     weatherData.isForecastsDataPending = false;
-
-    if (doc.isEmpty()) {
-        return;
-    }
 
     const QJsonValue properties = doc[u"properties"_s];
     if (!properties.isObject()) {
+        qCWarning(IONENGINE_NOAA) << "Malformed forecast report" << doc;
         return;
     }
+
+    QList<WeatherData::Forecast> &forecasts = weatherData.forecasts;
+    forecasts.clear();
 
     const QJsonArray periods = properties[u"periods"_s].toArray();
     forecasts.reserve(periods.count());
@@ -811,6 +814,9 @@ void NOAAIon::readForecast(const QString &source, const QJsonDocument &doc)
         forecasts << forecast;
     }
 
+    qCDebug(IONENGINE_NOAA) << "Received forecast data:" << forecasts.count() << "periods."
+                            << "Starts at night:" << (!forecasts.isEmpty() && !forecasts.first().isDayTime);
+
     updateWeather(source);
 }
 
@@ -829,12 +835,9 @@ void NOAAIon::getPointsInfo(const QString &source)
 
 void NOAAIon::readPointsInfo(const QString &source, const QJsonDocument &doc)
 {
-    if (doc.isEmpty()) {
-        return;
-    }
-
     const auto properties = doc[u"properties"_s];
     if (!properties.isObject()) {
+        qCWarning(IONENGINE_NOAA) << "Malformed points information" << doc;
         return;
     }
 
@@ -906,10 +909,6 @@ QString formatAlertDescription(QString description)
 
 void NOAAIon::readAlerts(const QString &source, const QJsonDocument &doc)
 {
-    if (doc.isEmpty()) {
-        return;
-    }
-
     auto &alerts = m_weatherData[source].alerts;
     alerts.clear();
 