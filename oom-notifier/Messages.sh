cc1be0007d813d72dcee70439d617065f9b95b56
Albert Astals Cid <aacid@kde.org>
2024-10-29 19:34:46 +0100

    Remove xgettext call over non existing files that generates error

--- oom-notifier/Messages.sh
+++ oom-notifier/Messages.sh
@@ -8,6 +8,3 @@
 podir=${podir:?} # ensure it is defined
 
 $XGETTEXT $(find . -name \*.cpp -o -name \*.h) -o "$podir"/oom-notifier.pot
-# Extract JavaScripty files as what they are, otherwise for example template literals won't work correctly (by default we extract as C++).
-# https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-$XGETTEXT --join-existing --language=JavaScript $(find . -name \*.qml -o -name \*.js) -o "$podir"/oom-notifier.pot
