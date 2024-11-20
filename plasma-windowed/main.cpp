a23432f218f9a4094f31e31ea1c109464cb5bf04
Heiko Becker <heiko.becker@kde.org>
2024-11-20 22:06:20 +0100

    plasmawindowed: Use shared contexts analogue to plasmashell
    This fixes loading applets using QtWebEngine, which otherwise may
    complain about something like this:
    
    "QtWebEngineQuick::initialize() must be called from the Qt gui thread."
    
    According to Qt's docs [1] setting the Qt::AA_ShareOpenGLContexts
    attribute before constructing a QGuiApplication, has the same effect as
    QtWebEngineQuick::initialize() and avoids pulling in the humongous
    dependency.
    
    For plasmashell it has been introduced with
    6c1c6b86db07348b636c39ee344a12570e4e322c.
    
    CCBUG: 495758
    
    [1] https://doc.qt.io/qt-6/qtwebenginequick.html

--- plasma-windowed/main.cpp
+++ plasma-windowed/main.cpp
@@ -29,6 +29,7 @@ int main(int argc, char **argv)
         QQmlDebuggingEnabler::enableDebugging(true);
     }
 #endif
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
 
     QQuickWindow::setDefaultAlphaBuffer(true);
 
