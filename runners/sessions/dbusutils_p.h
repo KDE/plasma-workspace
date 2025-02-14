a6bdcfbe302bf492c84f4f1808f5f3c56a18d28e
Marco Martin <notmart@gmail.com>
2025-02-14 17:17:45 +0100

    Make the session runner a remote one
    using KMessageBox is very problematic to be invoked in a QML application
    such as KRunner or plasma-desktop
    
    makes it safer if it's a completely remote one working in own process

---
+++ runners/sessions/dbusutils_p.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <KRunner/QueryMatch>
+#include <QDBusArgument>
+#include <QList>
+#include <QString>
+#include <QVariantMap>
+
+struct RemoteMatch {
+    // sssuda{sv}
+    QString id;
+    QString text;
+    QString iconName;
+    int categoryRelevance = qToUnderlying(KRunner::QueryMatch::CategoryRelevance::Lowest);
+    qreal relevance = 0;
+    QVariantMap properties;
+};
+
+typedef QList<RemoteMatch> RemoteMatches;
+
+struct RemoteAction {
+    QString id;
+    QString text;
+    QString iconName;
+};
+
+typedef QList<RemoteAction> RemoteActions;
+
+inline QDBusArgument &operator<<(QDBusArgument &argument, const RemoteMatch &match)
+{
+    argument.beginStructure();
+    argument << match.id;
+    argument << match.text;
+    argument << match.iconName;
+    argument << match.categoryRelevance;
+    argument << match.relevance;
+    argument << match.properties;
+    argument.endStructure();
+    return argument;
+}
+
+inline const QDBusArgument &operator>>(const QDBusArgument &argument, RemoteMatch &match)
+{
+    argument.beginStructure();
+    argument >> match.id;
+    argument >> match.text;
+    argument >> match.iconName;
+    argument >> match.categoryRelevance;
+    argument >> match.relevance;
+    argument >> match.properties;
+    argument.endStructure();
+
+    return argument;
+}
+
+inline QDBusArgument &operator<<(QDBusArgument &argument, const RemoteAction &action)
+{
+    argument.beginStructure();
+    argument << action.id;
+    argument << action.text;
+    argument << action.iconName;
+    argument.endStructure();
+    return argument;
+}
+
+inline const QDBusArgument &operator>>(const QDBusArgument &argument, RemoteAction &action)
+{
+    argument.beginStructure();
+    argument >> action.id;
+    argument >> action.text;
+    argument >> action.iconName;
+    argument.endStructure();
+    return argument;
+}
--- runners/sessions/sessionrunner.cpp
+++ runners/sessions/sessionrunner.cpp
@@ -7,63 +7,82 @@
 
 #include "sessionrunner.h"
 
+#include <QApplication>
+
+#include <KAboutData>
+#include <KCrash>
 #include <KGuiItem>
 #include <KLocalizedString>
 #include <KMessageBox>
 #include <KSharedConfig>
 
-K_PLUGIN_CLASS_WITH_JSON(SessionRunner, "plasma-runner-sessions.json")
+#include "config-workspace.h"
+#include "dbusutils_p.h"
 
 using namespace Qt::StringLiterals;
 
-SessionRunner::SessionRunner(QObject *parent, const KPluginMetaData &metaData)
-    : KRunner::AbstractRunner(parent, metaData)
+int main(int argc, char **argv)
+{
+    QCoreApplication::setAttribute(Qt::AA_DisableSessionManager);
+    QApplication::setQuitOnLastWindowClosed(false);
+    QApplication app(argc, argv); // KRun needs widgets for error message boxes
+
+    KAboutData about(QStringLiteral("sessionrunner"), QString(), QStringLiteral(WORKSPACE_VERSION_STRING));
+    KAboutData::setApplicationData(about);
+
+    KCrash::initialize();
+
+    SessionRunner r;
+    return app.exec();
+}
+
+SessionRunner::SessionRunner(QObject *parent)
+    : QObject(parent)
 {
     m_logoutKeywords = i18nc("KRunner keywords (split by semicolons without whitespace) to log out of the session", "logout;log out")
                            .split(QLatin1Char(';'), Qt::SkipEmptyParts);
     if (m_session.canLogout()) {
-        addSyntax(m_logoutKeywords, i18n("Logs out, exiting the current desktop session"));
+        // TODO addSyntax(m_logoutKeywords, i18n("Logs out, exiting the current desktop session"));
     }
 
     m_shutdownKeywords = i18nc("KRunner keywords (split by semicolons without whitespace) to shut down the computer", "shutdown;shut down;power;power off")
                              .split(QLatin1Char(';'), Qt::SkipEmptyParts);
     if (m_session.canShutdown()) {
-        addSyntax(m_shutdownKeywords, i18n("Turns off the computer"));
+        // TODO addSyntax(m_shutdownKeywords, i18n("Turns off the computer"));
     }
 
     m_restartKeywords = i18nc("KRunner keywords (split by semicolons without whitespace) to restart the computer", "restart;reboot")
                             .split(QLatin1Char(';'), Qt::SkipEmptyParts);
     if (m_session.canReboot()) {
-        addSyntax(m_restartKeywords, i18n("Reboots the computer"));
+        // TODO addSyntax(m_restartKeywords, i18n("Reboots the computer"));
     }
 
     m_lockKeywords =
         i18nc("KRunner keywords (split by semicolons without whitespace) to lock the screen", "lock;lock screen").split(QLatin1Char(';'), Qt::SkipEmptyParts);
     if (m_session.canLock()) {
-        addSyntax(m_lockKeywords, i18n("Locks the current sessions and starts the screen saver"));
+        // TODO addSyntax(m_lockKeywords, i18n("Locks the current sessions and starts the screen saver"));
     }
 
     m_saveKeywords = i18nc("KRunner keywords (split by semicolons without whitespace) to save the desktop session", "save;save session")
                          .split(QLatin1Char(';'), Qt::SkipEmptyParts);
     if (m_session.canSaveSession()) {
-        addSyntax(m_saveKeywords, i18n("Saves the current session for session restoration"));
+        // TODO addSyntax(m_saveKeywords, i18n("Saves the current session for session restoration"));
     }
 
     m_usersKeywords = i18nc("KRunner keywords (split by semicolons without whitespace) to switch user sessions", "switch user;new session")
                           .split(QLatin1Char(';'), Qt::SkipEmptyParts);
     if (m_session.canSwitchUser()) {
-        addSyntax(m_usersKeywords, i18n("Starts a new session as a different user"));
+        // TODO addSyntax(m_usersKeywords, i18n("Starts a new session as a different user"));
     }
 
     m_sessionsKeyword = i18nc("KRunner keyword to list user sessions", "sessions");
-    KRunner::RunnerSyntax sessionsSyntax(m_sessionsKeyword, i18n("Lists all sessions"));
-    addSyntax(sessionsSyntax);
+    // TODO addSyntax(m_sessionsKeyword, i18n("Lists all sessions"));
 
     m_switchKeyword = i18nc("KRunner keyword to switch user sessions", "switch");
-    addSyntax(m_switchKeyword + QStringLiteral(" :q:"),
-              i18n("Switches to the active session for the user :q:, or lists all active sessions if :q: is not provided"));
+    // TODO addSyntax(m_switchKeyword + QStringLiteral(" :q:"),
+    //           i18n("Switches to the active session for the user :q:, or lists all active sessions if :q: is not provided"));
 
-    setMinLetterCount(3);
+    // setMinLetterCount(3);
 }
 
 static inline bool anyKeywordMatches(const QStringList &keywords, const QString &term)
@@ -73,57 +92,58 @@ static inline bool anyKeywordMatches(const QStringList &keywords, const QString 
     });
 }
 
-void SessionRunner::matchCommands(QList<KRunner::QueryMatch> &matches, const QString &term)
+RemoteMatches SessionRunner::matchCommands(const QString &term)
 {
-    KRunner::QueryMatch match(this);
-    match.setCategoryRelevance(KRunner::QueryMatch::CategoryRelevance::Highest);
-    match.setRelevance(0.9);
+    RemoteMatches matches;
+    RemoteMatch match;
+    match.categoryRelevance = qToUnderlying(KRunner::QueryMatch::CategoryRelevance::Highest);
+    match.relevance = 0.9;
     if (anyKeywordMatches(m_logoutKeywords, term)) {
         if (m_session.canLogout()) {
-            match.setText(i18nc("log out command", "Log Out"));
-            match.setIconName(QStringLiteral("system-log-out"));
-            match.setData(LogoutAction);
+            match.text = i18nc("log out command", "Log Out");
+            match.iconName = QStringLiteral("system-log-out");
+            match.properties[QStringLiteral("action")] = LogoutAction;
             matches << match;
         }
     } else if (anyKeywordMatches(m_shutdownKeywords, term)) {
         if (m_session.canShutdown()) {
-            KRunner::QueryMatch match(this);
-            match.setText(i18nc("turn off computer command", "Shut Down"));
-            match.setIconName(QStringLiteral("system-shutdown"));
-            match.setData(ShutdownAction);
+            RemoteMatch match;
+            match.text = i18nc("turn off computer command", "Shut Down");
+            match.iconName = QStringLiteral("system-shutdown");
+            match.properties[QStringLiteral("action")] = ShutdownAction;
             matches << match;
         }
     } else if (anyKeywordMatches(m_restartKeywords, term)) {
         if (m_session.canReboot()) {
-            match.setText(i18nc("restart computer command", "Restart"));
-            match.setIconName(QStringLiteral("system-reboot"));
-            match.setData(RestartAction);
+            match.text = i18nc("restart computer command", "Restart");
+            match.iconName = QStringLiteral("system-reboot");
+            match.properties[QStringLiteral("action")] = RestartAction;
             matches << match;
         }
     } else if (anyKeywordMatches(m_lockKeywords, term)) {
         if (m_session.canLock()) {
-            match.setText(i18nc("lock screen command", "Lock"));
-            match.setIconName(QStringLiteral("system-lock-screen"));
-            match.setData(LockAction);
+            match.text = i18nc("lock screen command", "Lock");
+            match.iconName = QStringLiteral("system-lock-screen");
+            match.properties[QStringLiteral("action")] = LockAction;
             matches << match;
         }
     } else if (anyKeywordMatches(m_saveKeywords, term)) {
         if (m_session.canSaveSession()) {
-            match.setText(i18n("Save Session"));
-            match.setIconName(QStringLiteral("system-save-session"));
-            match.setData(SaveAction);
+            match.text = i18n("Save Session");
+            match.iconName = QStringLiteral("system-save-session");
+            match.properties[QStringLiteral("action")] = SaveAction;
             matches << match;
         }
     }
+    return matches;
 }
 
-void SessionRunner::match(KRunner::RunnerContext &context)
+RemoteMatches SessionRunner::Match(const QString &term)
 {
-    const QString term = context.query();
     QString user;
     bool matchUser = false;
 
-    QList<KRunner::QueryMatch> matches;
+    RemoteMatches matches;
 
     // first compare with "sessions" keyword
     // "SESSIONS" must *NOT* be translated (i18n)
@@ -140,17 +160,17 @@ void SessionRunner::match(KRunner::RunnerContext &context)
         } else {
             // we know it's not "sessions" or "switch <something>", so let's
             // try some other possibilities
-            matchCommands(matches, term);
+            matches << matchCommands(term);
         }
     }
 
     bool switchUser = listAll || anyKeywordMatches(m_usersKeywords, term);
 
     if (switchUser && m_session.canSwitchUser() && dm.isSwitchable() && dm.numReserve() >= 0) {
-        KRunner::QueryMatch match(this);
-        match.setCategoryRelevance(KRunner::QueryMatch::CategoryRelevance::Highest);
-        match.setIconName(QStringLiteral("system-switch-user"));
-        match.setText(i18n("Switch User"));
+        RemoteMatch match;
+        match.categoryRelevance = qToUnderlying(KRunner::QueryMatch::CategoryRelevance::Highest);
+        match.iconName = QStringLiteral("system-switch-user");
+        match.text = i18n("Switch User");
         matches << match;
     }
 
@@ -187,21 +207,22 @@ void SessionRunner::match(KRunner::RunnerContext &context)
                 continue;
             }
 
-            KRunner::QueryMatch match(this);
-            match.setCategoryRelevance(categoryRelevance);
-            match.setRelevance(relevance);
-            match.setIconName(QStringLiteral("user-identity"));
-            match.setText(name);
-            match.setData(QString::number(session.vt));
+            RemoteMatch match;
+            match.categoryRelevance = qToUnderlying(categoryRelevance);
+            match.relevance = relevance;
+            match.iconName = QStringLiteral("user-identity");
+            match.text = name;
+            match.properties[QStringLiteral("terminal")] = QString::number(session.vt);
             matches << match;
         }
     }
 
-    context.addMatches(matches);
+    return matches;
 }
 
-void SessionRunner::run(const KRunner::RunnerContext & /*context*/, const KRunner::QueryMatch &match)
+void SessionRunner::Run(const QString &id, const QString &actionId)
 {
+    /*
     if (match.data().typeId() == QMetaType::Int) {
         switch (match.data().toInt()) {
         case LogoutAction:
@@ -252,7 +273,7 @@ void SessionRunner::run(const KRunner::RunnerContext & /*context*/, const KRunne
     if (confirmNewSession == KMessageBox::Continue) {
         m_session.lock();
         dm.startReserve();
-    }
+    }*/
 }
 
 #include "sessionrunner.moc"
