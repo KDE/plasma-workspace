245e511f16324ae9cf0b33d3745e4c0180a26407
Fushan Wen <qydwhotmail@gmail.com>
2025-02-28 17:13:25 +0800

    components/dbus: add documentation for DBusConnection

--- components/dbus/dbusconnection.h
+++ components/dbus/dbusconnection.h
@@ -26,7 +26,50 @@ class DBusConnection : public QObject
 public:
     explicit DBusConnection(QObject *parent = nullptr);
 
+    /*!
+        \qmlmethod DBusPendingReply org.kde.plasma.workspace.dbus::DBusConnection::asyncCall(message)
+
+        Asynchronously calls the D-Bus method described by the given message.
+        \a message The DBusMessage object that contains information about the D-Bus method call.
+
+        \qml
+        SessionBus.asyncCall({
+            "service": "org.kde.KSplash",
+            "path": "/KSplash",
+            "iface": "org.kde.KSplash",
+            "member": "setStage",
+            "signature": "s",
+            "arguments": [new DBus.string("break")],
+        })
+        \endqml
+    */
     Q_INVOKABLE DBusPendingReply *asyncCall(const DBusMessage &message);
+
+    /*!
+        \qmlmethod void org.kde.plasma.workspace.dbus::DBusConnection::asyncCall(message, resolve, reject)
+
+        Asynchronously calls the D-Bus method described by the given message with JavaScript callback functions.
+        \a message The DBusMessage object that contains information about the D-Bus method call.
+        \a resolve A QJSValue representing the JavaScript callback function to be called when the asynchronous call succeeds.
+        \a reject A QJSValue representing the JavaScript callback function to be called when the asynchronous call fails.
+
+        \qml
+        const promise = new Promise((resolve, reject) => {
+            SessionBus.asyncCall({
+                "service": "org.kde.KSplash",
+                "path": "/KSplash",
+                "iface": "org.kde.KSplash",
+                "member": "setStage",
+                "signature": "s",
+                "arguments": [new DBus.string("break")],
+            }, resolve, reject)
+        }).then((result) => {
+            ...
+        }).catch((error) => {
+            ...
+        })
+        \endqml
+    */
     Q_INVOKABLE void asyncCall(const DBusMessage &message, const QJSValue &resolve, const QJSValue &reject);
 
     static QByteArray parseSignatureFromIntrospection(QStringView introspection, const DBusMessage &message);
@@ -40,6 +83,22 @@ protected:
     QDBusConnection m_connection;
 };
 
+/*!
+    \qmltype SessionBus
+    \inherits DBusConnection
+    \nativetype SessionBusConnection
+    \inqmlmodule org.kde.plasma.workspace.dbus
+
+    \brief Provides a connection to the session bus.
+
+    This class inherits from DBusConnection and provides a way to interact with the session bus.
+
+    \qml
+        SessionBus.asyncCall(message)
+    \endqml
+
+    \sa SystemBus
+*/
 class SessionBusConnection : public DBusConnection
 {
     Q_OBJECT
@@ -50,6 +109,22 @@ public:
     explicit SessionBusConnection(QObject *parent = nullptr);
 };
 
+/*!
+    \qmltype SystemBus
+    \inherits DBusConnection
+    \nativetype SystemBusConnection
+    \inqmlmodule org.kde.plasma.workspace.dbus
+
+    \brief Provides a connection to the system bus.
+
+    This class inherits from DBusConnection and provides a way to interact with the system bus.
+
+    \qml
+        SystemBus.asyncCall(message)
+    \endqml
+
+    \sa SessionBus
+*/
 class SystemBusConnection : public DBusConnection
 {
     Q_OBJECT
