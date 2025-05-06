f8a8c71b73de647fd5f041a1c3bf1889e5dd75a3
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-05-06 17:55:45 +0300

    dataengines/time: Switch to KSystemClockSkewNotifier

--- dataengines/time/timeengine.cpp
+++ dataengines/time/timeengine.cpp
@@ -7,17 +7,12 @@
 
 #include "timeengine.h"
 
+#include <KSystemClockSkewNotifier>
+
 #include <QDBusConnection>
 #include <QDate>
-#include <QSocketNotifier>
 #include <QStringList>
 #include <QTime>
-
-#ifdef Q_OS_LINUX
-#include <fcntl.h>
-#include <sys/timerfd.h>
-#include <unistd.h>
-#endif
 
 #include "debug.h"
 #include "timesource.h"
@@ -48,38 +43,9 @@ void TimeEngine::init()
     QDBusConnection dbus = QDBusConnection::sessionBus();
     dbus.connect(QString(), QString(), QStringLiteral("org.kde.KTimeZoned"), QStringLiteral("timeZoneChanged"), this, SLOT(tzConfigChanged()));
 
-#ifdef Q_OS_LINUX
-    // monitor for the system clock being changed
-    auto timeChangedFd = timerfd_create(CLOCK_REALTIME, O_CLOEXEC | O_NONBLOCK);
-    itimerspec timespec;
-    memset(&timespec, 0, sizeof(timespec)); // set all timers to 0 seconds, which creates a timer that won't do anything
-
-    int err = timerfd_settime(timeChangedFd, 3, &timespec, nullptr); // monitor for the time changing
-    //(flags == TFD_TIMER_ABSTIME | TFD_TIMER_CANCEL_ON_SET). However these are not exposed in glibc so value is hardcoded
-    if (err) {
-        qCWarning(DATAENGINE_TIME) << "Could not create timer with TFD_TIMER_CANCEL_ON_SET. Clock skews will not be detected. Error:"
-                                   << qPrintable(strerror(err));
-    }
-
-    connect(this, &QObject::destroyed, [timeChangedFd]() {
-        close(timeChangedFd);
-    });
-
-    auto notifier = new QSocketNotifier(timeChangedFd, QSocketNotifier::Read, this);
-    connect(notifier, &QSocketNotifier::activated, this, [this](int fd) {
-        uint64_t c;
-        read(fd, &c, 8);
-        clockSkewed();
-    });
-#else
-    dbus.connect(QString(), "/org/kde/kcmshell_clock", "org.kde.kcmshell_clock", "clockUpdated", this, SLOT(clockSkewed()));
-    dbus.connect(QStringLiteral("org.kde.Solid.PowerManagement"),
-                 QStringLiteral("/org/kde/Solid/PowerManagement/Actions/SuspendSession"),
-                 QStringLiteral("org.kde.Solid.PowerManagement.Actions.SuspendSession"),
-                 QStringLiteral("resumingFromSuspend"),
-                 this,
-                 SLOT(clockSkewed()));
-#endif
+    auto notifier = new KSystemClockSkewNotifier(this);
+    connect(notifier, &KSystemClockSkewNotifier::skewed, this, &TimeEngine::clockSkewed);
+    notifier->setActive(true);
 }
 
 void TimeEngine::clockSkewed()
