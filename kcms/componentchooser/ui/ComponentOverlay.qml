KDE/plasma-workspace

commit 46e9e9e469153df9b492855fed69ea520ca0df16

kcms/componentchooser: port Overlaysheet to Dialog
This was an older usage of Overlaysheet that predated the creation of
Dialog. This is a dialog-style UI, so use Dialog.

kcms/componentchooser/ui/ComponentOverlay.qml
@@ -10,7 +10,7 @@ import QtQuick.Controls as QQC2
 import org.kde.kirigami as Kirigami
 import org.kde.kcmutils as KCM
 
-Kirigami.OverlaySheet {
+Kirigami.Dialog {
     id: root
 
     property QtObject componentChooser
@@ -41,6 +41,8 @@ Kirigami.OverlaySheet {
         return "<ul>" + list.map(formatListItem).join("\n") + "</ul>";
     }
 
+    padding: Kirigami.Units.largeSpacing
+
     onOpened: {
         focus = true;
     }
@@ -54,10 +56,9 @@ Kirigami.OverlaySheet {
     modal: true
     QQC2.Overlay.modal: KcmPopupModal {}
 
-    ColumnLayout {
+    contentItem: ColumnLayout {
         enabled: root.componentChooser !== null
         spacing: Kirigami.Units.smallSpacing
-        Layout.preferredWidth: Kirigami.Units.gridUnit * 25
 
         RowLayout {
             Layout.fillWidth: true

kcms/componentchooser/ui/main.qml
@@ -23,6 +23,7 @@ KCM.SimpleKCM {
     ComponentOverlay {
         id: overlay
         parent: root.QQC2.Overlay.overlay
+        width: Math.min(root.width, Kirigami.Units.gridUnit * 250)
     }
 
     Kirigami.FormLayout {

commit c0b357d17633f2b075afb85c908d4581900f306d

kcms/componentchooser: fix dialog scrollability
I broke this in 46e9e9e469153df9b492855fed69ea520ca0df16 while porting
the old OverlaySheet version to Dialog; by overriding the contentItem
of the dialog, I blew away the invisible internal ScrollView that the
content gets magically re-parented to. Which is probably a good example
of why this kind of magic is a bad idea overall.

BUG: 504418
FIXED-IN: 6.3.6

kcms/componentchooser/ui/ComponentOverlay.qml
@@ -56,7 +56,7 @@ Kirigami.Dialog {
     modal: true
     QQC2.Overlay.modal: KcmPopupModal {}
 
-    contentItem: ColumnLayout {
+    ColumnLayout {
         enabled: root.componentChooser !== null
         spacing: Kirigami.Units.smallSpacing
 