bb9a873ce227de280ce80cda4c4bd9f5981fbd10
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-05-30 16:09:18 +0300

    kcms: Add night time configuration module
    This adds the corresponding configuration ui for knighttimed.

---
+++ kcms/nighttime/darklightschedulepreview.cpp
@@ -0,0 +1,183 @@
+/*
+    SPDX-FileCopyrightText: 2025 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "darklightschedulepreview.h"
+
+#include <KDarkLightSchedule>
+
+DarkLightSchedulePreview::DarkLightSchedulePreview(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void DarkLightSchedulePreview::classBegin()
+{
+}
+
+void DarkLightSchedulePreview::componentComplete()
+{
+    recalculate();
+    m_complete = true;
+}
+
+QGeoCoordinate DarkLightSchedulePreview::coordinate() const
+{
+    return m_coordinate;
+}
+
+void DarkLightSchedulePreview::setCoordinate(const QGeoCoordinate &coordinate)
+{
+    if (m_coordinate != coordinate) {
+        m_coordinate = coordinate;
+        if (m_complete) {
+            recalculate();
+        }
+        Q_EMIT coordinateChanged();
+    }
+}
+
+void DarkLightSchedulePreview::resetCoordinate()
+{
+    if (m_coordinate.isValid()) {
+        m_coordinate = QGeoCoordinate();
+        if (m_complete) {
+            recalculate();
+        }
+        Q_EMIT coordinateChanged();
+    }
+}
+
+QString DarkLightSchedulePreview::sunsetStart() const
+{
+    return m_sunsetStart;
+}
+
+void DarkLightSchedulePreview::setSunsetStart(const QString &start)
+{
+    if (m_sunsetStart != start) {
+        m_sunsetStart = start;
+        if (m_complete) {
+            recalculate();
+        }
+        Q_EMIT sunsetStartChanged();
+    }
+}
+
+QString DarkLightSchedulePreview::sunriseStart() const
+{
+    return m_sunriseStart;
+}
+
+void DarkLightSchedulePreview::setSunriseStart(const QString &start)
+{
+    if (m_sunriseStart != start) {
+        m_sunriseStart = start;
+        if (m_complete) {
+            recalculate();
+        }
+        Q_EMIT sunriseStartChanged();
+    }
+}
+
+int DarkLightSchedulePreview::transitionDuration() const
+{
+    return m_transitionDuration.count();
+}
+
+void DarkLightSchedulePreview::setTransitionDuration(int duration)
+{
+    if (m_transitionDuration.count() != duration) {
+        m_transitionDuration = std::chrono::milliseconds(duration);
+        if (m_complete) {
+            recalculate();
+        }
+        Q_EMIT transitionDurationChanged();
+    }
+}
+
+QDateTime DarkLightSchedulePreview::startSunriseDateTime() const
+{
+    return m_startSunriseDateTime;
+}
+
+void DarkLightSchedulePreview::setStartSunriseDateTime(const QDateTime &dateTime)
+{
+    if (m_startSunriseDateTime != dateTime) {
+        m_startSunriseDateTime = dateTime;
+        Q_EMIT startSunriseDateTimeChanged();
+    }
+}
+
+QDateTime DarkLightSchedulePreview::endSunriseDateTime() const
+{
+    return m_endSunriseDateTime;
+}
+
+void DarkLightSchedulePreview::setEndSunriseDateTime(const QDateTime &dateTime)
+{
+    if (m_endSunriseDateTime != dateTime) {
+        m_endSunriseDateTime = dateTime;
+        Q_EMIT endSunriseDateTimeChanged();
+    }
+}
+
+QDateTime DarkLightSchedulePreview::startSunsetDateTime() const
+{
+    return m_startSunsetDateTime;
+}
+
+void DarkLightSchedulePreview::setStartSunsetDateTime(const QDateTime &dateTime)
+{
+    if (m_startSunsetDateTime != dateTime) {
+        m_startSunsetDateTime = dateTime;
+        Q_EMIT startSunsetDateTimeChanged();
+    }
+}
+
+QDateTime DarkLightSchedulePreview::endSunsetDateTime() const
+{
+    return m_endSunsetDateTime;
+}
+
+void DarkLightSchedulePreview::setEndSunsetDateTime(const QDateTime &dateTime)
+{
+    if (m_endSunsetDateTime != dateTime) {
+        m_endSunsetDateTime = dateTime;
+        Q_EMIT endSunsetDateTimeChanged();
+    }
+}
+
+void DarkLightSchedulePreview::recalculate()
+{
+    const QDateTime now = QDateTime::currentDateTime();
+
+    std::optional<KDarkLightSchedule> schedule;
+    if (m_coordinate.isValid()) {
+        schedule = KDarkLightSchedule::forecast(now, m_coordinate.latitude(), m_coordinate.longitude());
+    }
+    if (!schedule) {
+        const QTime sunriseStart = QTime::fromString(m_sunriseStart, QStringLiteral("hh:mm"));
+        const QTime sunsetStart = QTime::fromString(m_sunsetStart, QStringLiteral("hh:mm"));
+        schedule = KDarkLightSchedule::forecast(now, sunriseStart, sunsetStart, m_transitionDuration);
+    }
+
+    apply(*schedule->previousTransition(now));
+    apply(*schedule->nextTransition(now));
+}
+
+void DarkLightSchedulePreview::apply(const KDarkLightTransition &transition)
+{
+    switch (transition.type()) {
+    case KDarkLightTransition::Morning:
+        setStartSunriseDateTime(transition.startDateTime());
+        setEndSunriseDateTime(transition.endDateTime());
+        break;
+    case KDarkLightTransition::Evening:
+        setStartSunsetDateTime(transition.startDateTime());
+        setEndSunsetDateTime(transition.endDateTime());
+        break;
+    }
+}
