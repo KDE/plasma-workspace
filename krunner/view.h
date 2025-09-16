9f75da7ceff31e433b380b242355931b06a70551
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-09-16 15:52:29 +0300

    shell: Pass an xdg activation token from DesktopShell to KRunner
    This makes sure that krunner gets focused after starting to type while
    a desktop view is focused when using medium or high focus stealing
    prevention level.

--- krunner/view.h
+++ krunner/view.h
@@ -93,6 +93,7 @@ public Q_SLOTS:
     void displayWithClipboardContents();
     void query(const QString &term);
     void querySingleRunner(const QString &runnerName, const QString &term);
+    void queryWithActivationToken(const QString &term, const QString &activationToken);
 
 protected Q_SLOTS:
     void loadConfig();
