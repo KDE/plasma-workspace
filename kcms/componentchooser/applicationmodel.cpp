2423ebd94ee3890df09e63b14320faa989ac3b2c
Sune Vuorela <sune@vuorela.dk>
2025-09-26 15:11:09 +0000

    Revert "kcms/componentchooser: Don't offer NoDisplay services"
    This reverts commit 6ee50d925074fd9fcb36e43a2cab9b84f026727e
    
    (cherry picked from commit 4f45f672be8c3f18f1b5af02264929e395a4bbfa)
    
    Co-authored-by: Sune Vuorela <sune@vuorela.dk>

--- kcms/componentchooser/applicationmodel.cpp
+++ kcms/componentchooser/applicationmodel.cpp
@@ -47,7 +47,7 @@ void ApplicationModel::load(const QString &mimeType,
     }
 
     KApplicationTrader::query([preferredService, applicationCategory, mimeType, defaultApplication, this](const KService::Ptr &service) {
-        if (service->exec().isEmpty() || service->noDisplay() || (!applicationCategory.isEmpty() && !service->categories().contains(applicationCategory))
+        if (service->exec().isEmpty() || (!applicationCategory.isEmpty() && !service->categories().contains(applicationCategory))
             || (!mimeType.isEmpty() && !ComponentChooser::serviceSupportsMimeType(service, mimeType))
             || (preferredService && preferredService->storageId() == service->storageId())) {
             return false;
