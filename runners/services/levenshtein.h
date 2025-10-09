c7efc183869a15ae32974e2c5aa779d97da7b82d
Fabian Vogt <fabian@ritter-vogt.de>
2025-10-09 13:13:05 +0200

    servicerunner: Add the second half to #pragma once

--- runners/services/levenshtein.h
+++ runners/services/levenshtein.h
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 // SPDX-FileCopyrightText: 2025 Harald Sitter <sitter@kde.org>
 
-#pragma
+#pragma once
 
 #include <QLoggingCategory>
 #include <QString>
