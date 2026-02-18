da597c8962da1acd9776392b983dd20daee03e4a
Fushan Wen <qydwhotmail@gmail.com>
2026-02-18 21:31:34 +0800

    applets/keyboardindicator: run test on Wayland
    As there is no key mapping for Caps_Lock in Appium, change lockedtest to
    pressedtest and use Control key instead.

--- appiumtests/applets/keyboardindicatortest/keyboardindicatortest.py
+++ appiumtests/applets/keyboardindicatortest/keyboardindicatortest.py
@@ -4,13 +4,13 @@
 # SPDX-License-Identifier: MIT
 
 import os
-import subprocess
-import time
 import unittest
 
 from appium import webdriver
 from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 
 class LockedTest(unittest.TestCase):
@@ -20,7 +20,7 @@ class LockedTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         options = AppiumOptions()
-        options.set_capability("app", f"{os.getenv('QML_EXEC')} {os.path.dirname(os.path.realpath(__file__))}/lockedtest.qml")
+        options.set_capability("app", f"{os.getenv('QML_EXEC')} {os.path.dirname(os.path.realpath(__file__))}/keyboardindicatortest.qml")
         cls.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', options=options)
         options.set_capability("timeouts", {'implicit': 10000})
 
@@ -29,13 +29,13 @@ class LockedTest(unittest.TestCase):
         # Make sure to terminate the driver again, lest it dangles.
         cls.driver.quit()
 
-    def test_caps_lock(self) -> None:
-        self.driver.find_element(AppiumBy.NAME, "unlocked")
-        subprocess.check_call(["xdotool", "key", "Caps_Lock"])
-        self.driver.find_element(AppiumBy.NAME, "locked")
-        subprocess.check_call(["xdotool", "key", "Caps_Lock"])
-        self.driver.find_element(AppiumBy.NAME, "unlocked")
+    def test_presse_and_release(self) -> None:
+        self.driver.find_element(AppiumBy.NAME, "0-0")
+        ActionChains(self.driver).send_keys(Keys.CONTROL).perform()
+        self.driver.find_element(AppiumBy.NAME, "1-1")
+        ActionChains(self.driver).send_keys(Keys.CONTROL).perform()
+        self.driver.find_element(AppiumBy.NAME, "2-2")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(failfast=True)
