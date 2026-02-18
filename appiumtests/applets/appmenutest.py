277273c6377b0591fbcc6c0d9bdc4b4b9e9e6ef0
Fushan Wen <qydwhotmail@gmail.com>
2026-02-18 22:16:12 +0800

    appiumtests/applets: run appmenutest on Wayland

--- appiumtests/applets/appmenutest.py
+++ appiumtests/applets/appmenutest.py
@@ -38,7 +38,6 @@ class AppMenuTest(unittest.TestCase):
 
     driver: webdriver.Remote
     kded: subprocess.Popen | None = None
-    gmenudbusmenuproxy: subprocess.Popen | None = None
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -58,9 +57,6 @@ class AppMenuTest(unittest.TestCase):
             assert kded_started, "kded is not started"
             kded_reply: GLib.Variant = session_bus.call_sync(f"org.kde.kded{KDE_VERSION}", "/kded", f"org.kde.kded{KDE_VERSION}", "loadModule", GLib.Variant("(s)", ["appmenu"]), GLib.VariantType("(b)"), Gio.DBusSendMessageFlags.NONE, 1000)
             assert kded_reply.get_child_value(0).get_boolean(), "appmenu module is not loaded"
-
-        if not "gmenudbusmenuproxy" in subprocess.check_output(["ps", "-ef"]).decode():
-            cls.gmenudbusmenuproxy = subprocess.Popen([os.path.join(CMAKE_RUNTIME_OUTPUT_DIRECTORY, "gmenudbusmenuproxy")])
 
         options = AppiumOptions()
         options.set_capability("app", f"plasmawindowed -p org.kde.plasma.nano {WIDGET_ID}")
@@ -87,9 +83,6 @@ class AppMenuTest(unittest.TestCase):
         """
         Make sure to terminate the driver again, lest it dangles.
         """
-        if cls.gmenudbusmenuproxy is not None:
-            subprocess.check_call([f"kquitapp{KDE_VERSION}", "gmenudbusmenuproxy"])
-            cls.gmenudbusmenuproxy.wait(5)
         if cls.kded is not None:
             subprocess.check_call([f"kquitapp{KDE_VERSION}", f"kded{KDE_VERSION}"])
             cls.kded.wait(5)
@@ -105,18 +98,22 @@ class AppMenuTest(unittest.TestCase):
         """
         Activate a window with two hidden submenus and match strings in the widget
         """
-        gtk_app = subprocess.Popen(["python3", os.path.join(os.path.dirname(os.path.abspath(__file__)), "appmenutest_window.py")], stdout=sys.stderr, stderr=sys.stderr)
-        self.addCleanup(gtk_app.terminate)
+        app = subprocess.Popen(["python3", os.path.join(os.path.dirname(os.path.abspath(__file__)), "appmenutest_window.py")], stdout=sys.stderr, stderr=sys.stderr)
+        self.addCleanup(app.kill)
 
         menu1 = self.driver.find_element(AppiumBy.NAME, "foo")
         menu2 = self.driver.find_element(AppiumBy.NAME, "bar")
 
-        gtk_app.terminate()
-        gtk_app.wait()
+        app.terminate()
+        app.wait()
         wait = WebDriverWait(self.driver, 5)
         wait.until_not(lambda _: menu1.is_displayed())
         wait.until_not(lambda _: menu2.is_displayed())
 
 
 if __name__ == '__main__':
+    try:
+        from PySide6.QtCore import QObject
+    except ModuleNotFoundError:
+        sys.exit(0)
     unittest.main()
--- appiumtests/applets/appmenutest_window.py
+++ appiumtests/applets/appmenutest_window.py
@@ -3,41 +3,29 @@
 # SPDX-FileCopyrightText: 2024 Fushan Wen <qydwhotmail@gmail.com>
 # SPDX-License-Identifier: MIT
 
-import gi
-
-gi.require_version('Gtk', '4.0')
-from gi.repository import GLib, Gtk
+from PySide6.QtWidgets import QApplication, QMainWindow, QMenu
+from PySide6.QtCore import QTimer
 
 
-class TestWindow(Gtk.ApplicationWindow):
+class TestWindow(QMainWindow):
 
-    def __init__(self, _app: Gtk.Application) -> None:
-        super().__init__(application=_app, title="Test Window")
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        self.setWindowTitle("Test Window")
 
-        builder = Gtk.Builder.new_from_string("""
-<interface>
-    <menu id='menubar'>
-        <submenu>
-            <attribute name='label'>_foo</attribute>
-        </submenu>
-        <submenu>
-            <attribute name='label'>_bar</attribute>
-        </submenu>
-    </menu>
-</interface>
-        """, -1)
-        menubar = builder.get_object("menubar")
-        _app.set_menubar(menubar)
-        self.set_show_menubar(False)
-        GLib.timeout_add_seconds(60, self.close)
+        menu_bar = self.menuBar()
+        foo_menu = QMenu("&foo", self)
+        bar_menu = QMenu("&bar", self)
+        menu_bar.addMenu(foo_menu)
+        menu_bar.addMenu(bar_menu)
+        menu_bar.setVisible(False)
 
-
-def on_activate(_app: Gtk.Application) -> None:
-    win = TestWindow(_app)
-    win.present()
+        QTimer.singleShot(60000, self.close)
 
 
 if __name__ == "__main__":
-    app = Gtk.Application(application_id='org.kde.testwindow')
-    app.connect('activate', on_activate)
-    app.run(None)
+    app = QApplication([])
+    app.setDesktopFileName("org.kde.testwindow")
+    win = TestWindow()
+    win.show()
+    app.exec()
