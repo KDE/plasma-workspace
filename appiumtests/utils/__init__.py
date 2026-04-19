8af7dd60c723c84b43c431be3ee0aa75a7b7c9f5
Fushan Wen <qydwhotmail@gmail.com>
2026-04-19 17:51:33 +0800

    appiumtests: update utils/__init__.py to export all utility modules
    Export constants, base classes, and helper functions from the utils package
    for easy import in test files.
    
    Assisted-by: Kilo Code:glm-5

--- appiumtests/utils/__init__.py
+++ appiumtests/utils/__init__.py
@@ -1,2 +1,31 @@
-# SPDX-FileCopyrightText: 2023 Fushan Wen <qydwhotmail@gmail.com>
-# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2026 Fushan Wen <qydwhotmail@gmail.com>
+# SPDX-License-Identifier: MIT
+
+from .constants import (
+    APPIUM_SERVER_URL,
+    KDE_VERSION,
+    DEFAULT_IMPLICIT_TIMEOUT,
+    DEFAULT_WAIT_TIMEOUT,
+    PROCESS_WAIT_ITERATIONS,
+    PROCESS_WAIT_INTERVAL,
+)
+from .base_test import PlasmaAppletTest, KCMTest
+from .process_helpers import quit_plasmawindowed, wait_for_process_exit, cleanup_plasmawindowed
+from .notification_helpers import send_notification
+from .GLibMainLoopThread import GLibMainLoopThread
+
+__all__ = [
+    "APPIUM_SERVER_URL",
+    "KDE_VERSION",
+    "DEFAULT_IMPLICIT_TIMEOUT",
+    "DEFAULT_WAIT_TIMEOUT",
+    "PROCESS_WAIT_ITERATIONS",
+    "PROCESS_WAIT_INTERVAL",
+    "PlasmaAppletTest",
+    "KCMTest",
+    "quit_plasmawindowed",
+    "wait_for_process_exit",
+    "cleanup_plasmawindowed",
+    "send_notification",
+    "GLibMainLoopThread",
+]
