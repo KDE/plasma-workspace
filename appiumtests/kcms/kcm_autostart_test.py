b3259edaa8acdb089d673f59cea9f385f1795999
Fushan Wen <qydwhotmail@gmail.com>
2026-04-19 17:52:06 +0800

    appiumtests: refactor simple KCM tests to use KCMTest base class
    Refactor kcm_style_test, kcm_colors_test, kcm_desktoptheme_test,
    kcm_icons_test, kcm_fonts_test, kcm_autostart_test, kcm_feedback_test,
    kcm_fontinst_test, kcm_lookandfeel_test, kcm_nightlight_test,
    kcm_notifications_test, and kcm_regionandlang_test to inherit from
    KCMTest, removing duplicated setup/teardown code.
    
    Assisted-by: Kilo Code:glm-5

--- appiumtests/kcms/kcm_autostart_test.py
+++ appiumtests/kcms/kcm_autostart_test.py
@@ -3,59 +3,25 @@
 # SPDX-FileCopyrightText: 2023 Fushan Wen <qydwhotmail@gmail.com>
 # SPDX-License-Identifier: MIT
 
-import subprocess
-import time
 import unittest
 from typing import Final
 
-from appium import webdriver
-from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
 
-KDE_VERSION: Final = 6
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils.base_test import KCMTest
+
 KCM_ID: Final = "kcm_autostart"
 
 
-class KCMAutoStartTest(unittest.TestCase):
+class KCMAutoStartTest(KCMTest):
     """
     Tests for kcm_autostart
     """
 
-    driver: webdriver.Remote
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        """
-        Opens the KCM and initialize the webdriver
-        """
-        options = AppiumOptions()
-        options.set_capability("app", f"kcmshell{KDE_VERSION} {KCM_ID}")
-        options.set_capability("timeouts", {'implicit': 10000})
-        options.set_capability("environ", {
-            "LC_ALL": "en_US.UTF-8",
-        })
-        cls.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', options=options)
-
-    def tearDown(self) -> None:
-        """
-        Take screenshot when the current test fails
-        """
-        if not self._outcome.result.wasSuccessful():
-            self.driver.get_screenshot_as_file(f"failed_test_shot_{KCM_ID}_#{self.id()}.png")
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        """
-        Make sure to terminate the driver again, lest it dangles.
-        """
-        cls.driver.find_element(AppiumBy.XPATH, "//*[@name='Close' and contains(@accessibility-id, 'Button')]").click()
-        for _ in range(10):
-            try:
-                subprocess.check_call(["pidof", f"kcmshell{KDE_VERSION}"])
-            except subprocess.CalledProcessError:
-                break
-            time.sleep(1)
-        cls.driver.quit()
+    kcm_id = KCM_ID
 
     def test_0_open(self) -> None:
         """
