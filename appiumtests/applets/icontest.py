67600eb28a9071a4d4220567a32cb7090fa9b47a
Fushan Wen <qydwhotmail@gmail.com>
2026-04-19 17:51:40 +0800

    appiumtests: refactor simple applet tests to use PlasmaAppletTest base class
    Refactor analogclocktest, calendartest, icontest, lock_logouttest, and
    manage-inputmethodtest to inherit from PlasmaAppletTest, removing
    duplicated setup/teardown code.
    
    Assisted-by: Kilo Code:glm-5

--- appiumtests/applets/icontest.py
+++ appiumtests/applets/icontest.py
@@ -3,61 +3,29 @@
 # SPDX-FileCopyrightText: 2023 Fushan Wen <qydwhotmail@gmail.com>
 # SPDX-License-Identifier: MIT
 
-import subprocess
-import time
 import unittest
 from typing import Final
 
-from appium import webdriver
-from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
 
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils.base_test import PlasmaAppletTest
+
 WIDGET_ID: Final = "org.kde.plasma.icon"
-KDE_VERSION: Final = 6
 
 
-class IconTest(unittest.TestCase):
+class IconTest(PlasmaAppletTest):
     """
     Tests for the icon widget
     """
 
-    driver: webdriver.Remote
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        """
-        Opens the widget and initialize the webdriver
-        """
-        options = AppiumOptions()
-        options.set_capability("app", f"plasmawindowed -p org.kde.plasma.nano {WIDGET_ID}")
-        options.set_capability("environ", {
-            "LC_ALL": "en_US.UTF-8",
-            "QT_FATAL_WARNINGS": "1",
-            "QT_LOGGING_RULES": "qt.accessibility.atspi.warning=false;qt.qml.propertyCache.append.warning=false",
-        })
-        options.set_capability("timeouts", {'implicit': 10000})
-        cls.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', options=options)
-
-    def tearDown(self) -> None:
-        """
-        Take screenshot when the current test fails
-        """
-        if not self._outcome.result.wasSuccessful():
-            self.driver.get_screenshot_as_file(f"failed_test_shot_{WIDGET_ID}_#{self.id()}.png")
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        """
-        Make sure to terminate the driver again, lest it dangles.
-        """
-        subprocess.check_call([f"kquitapp{KDE_VERSION}", "plasmawindowed"])
-        for _ in range(10):
-            try:
-                subprocess.check_call(["pidof", "plasmawindowed"])
-            except subprocess.CalledProcessError:
-                break
-            time.sleep(1)
-        cls.driver.quit()
+    widget_id = WIDGET_ID
+    extra_environ = {
+        "QT_FATAL_WARNINGS": "1",
+        "QT_LOGGING_RULES": "qt.accessibility.atspi.warning=false;qt.qml.propertyCache.append.warning=false",
+    }
 
     def test_0_open(self) -> None:
         """
--- appiumtests/applets/manage-inputmethodtest.py
+++ appiumtests/applets/manage-inputmethodtest.py
@@ -6,48 +6,26 @@
 import unittest
 from typing import Final
 
-from appium import webdriver
-from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
+
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils.base_test import PlasmaAppletTest
 
 WIDGET_ID: Final = "org.kde.plasma.manage-inputmethod"
 
 
-class ManageInputMethodTest(unittest.TestCase):
+class ManageInputMethodTest(PlasmaAppletTest):
     """
     Tests for the Input Method widget
     """
 
-    driver: webdriver.Remote
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        """
-        Opens the widget and initialize the webdriver
-        """
-        options = AppiumOptions()
-        options.set_capability("app", f"plasmawindowed -p org.kde.plasma.nano {WIDGET_ID}")
-        options.set_capability("environ", {
-            "LC_ALL": "en_US.UTF-8",
-            "QT_FATAL_WARNINGS": "1",
-            "QT_LOGGING_RULES": "qt.accessibility.atspi.warning=false;qt.qml.propertyCache.append.warning=false",
-        })
-        options.set_capability("timeouts", {'implicit': 10000})
-        cls.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', options=options)
-
-    def tearDown(self) -> None:
-        """
-        Take screenshot when the current test fails
-        """
-        if not self._outcome.result.wasSuccessful():
-            self.driver.get_screenshot_as_file(f"failed_test_shot_{WIDGET_ID}_#{self.id()}.png")
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        """
-        Make sure to terminate the driver again, lest it dangles.
-        """
-        cls.driver.quit()
+    widget_id = WIDGET_ID
+    extra_environ = {
+        "QT_FATAL_WARNINGS": "1",
+        "QT_LOGGING_RULES": "qt.accessibility.atspi.warning=false;qt.qml.propertyCache.append.warning=false",
+    }
 
     def test_0_open(self) -> None:
         """
