31f2c83b4a83d22441bdaacaf0701fe65a740ac3
Marco Martin <notmart@gmail.com>
2026-06-12 12:35:04 +0200

    AutoHideScreenEdge: Remove X11 code
    remove x11 specific codepaths

--- shell/autohidescreenedge.cpp
+++ shell/autohidescreenedge.cpp
@@ -20,10 +20,6 @@
 #include <QtWaylandClient/QtWaylandClientVersion>
 #include <qpa/qplatformwindow_p.h>
 
-#if HAVE_X11
-#include <xcb/xcb.h>
-#endif
-
 class WaylandScreenEdgeManagerV1 : public QWaylandClientExtensionTemplate<WaylandScreenEdgeManagerV1>, public QtWayland::kde_screen_edge_manager_v1
 {
     Q_OBJECT
@@ -174,91 +170,6 @@ void WaylandAutoHideScreenEdge::destroy()
     m_edge.reset();
 }
 
-#if HAVE_X11
-
-class X11AutoHideScreenEdge : public AutoHideScreenEdge
-{
-    Q_OBJECT
-
-public:
-    X11AutoHideScreenEdge(PanelView *view);
-    ~X11AutoHideScreenEdge() override;
-
-    void deactivate() override;
-    void activate() override;
-
-private:
-    xcb_atom_t m_atom = XCB_ATOM_NONE;
-};
-
-X11AutoHideScreenEdge::X11AutoHideScreenEdge(PanelView *view)
-    : AutoHideScreenEdge(view)
-{
-    xcb_connection_t *connection = qGuiApp->nativeInterface<QNativeInterface::QX11Application>()->connection();
-
-    constexpr QByteArrayView atomName("_KDE_NET_WM_SCREEN_EDGE_SHOW");
-    xcb_intern_atom_cookie_t cookie = xcb_intern_atom_unchecked(connection, false, atomName.length(), atomName.constData());
-    xcb_intern_atom_reply_t *reply = xcb_intern_atom_reply(connection, cookie, nullptr);
-    if (reply) {
-        m_atom = reply->atom;
-        free(reply);
-    }
-}
-
-X11AutoHideScreenEdge::~X11AutoHideScreenEdge()
-{
-    if (!m_view) {
-        return;
-    }
-    deactivate();
-}
-
-void X11AutoHideScreenEdge::deactivate()
-{
-    if (m_atom != XCB_ATOM_NONE) {
-        xcb_delete_property(qGuiApp->nativeInterface<QNativeInterface::QX11Application>()->connection(), m_view->winId(), m_atom);
-    }
-}
-
-void X11AutoHideScreenEdge::activate()
-{
-    if (m_atom == XCB_ATOM_NONE) {
-        return;
-    }
-
-    uint32_t value = 0;
-
-    switch (m_view->location()) {
-    case Plasma::Types::TopEdge:
-        value = 0;
-        break;
-    case Plasma::Types::RightEdge:
-        value = 1;
-        break;
-    case Plasma::Types::BottomEdge:
-        value = 2;
-        break;
-    case Plasma::Types::LeftEdge:
-        value = 3;
-        break;
-    case Plasma::Types::Floating:
-    default:
-        value = 4;
-        break;
-    }
-
-    xcb_change_property(qGuiApp->nativeInterface<QNativeInterface::QX11Application>()->connection(),
-                        XCB_PROP_MODE_REPLACE,
-                        m_view->winId(),
-                        m_atom,
-                        XCB_ATOM_CARDINAL,
-                        32,
-                        1,
-                        &value);
-}
-
-#endif
-
 AutoHideScreenEdge::AutoHideScreenEdge(PanelView *view)
     : QObject(view)
     , m_view(view)
@@ -267,15 +178,7 @@ AutoHideScreenEdge::AutoHideScreenEdge(PanelView *view)
 
 AutoHideScreenEdge *AutoHideScreenEdge::create(PanelView *view)
 {
-    if (KWindowSystem::isPlatformWayland()) {
-        return new WaylandAutoHideScreenEdge(view);
-    } else {
-#if HAVE_X11
-        return new X11AutoHideScreenEdge(view);
-#else
-        Q_UNREACHABLE();
-#endif
-    }
+    return new WaylandAutoHideScreenEdge(view);
 }
 
 #include "autohidescreenedge.moc"
