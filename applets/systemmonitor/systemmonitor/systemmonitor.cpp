8e613ff80916e37d4a88b3041beb46432c1ed4b4
Nicolas Fella <nicolas.fella@gmx.de>
2026-06-25 15:59:01 +0200

    applets/systemmonitor: Use better way to obtain Plasma engine
    Such API exists meanwhile

--- applets/systemmonitor/systemmonitor/systemmonitor.cpp
+++ applets/systemmonitor/systemmonitor/systemmonitor.cpp
@@ -20,7 +20,7 @@
 #include <KLocalizedString>
 #include <KNotificationJobUiDelegate>
 #include <KService>
-#include <PlasmaQuick/SharedQmlEngine>
+#include <PlasmaQuick/PlasmaQuick>
 
 using namespace Qt::StringLiterals;
 
@@ -36,11 +36,8 @@ void SystemMonitor::init()
 {
     configChanged();
 
-    // FIXME HACK: better way to get the engine At least AppletQuickItem should have an engine() getter
-    auto qmlObject = new PlasmaQuick::SharedQmlEngine();
     KConfigGroup cg = config();
-    m_sensorFaceController = new KSysGuard::SensorFaceController(cg, qmlObject->engine().get(), new QQmlEngine(this));
-    qmlObject->deleteLater();
+    m_sensorFaceController = new KSysGuard::SensorFaceController(cg, PlasmaQuick::globalEngine().get(), new QQmlEngine(this));
 
     // NOTE: taking the pluginId from the child applet (cpu monitor, memory, whatever) is done implicitly by not embedding metadata in this applet
     const QString preset = config().readEntry("CurrentPreset", pluginMetaData().pluginId());
