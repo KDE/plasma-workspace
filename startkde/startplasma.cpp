8932b46e20506f0b5aada06ef23ac22689d67504
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2026-07-15 10:02:50 +0300

    startkde: Update LookAndFeelPackage in kdeglobals
    This prevents the autoswitching module from potentially applying the new
    global theme again.

--- startkde/startplasma.cpp
+++ startkde/startplasma.cpp
@@ -396,10 +396,8 @@ static std::optional<std::pair<QString, KLookAndFeelManager::Contents>> dayNight
     return std::make_pair(lookAndFeelName, KLookAndFeelManager::AppearanceSettings);
 }
 
-static std::pair<QString, KLookAndFeelManager::Contents> determineLookAndFeel()
+static std::pair<QString, KLookAndFeelManager::Contents> determineLookAndFeel(const LookAndFeelSettings &settings)
 {
-    const LookAndFeelSettings settings;
-
     if (settings.automaticLookAndFeel()) {
         if (const auto lookAndFeel = dayNightLookAndFeel(settings)) {
             return *lookAndFeel;
@@ -429,8 +427,15 @@ void setupPlasmaEnvironment()
     QDir().mkpath(extraConfigDir);
     qputenv("XDG_CONFIG_DIRS", QByteArray(QFile::encodeName(extraConfigDir) + ':' + currentConfigDirs));
 
-    const auto &[lookAndFeelName, lookAndFeelContents] = determineLookAndFeel();
+    LookAndFeelSettings lookAndFeelSettings;
+    const auto &[lookAndFeelName, lookAndFeelContents] = determineLookAndFeel(lookAndFeelSettings);
     qCDebug(PLASMA_STARTUP) << "Applying" << lookAndFeelName << "global theme";
+
+    if (lookAndFeelSettings.lookAndFeelPackage() != lookAndFeelName) {
+        lookAndFeelSettings.setLookAndFeelPackage(lookAndFeelName);
+        lookAndFeelSettings.save();
+    }
+
     QFile activeLnf(extraConfigDir + QLatin1String("/package"));
     if (!activeLnf.open(QIODevice::ReadOnly) || activeLnf.readLine() != lookAndFeelName.toUtf8()) {
         KPackage::Package package = KPackage::PackageLoader::self()->loadPackage(QStringLiteral("Plasma/LookAndFeel"), lookAndFeelName);
