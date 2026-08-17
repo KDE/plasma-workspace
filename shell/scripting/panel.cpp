39d7057ac573eae7442d7b4a7921208de07a7087
Nate Graham <nate@kde.org>
2026-08-17 15:36:08 -0400

    shell/scripting: fix setOpacity calling wrong method
    (cherry picked from commit 0c85cad8a88e2bdbaf08ee10f4e1ec844cc9519f)
    
    Co-authored-by: Luis Bocanegra <luisbocanegra17b@gmail.com>

--- shell/scripting/panel.cpp
+++ shell/scripting/panel.cpp
@@ -392,7 +392,7 @@ void Panel::setOpacity(const QString &mode)
     }
 
     if (panel()) {
-        panel()->setOpacity(opacityMode);
+        panel()->setOpacityMode(opacityMode);
     } else {
         panelConfig().parent().writeEntry("panelOpacity", (int)opacityMode);
     }
