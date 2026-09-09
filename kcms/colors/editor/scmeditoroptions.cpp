KDE/plasma-workspace

commit e5843e7dcc367751b894eca6dbaf6965c2f5b647

Colors: Add frameContrast value
Change the slider from Colors editor into a spinbox, that can be
used to set the outline contrast value from between 0% to 100%.

This value will be updated to the Colorscheme file, and the
KDE Globals file, which then different platforms can use.

See issue https://invent.kde.org/teams/vdg/issues/-/issues/108

kcms/colors/editor/scmeditoroptions.cpp
@@ -9,6 +9,7 @@
 
 #include <KConfigGroup>
 #include <QDebug>
+#include <QTimer>
 
 #include "../colorsapplicator.h"
 
@@ -17,10 +18,19 @@ using namespace Qt::StringLiterals;
 SchemeEditorOptions::SchemeEditorOptions(KSharedConfigPtr config, QWidget *parent)
     : QWidget(parent)
     , m_config(config)
+    , m_spinboxUpdateTimer(new QTimer(this))
 {
     setupUi(this);
     m_disableUpdates = false;
+    m_spinboxUpdateTimer->setSingleShot(true);
+    // Smoother spinbox
+    connect(m_spinboxUpdateTimer, &QTimer::timeout, this, [this]() {
+        Q_EMIT changed(true);
+        updateContrastExample();
+    });
     loadOptions();
+    updateContrastExample();
+    contrastPercentageSpinBox->setDecimals(0);
 }
 
 void SchemeEditorOptions::updateValues()
@@ -35,8 +45,10 @@ void SchemeEditorOptions::loadOptions()
 
     accentTitlebar->setChecked(generalGroup.readEntry("TitlebarIsAccentColored", generalGroup.readEntry("accentActiveTitlebar", false)));
 
-    KConfigGroup KDEgroup(m_config, u"KDE"_s);
-    contrastSlider->setValue(KDEgroup.readEntry("contrast", KColorScheme::contrastF() * 10));
+    KConfigGroup KDEGroup(m_config, u"KDE"_s);
+    contrastPercentageSpinBox->blockSignals(true);
+    contrastPercentageSpinBox->setValue(KDEGroup.readEntry("frameContrast", KColorScheme::frameContrast()) * 100.0);
+    contrastPercentageSpinBox->blockSignals(false);
 
     KConfigGroup group(m_config, u"ColorEffects:Inactive"_s);
     useInactiveEffects->setChecked(group.readEntry("Enable", false));
@@ -57,12 +69,16 @@ void SchemeEditorOptions::loadOptions()
 }
 
 // Option slot
-void SchemeEditorOptions::on_contrastSlider_valueChanged(int value)
+void SchemeEditorOptions::on_contrastPercentageSpinBox_valueChanged(double value)
 {
+    // Convert the old value to something between 1 to 10
     KConfigGroup group(m_config, u"KDE"_s);
-    group.writeEntry("contrast", value);
+    group.writeEntry("Contrast", qRound(value / 10.0));
 
-    Q_EMIT changed(true);
+    // We need to make sure we always write a double
+    group.writeEntry("frameContrast", qreal(value / 100.0));
+
+    m_spinboxUpdateTimer->start();
 }
 
 void SchemeEditorOptions::on_shadeSortedColumn_stateChanged(int state)
@@ -132,6 +148,13 @@ void SchemeEditorOptions::on_accentTitlebar_stateChanged(int state)
     group.deleteEntry("accentInactiveTitlebar");
 
     Q_EMIT changed(true);
+}
+
+void SchemeEditorOptions::updateContrastExample()
+{
+    const QColor frameColor(
+        KColorUtils::mix(palette().color(QPalette::Window), palette().color(QPalette::WindowText), contrastPercentageSpinBox->value() / 100.0));
+    contrastExample->setStyleSheet(QStringLiteral("background-color: %1").arg(frameColor.name()));
 }
 
 #include "moc_scmeditoroptions.cpp"

kcms/colors/colorsapplicator.cpp
@@ -291,6 +291,15 @@ void applyScheme(const QString &colorSchemePath, KConfig *configOutput, KConfig:
         ++i;
     }
 
+    KConfigGroup groupKDETheme(config, u"KDE"_s);
+    KConfigGroup groupKDEOut(configOutput, u"KDE"_s);
+
+    groupKDEOut.writeEntry(QStringLiteral("frameContrast"),
+                           groupKDETheme.readEntry(QStringLiteral("frameContrast"), KColorScheme::frameContrast()),
+                           writeConfigFlag.setFlag(KConfig::WriteConfigFlag::Notify));
+
+    groupKDEOut.writeEntry(QStringLiteral("contrast"), groupKDETheme.readEntry(QStringLiteral("contrast"), 7), writeConfigFlag);
+
     if (hasAccent && (tintAccent || applyAccentToTitlebar)) { // Titlebar accent colouring
         const auto windowBackground = config->group(u"Colors:Window"_s).readEntry<QColor>("BackgroundNormal", QColor());
 

kcms/colors/editor/scmeditoroptions.h
@@ -29,7 +29,7 @@ Q_SIGNALS:
 private Q_SLOTS:
 
     // options slots
-    void on_contrastSlider_valueChanged(int value);
+    void on_contrastPercentageSpinBox_valueChanged(double value);
     void on_shadeSortedColumn_stateChanged(int state);
     void on_inactiveSelectionEffect_stateChanged(int state);
     void on_useInactiveEffects_stateChanged(int state);
@@ -42,7 +42,9 @@ private:
     void loadOptions();
     void setCommonForeground(KColorScheme::ForegroundRole role, int stackIndex, int buttonIndex);
     void setCommonDecoration(KColorScheme::DecorationRole role, int stackIndex, int buttonIndex);
+    void updateContrastExample();
 
     KSharedConfigPtr m_config;
     bool m_disableUpdates;
+    QTimer *m_spinboxUpdateTimer;
 };

kcms/colors/editor/scmeditoroptions.ui
@@ -59,17 +59,17 @@
       <number>100</number>
      </property>
      <property name="orientation">
-      <enum>Qt::Horizontal</enum>
+      <enum>Qt::Orientation::Horizontal</enum>
      </property>
     </widget>
    </item>
    <item>
     <widget class="QGroupBox" name="groupBox_5">
      <property name="toolTip">
-      <string>Shading of frames and lighting (&quot;3D&quot;) effects</string>
+      <string>Contrast of the frames, separators and outlines</string>
      </property>
      <property name="title">
-      <string>Shading</string>
+      <string>Frames and Outlines</string>
      </property>
      <layout class="QGridLayout" name="gridLayout_3">
       <property name="horizontalSpacing">
@@ -78,50 +78,39 @@
       <property name="verticalSpacing">
        <number>0</number>
       </property>
-      <item row="0" column="2">
-       <spacer name="horizontalSpacer">
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>198</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
-      <item row="0" column="3">
-       <widget class="QLabel" name="label_10">
-        <property name="text">
-         <string>Maximum</string>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="1" colspan="3">
-       <widget class="QSlider" name="contrastSlider">
-        <property name="maximum">
-         <number>10</number>
-        </property>
-        <property name="pageStep">
-         <number>5</number>
-        </property>
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-       </widget>
-      </item>
-      <item row="0" column="1">
-       <widget class="QLabel" name="label_9">
-        <property name="text">
-         <string>Minimum</string>
-        </property>
-       </widget>
-      </item>
       <item row="1" column="0">
        <widget class="QLabel" name="label_8">
         <property name="text">
          <string>Contrast</string>
+        </property>
+       </widget>
+      </item>
+      <item row="1" column="1">
+       <widget class="QWidget" name="contrastExample" native="true">
+        <property name="minimumSize">
+         <size>
+          <width>25</width>
+          <height>25</height>
+         </size>
+        </property>
+        <property name="maximumSize">
+         <size>
+          <width>25</width>
+          <height>25</height>
+         </size>
+        </property>
+        <property name="autoFillBackground">
+         <bool>false</bool>
+        </property>
+       </widget>
+      </item>
+      <item row="1" column="2">
+       <widget class="QDoubleSpinBox" name="contrastPercentageSpinBox">
+        <property name="suffix">
+         <string>%</string>
+        </property>
+        <property name="maximum">
+         <number>100</number>
         </property>
        </widget>
       </item>
@@ -131,7 +120,7 @@
    <item>
     <spacer name="verticalSpacer">
      <property name="orientation">
-      <enum>Qt::Vertical</enum>
+      <enum>Qt::Orientation::Vertical</enum>
      </property>
      <property name="sizeHint" stdset="0">
       <size>

commit aed6fe84236e81f29c564dc994e5e0bf2103e292

kcms/colors: fix typo when saving "contrast" value in the config
KColorScheme does not use `Contrast`, it uses `contrast` with a lowercase letter.

Fixes a regression since e5843e7dcc367751b894eca6dbaf6965c2f5b647.

kcms/colors/editor/scmeditoroptions.cpp
@@ -73,7 +73,7 @@ void SchemeEditorOptions::on_contrastPercentageSpinBox_valueChanged(double value
 {
     // Convert the old value to something between 1 to 10
     KConfigGroup group(m_config, u"KDE"_s);
-    group.writeEntry("Contrast", qRound(value / 10.0));
+    group.writeEntry("contrast", qRound(value / 10.0));
 
     // We need to make sure we always write a double
     group.writeEntry("frameContrast", qreal(value / 100.0));