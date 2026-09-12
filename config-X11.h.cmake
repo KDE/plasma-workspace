0a68364a7528f2c79e1d37aa8d65dc7d7795860b
Nicolas Fella <nicolas.fella@gmx.de>
2026-09-12 13:31:29 +0200

    Drop unused HAVE_XCURSOR/HAVE_XFIXES

--- config-X11.h.cmake
+++ config-X11.h.cmake
@@ -1,8 +1,2 @@
-/* Define to 1 if you have Xcursor */
-#cmakedefine HAVE_XCURSOR 1
-
-/* Define if you have the XFixes extension */
-#cmakedefine HAVE_XFIXES 1
-
 /* Define if you have X11 at all */
 #cmakedefine01 HAVE_X11
